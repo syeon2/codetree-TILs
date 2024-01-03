7a2692451147794fbc56fc08a3091b8f68c9edc2
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-01-03 14:11:46 +0900

    Title: 2줄 출력, Time: 86ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240103/2줄 출력/print-two-lines.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write("Hello");
+        bw.newLine();
+        bw.write("World");
+
+        bw.flush();
+        bw.close();
+    }
+}
