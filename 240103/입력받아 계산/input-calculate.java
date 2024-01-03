2e43ea80dda47a96689b81e05e1d8973aee61d19
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-01-03 14:54:02 +0900

    Title: 입력받아 계산, Time: 114ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240103/입력받아 계산/input-calculate.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int a = Integer.parseInt(br.readLine());
+
+        System.out.println(a + 2);
+    }
+}
