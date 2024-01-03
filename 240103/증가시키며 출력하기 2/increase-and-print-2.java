ceb461efa20acede2b7ef40c0480d11b6e5f6073
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-01-03 18:09:09 +0900

    Title: 증가시키며 출력하기 2, Time: 92ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240103/증가시키며 출력하기 2/increase-and-print-2.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        int n = 10;
+
+        while (n <= 26) {
+            System.out.printf("%d ", n++);
+        }
+    }
+}
