16a2cae08d552bc4adb23bba321ae30d600166cd
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-01-03 21:55:43 +0900

    Title: ab 사이에 없는 c, Time: 165ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240103/ab 사이에 없는 c/c-between-a-and-b-2.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        boolean flag = true;
+
+        for (int i = a; i <= b; i++) {
+            if (i % c == 0) {
+                flag = false;
+                break;
+            }
+        }
+
+        if (flag) System.out.println("YES");
+        else System.out.println("NO");
+    }
+}
