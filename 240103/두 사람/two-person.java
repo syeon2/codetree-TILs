67105112708ccb9f5c9dab170e0468460896405e
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-01-03 17:08:38 +0900

    Title: 두 사람, Time: 119ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240103/두 사람/two-person.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int ageA = sc.nextInt();
+        String sexA = sc.next();
+
+        int ageB = sc.nextInt();
+        String sexB = sc.next();
+
+        if ((ageA >= 19 && sexA.equals("M")) || (ageA >= 19 && sexA.equals("M"))) System.out.println(1);
+        else System.out.println(0);
+    }
+}
