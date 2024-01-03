e1a635228eab1fcec07bbceeb941825bf363fa71
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-01-03 21:49:46 +0900

    Title: 합성수 판별기, Time: 165ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240103/합성수 판별기/factorization-discriminator.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        boolean flag = false;
+
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) {
+                flag = true;
+                break;
+            }
+        }
+
+        if (flag) System.out.println("C");
+        else System.out.println("N");
+    }
+}
