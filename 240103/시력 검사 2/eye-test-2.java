2f980b7b465909e787d7f1add0fe384a2bb905e9
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-01-03 16:15:46 +0900

    Title: 시력 검사 2, Time: 145ms, Memory: 11MB, Status: Passed - CodeTree

--- 240103/시력 검사 2/eye-test-2.java
+++ 240103/시력 검사 2/eye-test-2.java
@@ -7,8 +7,8 @@ public class Main {
 
         double a = sc.nextDouble();
 
-        if (a > 1.0) System.out.println("High");
-        else if (a > 0.5) System.out.println("Middle");
+        if (a >= 1.0) System.out.println("High");
+        else if (a >= 0.5) System.out.println("Middle");
         else System.out.println("Low");
     }
 }
