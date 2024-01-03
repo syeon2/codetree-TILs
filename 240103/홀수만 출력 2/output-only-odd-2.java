98f04a3efa5ef90389c70b719ab331231dfb5c30
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-01-03 18:08:14 +0900

    Title: 홀수만 출력 2, Time: 157ms, Memory: 10MB, Status: Passed - CodeTree

--- 240103/홀수만 출력 2/output-only-odd-2.java
+++ 240103/홀수만 출력 2/output-only-odd-2.java
@@ -5,8 +5,8 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
 
-        int a = sc.nextInt();
         int b = sc.nextInt();
+        int a = sc.nextInt();
 
         for (int i = b; i >= a; i--) {
             if (i % 2 == 1) System.out.printf("%d ", i);
