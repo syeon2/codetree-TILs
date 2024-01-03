9834b2e0ecabd8f10f1919d564fcb18d408b899e
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-01-03 14:09:24 +0900

    Title: 따옴표 출력, Time: 88ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240103/따옴표 출력/print-quote.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		bw.write("He says \"It's a really simple sentence\".");
+		bw.flush();
+		bw.close();
+    }
+}
