7c09af8d7e1ccd511c1f86dc134c9aa42f320794
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-01-03 17:53:18 +0900

    Title: 차례로 출력, Time: 90ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240103/차례로 출력/print-in-order.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        for (int i = 5; i <= 17; i++) {
+            System.out.print(i);
+            System.out.print(" ");
+        }
+    }
+}
