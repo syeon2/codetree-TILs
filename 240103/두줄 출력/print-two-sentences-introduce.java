a50fd51b872dfca4bc6f5ab0b5f46c4a3a1ace9c
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-01-03 14:19:51 +0900

    Title: 두줄 출력, Time: 100ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240103/두줄 출력/print-two-sentences-introduce.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        bw.write("Hello students!");
+        bw.newLine();
+        bw.write("Welcome to LeebrosCode!");
+        
+        bw.flush();
+        bw.close();
+    }
+}
