9a91734c9e99783b29a9cf85567f69b27ff5ad71
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-01-03 14:50:59 +0900

    Title: 변수 값 복사하기, Time: 93ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240103/변수 값 복사하기/copying-variable-values.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        int a = 1;
+        int b = 2;
+        int c = 3;
+
+        a = c;
+        b = c;
+
+        System.out.printf("%d %d %d", a, b, c);
+    }
+}
