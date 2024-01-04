1f9acc007aaac1572d7dd0e0ccbc1b6e60e21ae0
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-01-04 22:12:32 +0900

    Title: 별표 출력하기 13, Time: 138ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240104/별표 출력하기 13/print-start-13.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+
+            if (i % 2 == 1) {
+                for (int k = 0; k < n - ((i + 1) / 2 - 1); k++) {
+                    System.out.print("* ");
+                }
+            } else {
+                for (int k = 0; k < n - i; k++) {
+                    System.out.print("* ");
+                }
+            }
+
+            System.out.println();
+        }
+
+        for (int i = n; i >= 1; i--) {
+
+            if (i % 2 == 1) {
+                for (int k = 0; k < n - ((i + 1) / 2 - 1); k++) {
+                    System.out.print("* ");
+                }
+            } else {
+                for (int k = 0; k < n - i; k++) {
+                    System.out.print("* ");
+                }
+            }
+
+            System.out.println();
+        }
+    }
+}
