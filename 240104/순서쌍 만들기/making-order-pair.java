6a4cc399954a52c835dabd76f9635383ab6daf92
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-01-04 23:36:09 +0900

    Title: 순서쌍 만들기, Time: 152ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240104/순서쌍 만들기/making-order-pair.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for (int i = n; i >= 1; i--) {
+
+            for (int k = n; k >= 1; k--) {
+                System.out.printf("(%d,%d) ", i, k);
+            }
+
+            System.out.println();
+        }
+    }
+}
