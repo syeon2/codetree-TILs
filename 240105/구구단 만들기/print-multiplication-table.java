898a275a41befb1c5c6b984e90311cb94b80f180
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-01-05 23:27:24 +0900

    Title: 구구단 만들기, Time: 169ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240105/구구단 만들기/print-multiplication-table.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        for (int i = 1; i <= 9; i++) {
+
+            for (int k = b; k >= a; k--) {
+                if (k % 2 == 0) {
+                    System.out.printf("%d * %d = %d", k, i, k * i);
+
+                    if (k > a) System.out.print(" / ");
+                }
+            }
+
+            System.out.println();
+        }
+    }
+}
