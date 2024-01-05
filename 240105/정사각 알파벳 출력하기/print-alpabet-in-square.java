e0af2cc0c982d9afba46b5ed91c652303c10e75f
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-01-05 23:59:24 +0900

    Title: 정사각 알파벳 출력하기, Time: 129ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240105/정사각 알파벳 출력하기/print-alpabet-in-square.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int cnt = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            for (int k = 0; k < n; k++) {
+                System.out.print((char) ('A' + cnt++));
+            }
+
+            System.out.println();
+        }
+    }
+}
