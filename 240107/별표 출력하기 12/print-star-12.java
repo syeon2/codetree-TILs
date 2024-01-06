2b39ea36a614d911942657df9cf2b4b16f3535c9
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-01-07 00:18:49 +0900

    Title: 별표 출력하기 12, Time: 122ms, Memory: 10MB, Status: Passed - CodeTree

--- 240107/별표 출력하기 12/print-star-12.java
+++ 240107/별표 출력하기 12/print-star-12.java
@@ -7,50 +7,53 @@ public class Main {
 
         int n = sc.nextInt();
 
-        for (int i = 1; i <= (n / 2) * 2; i++) {
+        if (n == 1) System.out.print("*");
+        else {
+            for (int i = 1; i <= (n / 2) * 2; i++) {
 
-            if (i == 1) {
-                for (int k = 0; k < n; k++) {
-                    System.out.print("* ");
-                }
-                System.out.println();
-            } else if (i == 2) {
-                System.out.print("  ");
-
-                for (int k = 0; k < (n / 2); k++) {
-                    System.out.print("*   ");
-                }
-
-                System.out.println();
-            } else {
-                int blank = 1;
-                int star = (n / 2) - 1;
-
-                int cnt = 0;
-
-                while (star >= 1) {
+                if (i == 1) {
+                    for (int k = 0; k < n; k++) {
+                        System.out.print("* ");
+                    }
+                    System.out.println();
+                } else if (i == 2) {
                     System.out.print("  ");
 
-                    for (int k = 1; k <= blank; k++) {
-                        System.out.print("    ");
-                    }
-                
-                    for (int k = 1; k <= star; k++) {
+                    for (int k = 0; k < (n / 2); k++) {
                         System.out.print("*   ");
                     }
 
                     System.out.println();
+                } else {
+                    int blank = 1;
+                    int star = (n / 2) - 1;
 
-                    if (cnt == 1) {
-                        cnt = 0;
-                        blank++;
-                        star--;
-                    } else {
-                        cnt++;
+                    int cnt = 0;
+
+                    while (star >= 1) {
+                        System.out.print("  ");
+
+                        for (int k = 1; k <= blank; k++) {
+                            System.out.print("    ");
+                        }
+                    
+                        for (int k = 1; k <= star; k++) {
+                            System.out.print("*   ");
+                        }
+
+                        System.out.println();
+
+                        if (cnt == 1) {
+                            cnt = 0;
+                            blank++;
+                            star--;
+                        } else {
+                            cnt++;
+                        }
                     }
-                }
 
-                break;
+                    break;
+                }
             }
         }
     }
