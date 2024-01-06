7fb614bd9861e2b02008526f2c7a4d36204ca7dc
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-01-06 23:49:34 +0900

    Title: 특정 위치의 숫자, Time: 137ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240106/특정 위치의 숫자/numbers-in-specific-location.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int sum = 0;
+
+        for (int i = 0; i < 10; i++) {
+            int n = sc.nextInt();
+
+            if (i == 2 || i == 4 || i == 9) sum += n;
+        }
+
+        System.out.print(sum);
+    }
+}
