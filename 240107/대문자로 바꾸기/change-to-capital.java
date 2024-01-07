27fdcfc965cea67d10184d001ef4231e8f3119d7
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-01-07 23:54:27 +0900

    Title: 대문자로 바꾸기, Time: 121ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240107/대문자로 바꾸기/change-to-capital.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 0; i < 5; i++) {
+
+            for (int k = 0; k < 3; k++) {
+                char c = sc.next().charAt(0);
+
+                System.out.print((char) (c - 'a' + 'A'));
+                System.out.print(" ");
+            }
+
+            System.out.println();
+        }
+    }
+}
