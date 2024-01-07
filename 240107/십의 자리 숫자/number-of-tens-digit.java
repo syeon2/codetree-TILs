ce5f7f0db6428af3b671f789449d247bc15dca97
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-01-07 21:29:16 +0900

    Title: 십의 자리 숫자, Time: 145ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240107/십의 자리 숫자/number-of-tens-digit.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int[] list = new int[9];
+
+        while (true) {
+            int n = sc.nextInt();
+
+            if (n == 0) break;
+
+            int temp = n / 10;
+
+            if (temp == 0) continue;
+            else list[temp - 1]++;
+        }
+
+        for (int i = 0; i < 9; i++) {
+            System.out.printf("%d - %d\n", i + 1, list[i]);
+        }
+    }
+}
