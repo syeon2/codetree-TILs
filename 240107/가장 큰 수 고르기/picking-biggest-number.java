e345b3cdeefa123a30ecfb0ecd9b5de4eaad0a2d
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-01-07 22:12:21 +0900

    Title: 가장 큰 수 고르기, Time: 151ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240107/가장 큰 수 고르기/picking-biggest-number.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int max = 0;
+
+        for (int i = 0; i < 10; i++) {
+            int n = sc.nextInt();
+
+            if (n > max) max = n;
+        }
+
+        System.out.println(max);
+    }
+}
