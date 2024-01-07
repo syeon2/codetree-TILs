a4f6228561d31a7cf1ca5657e6b19c2300eaa5d0
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-01-07 21:06:41 +0900

    Title: 일의 자리 배열, Time: 146ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240107/일의 자리 배열/array-with-units.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.printf("%d %d ", a, b);
+
+        for (int i = 3; i <= 10; i++) {
+            int temp = (a + b) % 10;
+
+            a = b;
+            b = temp;
+
+            System.out.printf("%d ", temp);
+        }
+    }
+}
