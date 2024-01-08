4b768c9cfaca05c8b7f0fb86945acf1ffe946423
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-01-08 14:50:37 +0900

    Title: 대문자로 출력하기, Time: 129ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240108/대문자로 출력하기/print-in-capital.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+
+            if (c >= 'a' && c <= 'z') System.out.print((char) (c - 'a' + 'A'));
+            else if (c >= 'A' && c <= 'Z') System.out.print(c);
+        }
+    }
+}
