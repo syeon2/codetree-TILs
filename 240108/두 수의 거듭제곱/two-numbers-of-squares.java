7a9f637728fb5b86b502f40b5d7e22dee6b67327
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-01-08 23:13:59 +0900

    Title: 두 수의 거듭제곱, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240108/두 수의 거듭제곱/two-numbers-of-squares.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(getNum(a, b));
+    }
+
+    public static int getNum(int a, int b) {
+        int prod = 1;
+
+        for (int i = 0; i < b; i++) {
+            prod *= a;
+        }
+
+        return prod;
+    }
+}
