9ea7858af33b2b0752640b39da18e3042120232d
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-01-08 14:55:00 +0900

    Title: 정수만 더하기, Time: 118ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240108/정수만 더하기/add-only-integers.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+
+        int sum = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+
+            if (c >= '0' && c <= '9') sum += (char) (c - '0');
+        }
+
+        System.out.print(sum);
+    }
+}
