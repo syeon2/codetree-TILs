f89c3ca2c2d38695dad2667a21044d3f0a0cc951
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-01-08 13:51:17 +0900

    Title: 두 번째를 첫 번째로, Time: 118ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240108/두 번째를 첫 번째로/second-to-first.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+
+        char first = str.charAt(0);
+        char second = str.charAt(1);
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == second) System.out.print(first);
+            else System.out.print(str.charAt(i));
+        }
+    }
+}
