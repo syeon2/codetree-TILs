994aff4c91a96ed6658aa6f2d001d603172502bb
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-01-08 15:06:48 +0900

    Title: 붙여서 합하기, Time: 125ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240108/붙여서 합하기/add-and-add.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        String s1 = sc.next();
+        String s2 = sc.next();
+
+        System.out.print(Integer.parseInt(s1 + s2) + Integer.parseInt(s2 + s1));
+    }
+}
