3f3464c0ebe65670c02dfaa116a89612cefe69a0
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-01-08 14:33:16 +0900

    Title: 규칙에 따라 밀기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240108/규칙에 따라 밀기/push-by-the-rules.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+        String direction = sc.next();
+
+        int idx = 0;
+
+        for (int i = 0; i < direction.length(); i++) {
+            if (direction.charAt(i) == 'L') idx += -1;
+            else idx += 1;
+        }
+
+        if (idx < 0) str = str.substring((idx * -1)) + str.substring(0, (idx * -1));
+        else if (idx > 0) str = str.substring(str.length() - idx) + str.substring(0, str.length() - idx);
+
+        System.out.print(str);
+    }
+}
