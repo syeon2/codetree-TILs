6842e102612b078e0a58a6d736597c27a1ff4a63
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-01-08 14:38:27 +0900

    Title: 아스키코드표 맞추기, Time: 146ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240108/아스키코드표 맞추기/chart-of-ASCII.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 0; i < 5; i++) {
+            int n = sc.nextInt();
+
+            System.out.print(((char) n) + " " );
+        }
+    }
+}
