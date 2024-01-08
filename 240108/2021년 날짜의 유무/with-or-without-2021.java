0ca822d1a9e91fafffb9cffddda1fa4944a2821a
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-01-08 23:47:33 +0900

    Title: 2021년 날짜의 유무, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240108/2021년 날짜의 유무/with-or-without-2021.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static int[] list = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int M = sc.nextInt();
+        int D = sc.nextInt();
+
+        if (isExist(M, D)) System.out.print("Yes");
+        else System.out.print("No");
+    }
+
+    public static boolean isExist(int m, int d) {
+        if (list[m - 1] < d) return false;
+        else return true;
+    }
+}
