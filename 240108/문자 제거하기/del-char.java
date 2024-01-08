f24c6758e5a80296ec2008bdfb5b997591ec490b
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-01-08 13:59:21 +0900

    Title: 문자 제거하기, Time: 134ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240108/문자 제거하기/del-char.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+
+        while (str.length() > 1) {
+            int n = sc.nextInt();
+
+            if (n > str.length()) {
+                str = str.substring(0, str.length() - 1);
+            }
+            else {
+                str = str.substring(0, n) + str.substring(n + 1);
+
+                System.out.println(str);
+            }
+        }
+
+        System.out.print(str);
+    }
+}
