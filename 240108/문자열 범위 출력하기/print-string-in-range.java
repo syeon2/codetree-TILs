2424c82aacde88ed94958dca161be222a1d3db16
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-01-08 09:14:33 +0900

    Title: 문자열 범위 출력하기, Time: 124ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240108/문자열 범위 출력하기/print-string-in-range.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.nextLine();
+
+        for (int i = 2; i < 10; i++) {
+            System.out.print(s.charAt(i));
+        }
+    }
+}
