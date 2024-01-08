5ead0ac024b386a17d9b44c69a0c86b526225dca
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-01-08 15:18:51 +0900

    Title: 각 자리 숫자들의 합, Time: 217ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240108/각 자리 숫자들의 합/sum-of-each-digit.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+
+        int sum = 0;
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+
+            int num = c - '0';
+
+            sum += num;
+        }
+
+        System.out.print(sum);
+    }
+}
