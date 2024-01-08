9527ba8ad96bfcd379789775f64984998189e8b8
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-01-08 20:24:45 +0900

    Title: 최대공약수 구하기, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

--- 240108/최대공약수 구하기/find-the-greatest-common-divisor.java
+++ 240108/최대공약수 구하기/find-the-greatest-common-divisor.java
@@ -8,14 +8,12 @@ public class Main {
         int a = sc.nextInt();
         int b = sc.nextInt();
 
-        int min = a;
-        if (min > b) min = b;
+        System.out.print(gcd(a, b));
+    }
 
-        for (int i = min; i >= 1; i--) {
-            if (a % i == 0 && b % i == 0) {
-                System.out.print(i);
-                break;
-            }
-        }
+    public static int gcd(int a, int b) {
+        if (b == 0) return a;
+
+        return gcd(b, a % b);
     }
 }
