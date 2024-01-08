a95f33546c53189349d9480c8349ef05d1e331b7
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-01-08 15:35:43 +0900

    Title: 지그재그로 숫자 채우기, Time: 417ms, Memory: 17MB, Status: Passed - CodeTree

--- 240108/지그재그로 숫자 채우기/zigzag-numbering.java
+++ 240108/지그재그로 숫자 채우기/zigzag-numbering.java
@@ -8,15 +8,27 @@ public class Main {
         int n = sc.nextInt();
         int m = sc.nextInt();
 
+        int[][] list = new int[n][m];
+
+        int cnt = 0;
+        
+        for (int i = 0; i < m; i++) {
+
+            if (i % 2 == 0) {
+                for (int k = 0; k < n; k++) {
+                    list[k][i] = cnt++;
+                }
+            } else {
+                for (int k = n - 1; k >= 0; k--) {
+                    list[k][i] = cnt++;
+                }
+            }
+        }
+
         for (int i = 0; i < n; i++) {
 
-            int temp = i;
-
             for (int k = 0; k < m; k++) {
-                System.out.printf("%d ", temp);
-
-                if (k == 0 || k % 2 == 0) temp += (n - i) * 2 - 1;
-                else temp++;
+                System.out.printf("%d ", list[i][k]);
             }
 
             System.out.println();
