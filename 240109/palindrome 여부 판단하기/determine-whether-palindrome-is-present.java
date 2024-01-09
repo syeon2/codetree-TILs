af4ae998c6bb0067fef2e2715218895c630e789b
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-01-09 19:49:33 +0900

    Title: palindrome 여부 판단하기, Time: 128ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 240109/palindrome 여부 판단하기/determine-whether-palindrome-is-present.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+
+        if (str.equals(reverse(str))) System.out.print("Yes");
+        else System.out.print("No");
+    }
+
+    public static String reverse(String str) {
+        char[] list = new char[str.length()];
+
+        for (int i = 0; i < str.length(); i++) {
+            list[i] = str.charAt(i);
+        }
+
+        return String.valueOf(list);
+    }
+}
