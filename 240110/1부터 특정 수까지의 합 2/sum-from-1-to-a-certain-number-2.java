149cc31492ea22960a704eeaa88fee16edbba447
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-01-10 10:21:47 +0900

    Title: 1부터 특정 수까지의 합 2, Time: 125ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240110/1부터 특정 수까지의 합 2/sum-from-1-to-a-certain-number-2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        int ans = recur(N);
+
+        System.out.println(ans);
+    }
+
+    public static int recur(int n) {
+        if (n == 1) return 1;
+
+        return n + recur(n - 1);
+    }
+
+}
