d8235838baf126e2fb2de13e71a9f1c90b7be13c
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-01-10 16:06:10 +0900

    Title: 두 개의 동일한 수열, Time: 167ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240110/두 개의 동일한 수열/two-equal-series.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        int[] list1 = new int[N];
+        int[] list2 = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            list1[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < N; i++) {
+            list2[i] = sc.nextInt();
+        }
+
+        Arrays.sort(list1);
+        Arrays.sort(list2);
+
+        boolean flag = true;
+
+        for (int i = 0; i < N; i++) {
+            if (list1[i] != list2[i]) {
+                flag = false;
+                break;
+            }
+        }
+
+        if (flag) System.out.print("Yes");
+        else System.out.print("No");
+    }
+}
