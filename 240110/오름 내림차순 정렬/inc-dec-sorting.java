d0a9e553f7051641c848fb438bbbb9b348f4a58d
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-01-10 15:59:08 +0900

    Title: 오름 내림차순 정렬, Time: 346ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 240110/오름 내림차순 정렬/inc-dec-sorting.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        int[] list = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            list[i] = sc.nextInt();
+        }
+
+        Arrays.sort(list);
+
+        for (int i = 0; i < N; i++) {
+            System.out.printf("%d ", list[i]);
+        }
+
+        System.out.println();
+
+        Integer[] list2 = Arrays.stream(list).boxed().toArray(Integer[]::new);
+
+        Arrays.sort(list2, Collections.reverseOrder());
+
+        for (int i = 0; i < N; i++) {
+            System.out.printf("%d ", list2[i]);
+        }
+    }
+}
