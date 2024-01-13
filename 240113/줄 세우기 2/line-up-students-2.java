829e744837cc74be3b38971dc273c7ac0ea74f00
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-01-13 22:06:02 +0900

    Title: 줄 세우기 2, Time: 404ms, Memory: 18MB, Status: Wrong Answer - CodeTree

--- 240113/줄 세우기 2/line-up-students-2.java
+++ 240113/줄 세우기 2/line-up-students-2.java
@@ -6,6 +6,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         int N = sc.nextInt();
+
         Integer[][] list = new Integer[N][3];
 
         for (int i = 0; i < N; i++) {
@@ -19,17 +20,23 @@ public class Main {
 
         Arrays.sort(list, new Comparator<Integer[]>() {
             @Override
-            public int compare(Integer[] a, Integer[] b) {
-                if (a[0] == b[0]) {  
-                    return b[1] - a[1];
+            public int compare(Integer[] list1, Integer[] list2) {
+                if (list1[0] == list2[0]) {
+                    if (list1[1] == list2[1]) {
+                        return list1[2] - list2[2];
+                    }
+
+                    return list2[1] - list1[1];
                 }
 
-                return a[0] - b[0];
+                return list1[0] - list2[0];
             }
         });
 
         for (int i = 0; i < N; i++) {
-            System.out.printf("%d %d %d\n", list[i][0], list[i][1], list[i][2]);
+            Integer[] a = list[i];
+
+            System.out.printf("%d %d %d\n", a[0], a[1], a[2]);
         }
     }
 }
