ef4e6380d11e44590a55836392b8f3cb8025241d
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-01-16 22:39:41 +0900

    Title: 2진수로 변환하기, Time: 141ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240116/2진수로 변환하기/convert-to-binary.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        int[] list = new int[20];
+        int cnt = 0;
+
+        while (N != 0) {
+            if (N % 2 == 0) list[cnt] = 0;
+            else list[cnt] = 1;
+
+            N /= 2;
+            cnt++;
+        }
+
+        for (int i = cnt - 1; i >= 0; i--) {
+            System.out.print(list[i]);
+        }
+    }
+}
