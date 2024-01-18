4fcdf8be576ec9056a3afbe1657d2f9680e7341d
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-01-18 22:02:47 +0900

    Title: 선두를 지켜라, Time: 208ms, Memory: 26MB, Status: Wrong Answer - CodeTree

---
+++ 240118/선두를 지켜라/keep-the-lead.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+
+        int[] listA = new int[1000001];
+        int[] listB = new int[1000001];
+
+        int curTimeA = 0;
+        int curTimeB = 0;
+
+        for (int n = 0; n < N; n++) {
+            int v = sc.nextInt();
+            int t = sc.nextInt();
+
+            for (int i = 1; i <= t; i++) {
+                listA[++curTimeA] = listA[curTimeA - 1] + v;
+            }
+        }
+
+        for (int m = 0; m < M; m++) {
+            int v = sc.nextInt();
+            int t = sc.nextInt();
+
+            for (int i = 1; i <= t; i++) {
+                listB[++curTimeB] = listB[curTimeB - 1] + v;
+            }
+        }
+
+        int flag = -1;
+        int cnt = 0;
+
+        for (int i = 1; i <= curTimeA; i++) {
+            if (listA[i] > listB[i] && (flag == 1 || flag == -1)) {
+                flag = 0;
+                cnt++;
+            } else if (listA[i] < listB[i] && (flag == 0 || flag == -1)) {
+                flag = 1;
+                cnt++;
+            }
+        }
+
+        System.out.print(cnt - 1);
+    }
+}
