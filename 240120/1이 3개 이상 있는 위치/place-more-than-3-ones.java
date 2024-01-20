5c1e09d597100a394c83d5f4ac69b46c17f321c6
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-01-20 10:26:58 +0900

    Title: 1이 3개 이상 있는 위치, Time: 280ms, Memory: 18MB, Status: Passed - CodeTree

--- 240120/1이 3개 이상 있는 위치/place-more-than-3-ones.java
+++ 240120/1이 3개 이상 있는 위치/place-more-than-3-ones.java
@@ -1,37 +1,40 @@
 import java.util.Scanner;
 
 public class Main {
+    public static int[] dx = {0, 1, 0, -1};
+    public static int[] dy = {1, 0, -1, 0};
+
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
 
         int N = sc.nextInt();
 
-        int[][] board = new int[N + 2][N + 2];
+        int[][] board = new int[N][N];
 
-        for (int i = 1; i <= N; i++) {
+        for (int i = 0; i < N; i++) {
 
-            int num = sc.nextInt();
-
-            for (int k = 1; k <= N; k++) {
-                board[i][k] = num;
+            for (int k = 0; k < N; k++) {
+                board[i][k] = sc.nextInt();
             }
         }
 
         int ans = 0;
 
-        for (int i = 1; i <= N; i++) {
-            int cnt = 0;
+        for (int i = 0; i < N; i++) {
 
-            for (int k = 1; k <= N; k++) {
+            for (int k = 0; k < N; k++) {
+                int cnt = 0;
 
-                if (board[i - 1][k] == 1) cnt++;
-                if (board[i + 1][k] == 1) cnt++;
-                if (board[i][k - 1] == 1) cnt++;
-                if (board[i][k + 1] == 1) cnt++;
+                for (int j = 0; j < 4; j++) {
+                    if ((i + dy[j] < 0 || i + dy[j] >= N) || (k + dx[j] < 0 || k + dx[j] >= N)) continue;
+                    else {
+                        if (board[i + dy[j]][k + dx[j]] == 1) cnt++;
+                    }
+                }
+
+                if (cnt >= 3) ans++;
             }
-
-            if (cnt >= 3) ans++;
         }
 
         System.out.print(ans);
