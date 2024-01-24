1101b32c5453293113f31e9b17018efd82be6f05
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-01-24 14:52:23 +0900

    Title: 이상한 진수 2, Time: 134ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240124/이상한 진수 2/awkward-digits-2.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+
+        System.out.print(solution(str));
+    }
+
+    public static int solution(String num) {
+        int temp = 0;
+
+        boolean flag = false;
+
+        for (int i = 0; i < num.length(); i++) {
+
+            if (num.charAt(i) == '1') temp += Math.pow(2, num.length() - i - 1);
+            else {
+                if (flag) continue;
+                else {
+                    temp += Math.pow(2, num.length() - i - 1);
+                    flag = true;
+                }
+            }
+        }
+
+        return temp;
+    }
+}
