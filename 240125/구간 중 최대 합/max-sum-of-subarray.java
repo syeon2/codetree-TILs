cb3c949c3f76072326433a61709e1fb4e553ddb9
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-01-25 22:44:40 +0900

    Title: 구간 중 최대 합, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240125/구간 중 최대 합/max-sum-of-subarray.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+
+        int[] list = new int[n];
+
+        for (int i = 0; i < n; i++) {
+
+            list[i] = sc.nextInt();
+        }
+
+        int max = 0;
+
+        for (int i = 0; i < n - 2; i++) {
+
+            int temp = 0;
+            for (int j = i; j < i + k; j++) {
+                temp += list[j];
+            }
+
+            if (max < temp) max = temp;
+        }
+
+        System.out.print(max);
+    }
+}
