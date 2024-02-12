e070cba63a79a1a143aa86dbf6b196ba2ccb9399
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-13 08:54:29 +0900

    Title: 합을 복사하기, Time: 97ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240213/합을 복사하기/copy-the-sum.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        int a = 1;
+        int b = 2;
+        int c = 3;
+
+        int sum = a + b + c;
+
+        a = b = c = sum;
+
+        System.out.println(a + " " + b + " " + c);
+    }
+}
