67d2ec795e84fc51b72c2a80d9d03068d742c9d1
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-13 08:51:47 +0900

    Title: 데이터 교환, Time: 80ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240213/데이터 교환/exchange-data.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        int a = 5;
+        int b = 6;
+        int c = 7;
+
+        int tempA = a;
+        int tempB = b;
+        int tempC = c;
+
+        c = tempB;
+        b = tempA;
+        a = tempC;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
