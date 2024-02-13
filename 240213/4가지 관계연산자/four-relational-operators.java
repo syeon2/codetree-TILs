826c96a4a75ce753f25e51c61b7dcbc999781aa6
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-13 13:56:59 +0900

    Title: 4가지 관계연산자, Time: 173ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240213/4가지 관계연산자/four-relational-operators.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        func(a >= b);
+        func(a > b);
+        func(b >= a);
+        func(b > a);
+    }
+
+    public static void func(boolean bol) {
+        if (bol) System.out.println(1);
+        else System.out.println(0);
+    }
+}
