4cf6213282b3773f9c9eca691b5ff442545494ef
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-13 18:31:39 +0900

    Title: 별표 출력하기 5, Time: 167ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240213/별표 출력하기 5/print-star-5.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for (int i = n; i >= 1; i--) {
+
+            for (int k = 1; k <= i; k++) {
+
+                for (int j = 1; j <= i; j++) {
+                    System.out.print("*");
+                }
+
+                System.out.print(" ");
+            }
+
+            System.out.println();
+        }
+    }
+}
