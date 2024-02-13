cb5c83edb9a78645d621ae516b8e731e0e6b90f8
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-13 22:24:55 +0900

    Title: 19단 출력, Time: 244ms, Memory: 12MB, Status: Passed - CodeTree

---
+++ 240213/19단 출력/nineteen-times-table.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 1; i <= 19; i++) {
+
+            for (int k = 1; k <= 19; k++) {
+                System.out.printf("%d * %d = %d", i, k, i * k);
+
+                if (k == 19 || k % 2 == 0) System.out.println();
+                else System.out.print(" / "); 
+            }
+        }
+    }
+}
