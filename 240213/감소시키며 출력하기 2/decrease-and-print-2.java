5bc2726bca394a627af06a997b102aaa153c7b0a
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-13 15:43:04 +0900

    Title: 감소시키며 출력하기 2, Time: 108ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240213/감소시키며 출력하기 2/decrease-and-print-2.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        int num = 26;
+
+        while (num >= 10) {
+            System.out.printf("%d ", num--);
+        }
+
+    }
+}
