70cf370f088ecae1d9e48aa69fc3bfe9b394bc48
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-14 08:08:56 +0900

    Title: 배열에서 원소의 특징, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240214/배열에서 원소의 특징/characteristics-of-elements-in-an-array.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int ans = 0;
+
+        for (int i = 0; i < 10; i++) {
+            int num = sc.nextInt();
+
+            if (num % 3 == 0) break;
+
+            ans = num;
+        }
+
+        System.out.print(ans);
+    }
+}
