fcfe22387530234fc7f9bd0d62dee0bb01857479
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-13 15:51:41 +0900

    Title: n번 더하기, Time: 152ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240213/n번 더하기/add-n-times.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int n = sc.nextInt();
+        int cnt = n;
+
+        while (cnt > 0) {
+            System.out.println(a + n);
+
+            a += n;
+            cnt--;
+        }
+    }
+}
