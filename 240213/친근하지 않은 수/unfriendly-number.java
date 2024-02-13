eb560b119da327369ebda29f713a3e200542777a
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-13 16:50:13 +0900

    Title: 친근하지 않은 수, Time: 137ms, Memory: 10MB, Status: Passed - CodeTree

--- 240213/친근하지 않은 수/unfriendly-number.java
+++ 240213/친근하지 않은 수/unfriendly-number.java
@@ -9,7 +9,7 @@ public class Main {
 
         int cnt = 0;
 
-        for (int i = 1; i <= 10; i++) {
+        for (int i = 1; i <= n; i++) {
             if (i % 2 == 0 || i % 3 == 0 || i % 5 == 0) continue;
             else cnt++;
         }
