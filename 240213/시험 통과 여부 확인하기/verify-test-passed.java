137bca409057bfe8c03f0143658f3c87c11fd0a5
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-13 13:28:53 +0900

    Title: 시험 통과 여부 확인하기, Time: 161ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240213/시험 통과 여부 확인하기/verify-test-passed.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int score = sc.nextInt();
+
+        if (score >= 80) System.out.println("pass");
+        else System.out.printf("%d more score", 80 - score);
+    }
+}
