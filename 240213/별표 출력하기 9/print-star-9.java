6d4d40476812340ea9409cc8b6843cfc2621292f
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-13 18:58:56 +0900

    Title: 별표 출력하기 9, Time: 126ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240213/별표 출력하기 9/print-star-9.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+
+            for (int k = 0; k < n - 1 - i; k++) {
+                System.out.print("  ");
+            }
+
+            for (int k = 0; k < ((n - i) * 2) - 1; k++) {
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+    }
+}
