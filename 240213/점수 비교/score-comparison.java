570d685961d02b90cf0f82154423c0f2f9af2ca5
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-13 14:00:19 +0900

    Title: 점수 비교, Time: 233ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240213/점수 비교/score-comparison.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int mathA = sc.nextInt();
+        int englishA = sc.nextInt();
+
+        int mathB = sc.nextInt();
+        int englishB = sc.nextInt();
+
+        if (mathA > mathB && englishA > englishB) System.out.println(1);
+        else System.out.println(0);
+    }
+}
