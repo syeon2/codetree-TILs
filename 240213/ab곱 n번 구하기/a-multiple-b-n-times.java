9e72533aa4f368ca50f6bfcb7fee07b3211a2564
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-13 23:04:16 +0900

    Title: ab곱 n번 구하기, Time: 132ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240213/ab곱 n번 구하기/a-multiple-b-n-times.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+
+            int prod = 1;
+
+            for (int k = a; k <= b; k++) {
+                prod *= k;
+            }
+
+            System.out.println(prod);
+        }
+    }
+}
