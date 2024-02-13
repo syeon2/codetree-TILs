373db73a8d00b64cb2bcaa6aa92620b31d23ae16
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-14 07:35:13 +0900

    Title: 10개 입력받기, Time: 160ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240214/10개 입력받기/receive-10-inputs.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int sum = 0;
+        int cnt = 0;
+
+        for (int i = 0; i < 10; i++) {
+            int num = sc.nextInt();
+
+            if (num == 0) break;
+
+            sum += num;
+            cnt++;
+        }
+
+        double avg = (double) sum / cnt;
+
+        System.out.print(String.format("%d %.1f", sum, avg));
+    }
+}
