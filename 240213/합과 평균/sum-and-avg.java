a147b43c973753ab8ca738581cc1725a766bc0a2
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-13 13:13:03 +0900

    Title: 합과 평균, Time: 166ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240213/합과 평균/sum-and-avg.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int sum = a + b;
+        double avg = (double) sum / 2;
+
+        System.out.printf("%d %.1f", sum, avg);
+    }
+}
