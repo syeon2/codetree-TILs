c0e44d23295901e1664d14f4cabbe96acef3d981
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-13 14:21:06 +0900

    Title: 좀 더 어려운 수학 점수, Time: 274ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240213/좀 더 어려운 수학 점수/math-scores-are-more-difficult.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int mathA = sc.nextInt();
+        int englishA = sc.nextInt();
+
+        int mathB = sc.nextInt();
+        int englishB = sc.nextInt();
+
+        if (mathA > mathB) System.out.println("A");
+        else if (mathA < mathB) System.out.println("B");
+        else if (mathA == mathB && englishA > englishB) System.out.println("A");
+        else if (mathA == mathB && englishA < englishB) System.out.print("B");
+    }
+}
