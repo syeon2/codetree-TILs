79edf8cae7c04254c886609f2ac17d5e8e28b5c0
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-13 19:05:14 +0900

    Title: 두 개의 직각삼각형, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240213/두 개의 직각삼각형/two-right-triangle.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for (int i = n; i >= 1; i--) {
+
+            for (int k = 1; k <= i; k++) {
+                System.out.print("*");
+            }
+
+            for (int k = 1; k <= n - i; k++) {
+                System.out.print("  ");
+            }
+
+            for (int k = 1; k <= i; k++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
