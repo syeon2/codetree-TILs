687207cf0516c4f58b17f255b698dae93ecaa4c9
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-13 19:13:49 +0900

    Title: 별 그리기, Time: 310ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240213/별 그리기/star-drawing.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+
+            for (int k = 1; k <= n - i; k++) {
+                System.out.print(" ");
+            }
+
+            for (int k = 1; k <= (i * 2) - 1; k++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+        for (int i = n - 1; i >= 1; i--) {
+
+            for (int k = 1; k <= n - i; k++) {
+                System.out.print(" ");
+            }
+
+            for (int k = 1; k <= (i * 2) - 1; k++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
