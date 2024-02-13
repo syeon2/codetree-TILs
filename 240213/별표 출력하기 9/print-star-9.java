598dad2f3b3084e104f8c4509a111bff585ed400
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-13 19:02:24 +0900

    Title: 별표 출력하기 9, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

--- 240213/별표 출력하기 9/print-star-9.java
+++ 240213/별표 출력하기 9/print-star-9.java
@@ -7,13 +7,13 @@ public class Main {
 
         int n = sc.nextInt();
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 1; i <= n; i++) {
 
-            for (int k = 0; k < n - 1 - i; k++) {
+            for (int k = 1; k <= n - i; k++) {
                 System.out.print("  ");
             }
 
-            for (int k = 0; k < ((n - i) * 2) - 1; k++) {
+            for (int k = 1; k <= (i * 2) - 1; k++) {
                 System.out.print("* ");
             }
 
