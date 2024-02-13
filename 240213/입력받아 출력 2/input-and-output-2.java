3ac30cba1dbdf1e0d20e8a53d8c75dec10e1425b
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-13 09:40:30 +0900

    Title: 입력받아 출력 2, Time: 144ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240213/입력받아 출력 2/input-and-output-2.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] list = br.readLine().split("-");
+
+        System.out.printf("%s%s", list[0], list[1]);
+    }
+}
