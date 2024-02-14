6f2f06a64ba263776a4cad0a38864283768027fc
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-15 07:34:09 +0900

    Title: 별 찍는 것을 5번 반복하기, Time: 96ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240215/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        printStar();
+        printStar();
+        printStar();
+        printStar();
+        printStar();
+    }
+
+    public static void printStar() {
+        System.out.println("**********");
+    }
+}
