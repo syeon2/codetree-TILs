1196c9188362c4a7704a3327599cfe7c48960dd9
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-14 17:17:33 +0900

    Title: 미는 횟수, Time: 123ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240214/미는 횟수/number-of-pushes.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        String A = sc.next();
+        String B = sc.next();
+
+        int cnt = 0;
+
+        for (int i = 0; i < 6; i++) {
+            if (A.equals(B)) break;
+            else {
+                A = A.substring(A.length() - 1) + A.substring(0, A.length() - 1);
+                cnt++;
+            }
+        }
+
+        if (cnt >= A.length()) System.out.print(-1);
+        else System.out.print(cnt);
+    }
+}
