8ca65a33fbe4d7f0010e659ec2eda3ea3e05ac6b
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-15 08:20:18 +0900

    Title: 함수를 이용한 소수 판별, Time: 141ms, Memory: 10MB, Status: Passed - CodeTree

--- 240215/함수를 이용한 소수 판별/decimal-decisions-using-functions.java
+++ 240215/함수를 이용한 소수 판별/decimal-decisions-using-functions.java
@@ -22,7 +22,8 @@ public class Main {
     }
 
     public static boolean isPrime(int n) {
-
+        if (n == 1) return false;
+        
         for (int i = 2; i < n; i++) {
             if (n % i == 0) return false;
         }
