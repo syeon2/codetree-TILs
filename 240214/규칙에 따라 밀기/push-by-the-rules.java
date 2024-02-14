77ac411683fdb3260dfff2ae9ce4a723837aa3b9
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-14 16:16:32 +0900

    Title: 규칙에 따라 밀기, Time: 119ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240214/규칙에 따라 밀기/push-by-the-rules.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+        char[] orders = sc.next().toCharArray();
+
+        int move = 0;
+        for (int i = 0; i < orders.length; i++) {
+            if (orders[i] == 'L') move--;
+            else move++;
+        }
+
+        if (move == 0) System.out.print(str);
+        else if (move < 0) {
+            move *= -1;
+
+            str = str.substring(move).concat(str.substring(0, move));
+        } else {
+            str = str.substring(str.length() - move).concat(str.substring(0, move));
+        }
+
+        System.out.print(str);
+    }
+}
