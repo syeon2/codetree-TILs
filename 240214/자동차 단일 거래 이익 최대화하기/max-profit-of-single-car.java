829306da1b4a23f8a6000854bf3f21325c9d62f1
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-14 09:54:10 +0900

    Title: 자동차 단일 거래 이익 최대화하기, Time: 277ms, Memory: 12MB, Status: Passed - CodeTree

---
+++ 240214/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        int[] list = new int[N];
+        for (int i = 0; i < N; i++) {
+            list[i] = sc.nextInt();
+        }
+
+        int max = 0;
+
+        for (int i = 0; i < N; i++) {
+
+            for (int k = i + 1; k < N; k++) {
+                if (list[k] - list[i] > max) max = list[k] - list[i];
+            }
+        }
+
+        System.out.print(max);
+    }
+}
