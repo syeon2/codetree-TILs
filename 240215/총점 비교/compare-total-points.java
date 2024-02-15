2d01ee624a35ea42e55ea3a372273b35b7d72f94
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-15 14:53:00 +0900

    Title: 총점 비교, Time: 217ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240215/총점 비교/compare-total-points.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        Student[] list = new Student[N];
+
+        for (int i = 0; i < N; i++) {
+            list[i] = new Student(sc.next(), sc.nextInt(), sc.nextInt(), sc.nextInt());
+        }
+
+        Arrays.sort(list);
+
+        for (int i = 0; i < N; i++) {
+            list[i].print();
+        }
+    }
+
+    public static class Student implements Comparable<Student> {
+        public String name;
+        public int a;
+        public int b;
+        public int c;
+
+        public Student(String name, int a, int b, int c) {
+            this.name = name;
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+
+        public void print() {
+            System.out.printf("%s %d %d %d\n", this.name, this.a, this.b, this.c);
+        }
+
+        @Override
+        public int compareTo(Student student) {
+            return (this.a + this.b + this.c) - (student.a + student.b + student.c);
+        }
+    }
+}
