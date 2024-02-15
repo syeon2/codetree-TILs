3c1832a001242b9e67cba74df84eb46c6e4fbf99
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-15 18:08:13 +0900

    Title: 왔다 갔던 구역 2, Time: 134ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240215/왔다 갔던 구역 2/area-been-to-and-from2.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        int[] plusList = new int[1001];
+        int[] minusList = new int[1001];
+
+        int curPos = 0;
+
+        for (int n = 0; n < N; n++) {
+            int move = sc.nextInt();
+            char direc = sc.next().charAt(0);
+
+            if (move < 0) {
+                for (int i = curPos - 1; i >= curPos + move; i--) {
+                    if (i >= 0) plusList[i]++;
+                    else minusList[Math.abs(i)]++;
+                }
+            } else {
+                for (int i = curPos + 1; i <= curPos + move; i++) {
+                    if (i >= 0) plusList[i]++;
+                    else minusList[Math.abs(i)]++;
+                }
+            }
+        }
+
+        int cnt = 0;
+        for (int i = 0; i < plusList.length; i++) {
+            if (plusList[i] >= 2) cnt++;
+            if (minusList[i] >= 2) cnt++;
+        }
+
+        System.out.print(cnt);
+    }
+}
