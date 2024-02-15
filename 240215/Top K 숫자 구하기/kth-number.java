4dff559ec9fc40fd3d8690472b9edcc6112b9d9e
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-15 13:09:56 +0900

    Title: Top K 숫자 구하기, Time: 178ms, Memory: 12MB, Status: Passed - CodeTree

---
+++ 240215/Top K 숫자 구하기/kth-number.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int k = sc.nextInt();
+
+        int[] list = new int[N];
+        for (int i = 0; i < list.length; i++) {
+            list[i] = sc.nextInt();
+        }
+
+        Arrays.sort(list);
+
+        System.out.print(list[k - 1]);
+    }
+}
