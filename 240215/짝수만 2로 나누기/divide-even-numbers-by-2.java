f953076b1c2a8eb9109961ea0ea3dacd1db3737d
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-15 10:10:53 +0900

    Title: 짝수만 2로 나누기, Time: 171ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240215/짝수만 2로 나누기/divide-even-numbers-by-2.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        int[] list = new int[N];
+        for (int i = 0; i < N; i++) {
+            list[i] = sc.nextInt();
+        }
+
+        func(list);
+
+        for (int i = 0; i < list.length; i++) {
+            System.out.printf("%d ", list[i]);
+        }
+    }
+
+    public static void func(int[] list) {
+        for (int i = 0; i < list.length; i++) {
+            if (list[i] % 2 == 0) list[i] /= 2;
+        }
+    }
+}
