df4abc2776a592c5e6d6deba9b21127435d39b31
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-16 09:17:48 +0900

    Title: 2진수로 변환하기, Time: 137ms, Memory: 10MB, Status: Passed - CodeTree

--- 240216/2진수로 변환하기/convert-to-binary.java
+++ 240216/2진수로 변환하기/convert-to-binary.java
@@ -7,17 +7,20 @@ public class Main {
 
         int n = sc.nextInt();
 
-        StringBuilder sb = new StringBuilder();
+        if (n == 0) System.out.print(0);
+        else {
+            StringBuilder sb = new StringBuilder();
 
-        while (n > 1) {
-            if (n % 2 == 0) sb.insert(0, "0");
-            else sb.insert(0, "1");
+            while (n > 1) {
+                if (n % 2 == 0) sb.insert(0, "0");
+                else sb.insert(0, "1");
 
-            n /= 2;
+                n /= 2;
+            }
+
+            if (n == 1) sb.insert(0, "1");
+
+            System.out.print(sb);
         }
-
-        if (n == 1) sb.insert(0, "1");
-
-        System.out.print(sb);
     }
 }
