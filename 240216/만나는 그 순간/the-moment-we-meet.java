e2d5ccab38d7aacacaf9a29506973af3c3fd6086
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-16 13:15:41 +0900

    Title: 만나는 그 순간, Time: 149ms, Memory: 17MB, Status: Wrong Answer - CodeTree

---
+++ 240216/만나는 그 순간/the-moment-we-meet.java
@@ -0,0 +1,65 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static int[] nList = new int[1000001];
+    public static int[] mList = new int[1000001];
+
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+
+        int nSecond = 1;
+        for (int n = 0; n < N; n++) {
+            char direc = sc.next().charAt(0);
+            int move = sc.nextInt();
+
+            if (direc == 'L') {
+                while (move-- > 0) {
+                    nList[nSecond] = nList[nSecond - 1] - 1;
+
+                    nSecond++;
+                }
+            } else {
+                while (move-- > 0) {
+                    nList[nSecond] = nList[nSecond - 1] + 1;
+
+                    nSecond++;
+                }
+            }
+        }
+
+        int mSecond = 1;
+        for (int m = 0; m < M; m++) {
+            char direc = sc.next().charAt(0);
+            int move = sc.nextInt();
+
+            if (direc == 'L') {
+                while (move-- > 0) {
+                    mList[mSecond] = mList[mSecond - 1] - 1;
+
+                    mSecond++;
+                }
+            } else {
+                while (move-- > 0) {
+                    mList[mSecond] = mList[mSecond - 1] + 1;
+
+                    mSecond++;
+                }
+            }
+        }
+
+        int ans = -1;
+        for (int i = 1; i <= mSecond; i++) {
+            if (nList[i] == mList[i]) {
+                ans = i;
+                break;
+            }
+        }
+
+        System.out.print(ans);
+    }
+}
