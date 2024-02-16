fad94bebe40374a1abd49fded4180f9267c8a11b
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-16 10:20:22 +0900

    Title: 왔다 갔던 구역 2, Time: 141ms, Memory: 10MB, Status: Passed - CodeTree

--- 240216/왔다 갔던 구역 2/area-been-to-and-from2.java
+++ 240216/왔다 갔던 구역 2/area-been-to-and-from2.java
@@ -2,54 +2,41 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static int[] start;
-    public static int[] end;
-
     public static int OFFSET = 1000;
-    public static int[] checked = new int[2001];
 
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
 
         int N = sc.nextInt();
+        int[] section = new int[2001];
 
-        start = new int[N];
-        end = new int[N];
-
-        int curPos = 0;
+        int curPos = OFFSET;
 
         for (int n = 0; n < N; n++) {
             int move = sc.nextInt();
             char direc = sc.next().charAt(0);
 
             if (direc == 'L') {
-                start[n] = curPos - move;
-                end[n] = curPos;
+                for (int i = curPos - 1; i >= curPos - move; i--) {
+                    section[i]++;
+                }
 
                 curPos -= move;
             } else {
-                start[n] = curPos;
-                end[n] = curPos + move;
+                for (int i = curPos; i < curPos + move; i++) {
+                    section[i]++;
+                }
 
                 curPos += move;
             }
         }
 
-        for (int i = 0; i < N; i++) {
-            int s = start[i];
-            int e = end[i];
-
-            for (int k = s; k < e; k++) {
-                checked[k + OFFSET]++;
-            }
-        }
-
         int cnt = 0;
-        for (int i = 0; i < checked.length; i++) {
-            if (checked[i] >= 2) cnt++;
+        for (int i = 0; i < section.length; i++) {
+            if (section[i] >= 2) cnt++;
         }
 
-        System.out.println(cnt);
+        System.out.print(cnt);
     }
 }
