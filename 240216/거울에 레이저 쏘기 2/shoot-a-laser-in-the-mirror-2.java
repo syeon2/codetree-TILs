38ca691c7ff76c5e0e35441b308d695061d100c3
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-16 17:15:58 +0900

    Title: 거울에 레이저 쏘기 2, Time: 343ms, Memory: 20MB, Status: Wrong Answer - CodeTree

---
+++ 240216/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.java
@@ -0,0 +1,85 @@
+import java.util.Scanner;
+
+public class Main {
+
+    // R, D, L, U
+    public static int[] dx = {1, 0, -1, 0};
+    public static int[] dy = {0, 1, 0, -1};
+
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        char[][] list = new char[N][N];
+
+        for (int n = 0; n < N; n++) {
+            char[] mirrors = sc.next().toCharArray();
+
+            for (int m = 0; m < mirrors.length; m++) {
+                list[n][m] = mirrors[m];
+            }
+        }
+
+        int start = sc.nextInt();
+
+        // 시작점 세팅
+        int cnt = 1;
+        int curDir = 0;
+
+        int curX = 0;
+        int curY = 0;
+
+        while (--start > 0) {
+            int nx = curX + dx[curDir];
+            int ny = curY = dy[curDir];
+
+            if (isRange(N, nx, ny)) {
+                curX = nx;
+                curY = ny;
+            } else {
+                curDir = curDir + 1;
+            }
+        }
+
+        if (curDir == 0) curDir = 1;
+        else if (curDir == 1) curDir = 2;
+        else if (curDir == 2) curDir = 3;
+        else curDir = 0;
+
+        curDir = getTurnDir(curDir, list[curY][curX]);
+
+        // 현재 상하좌우 중 시작점에서 방으로 들어갔을 경우 변경될 수 있는 방향
+        while (true) {
+            int nx = curX + dx[curDir];
+            int ny = curY + dy[curDir];
+
+            if (!isRange(N, nx, ny)) break;
+
+            curX = nx;
+            curY = ny;
+            curDir = getTurnDir(curDir, list[curY][curX]);
+            cnt++;
+        }
+
+        System.out.print(cnt);
+    }
+
+    public static int getTurnDir(int dir, char mirror) {
+        if (dir == 1 && mirror == '\\') return 0;
+        else if (dir == 1 && mirror == '/') return 2;
+        else if (dir == 2 && mirror == '\\') return 1;
+        else if (dir == 2 && mirror == '/') return 3;
+        else if (dir == 3 && mirror == '\\') return 2;
+        else if (dir == 3 && mirror == '/') return 0;
+        else if (dir == 0 && mirror == '\\') return 1;
+        else return 3;
+    }
+
+    public static boolean isRange(int N, int x, int y) {
+        if (x >= 0 && x < N && y >= 0 && y < N) return true;
+
+        return false;
+    }
+}
