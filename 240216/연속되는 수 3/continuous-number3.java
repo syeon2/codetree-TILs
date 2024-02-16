d33e287bd472db4d2a6bdbc4d1f33288cc593652
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-16 12:25:40 +0900

    Title: 연속되는 수 3, Time: 290ms, Memory: 11MB, Status: Passed - CodeTree

---
+++ 240216/연속되는 수 3/continuous-number3.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        int cnt = 1;
+        int max = 1;
+
+        int curNum = sc.nextInt();
+
+        for (int n = 0; n < N - 1; n++) {
+            int num = sc.nextInt();
+
+            if ((curNum > 0 && num > 0) || (curNum < 0 && num < 0)) {
+                cnt++;
+            } else {
+                max = Math.max(max, cnt);
+                cnt = 1;
+            }
+
+            curNum = num;
+        }
+
+        max = Math.max(max, cnt);
+
+        System.out.print(max);
+    }
+}
