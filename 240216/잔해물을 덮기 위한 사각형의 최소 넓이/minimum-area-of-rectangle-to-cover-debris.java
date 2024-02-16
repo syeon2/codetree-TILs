0ba346fa45f488e1f54a010cd709bedc0fbc4901
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-16 12:00:03 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 189ms, Memory: 26MB, Status: Wrong Answer - CodeTree

---
+++ 240216/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int[][] board = new int[2001][2001];
+        int offset = 1000;
+
+        int x1 = sc.nextInt();
+        int y1 = sc.nextInt();
+
+        int x2 = sc.nextInt();
+        int y2 = sc.nextInt();   
+
+        for (int i = Math.min(x1, x2); i < Math.max(x1, x2); i++) {
+
+            for (int k = Math.min(y1, y2); k < Math.max(y1, y2); k++) {
+                board[k + offset][i + offset]++;
+            }
+        }
+
+        int x3 = sc.nextInt();
+        int y3 = sc.nextInt();
+
+        int x4 = sc.nextInt();
+        int y4 = sc.nextInt();
+
+        for (int i = Math.min(x3, x4); i < Math.max(x3, x4); i++) {
+            for (int k = Math.min(y3, y4); k < Math.max(y3, y4); k++) {
+                board[k + offset][i + offset]--;
+            }
+        }
+
+        int minX = 2001;
+        int minY = 2001;
+        int maxX = -1;
+        int maxY = -1;
+
+        for (int i = 0; i < board.length; i++) {
+
+            for (int k = 0; k < board[i].length; k++) {
+                if (board[i][k] >= 1) {
+                    if (i < minY) minY = i;
+                    if (i > maxY) maxY = i;
+                    if (k < minX) minX = k;
+                    if (k > maxX) maxX = k;
+                }
+            }
+        }
+
+        int subX = maxX - minX;
+        int subY = maxY - minY;
+
+        System.out.print((subX + 1) * (subY + 1));
+    }
+}
