a3090ed6f611eb98dd7ce91c54388bcddddbdc34
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-17 10:24:28 +0900

    Title: 원 모양으로 되어있는 방, Time: 243ms, Memory: 13MB, Status: Passed - CodeTree

---
+++ 240217/원 모양으로 되어있는 방/a-room-in-a-circle.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int[] list = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            list[i] = sc.nextInt();
+        }
+
+        int min = Integer.MAX_VALUE;
+
+        for (int i = 0; i < N; i++) {
+
+            int temp = 0;
+            for (int k = i; k < N + i; k++) {
+                int idx = (N + k) % N;
+
+                temp += (k - i) * list[idx];
+            }
+
+            if (min > temp) min = temp;
+        }
+
+        System.out.print(min);
+    }
+}
