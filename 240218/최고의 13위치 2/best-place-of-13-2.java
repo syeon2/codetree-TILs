33f459443d34e2d873f01e8c98284b82adeb6daf
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-18 21:14:03 +0900

    Title: 최고의 13위치 2, Time: 133ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240218/최고의 13위치 2/best-place-of-13-2.java
+++ 240218/최고의 13위치 2/best-place-of-13-2.java
@@ -7,7 +7,6 @@ public class Main {
 
         int N = sc.nextInt();
         int[][] board = new int[N][N];
-
         for (int i = 0; i < N; i++) {
             for (int k = 0; k < N; k++) {
                 board[i][k] = sc.nextInt();
@@ -15,38 +14,21 @@ public class Main {
         }
 
         int ans = 0;
-        for (int i = 0; i < N; i++) {
+        // 중첩되는지 확인
+        for (int y1 = 0; y1 < N; y1++) {
+            for (int x1 = 0; x1 < N - 2; x1++) {
 
-            for (int k = 0; k < N - 2; k++) {
-
-                for (int y = 0; y < N; y++) {
-
-                    for (int x = 0; x < N - 2; x++) {
-                        if (i == k) {
-                            if (k == x || k + 1 == x || k + 2 == x || k == x + 2 || k + 1 == x + 2 || k + 2 == x + 2) continue;
+                for (int y2 = 0; y2 < N; y2++) {
+                    for (int x2 = 0; x2 < N - 2; x2++) {
+                        if (y1 == y2) {
+                            if (x1 == x2 || x1 == x2 + 1 || x1 == x2 + 2 || x2 + 2 == x1 || x2 + 1 == x1) continue;
                             else {
-                                int temp = 0;
+                                int temp = board[y1][x1] + board[y1][x1 + 1] + board[y1][x1 + 2] + board[y2][x2] + board[y2][x2 + 1] + board[y2][x2 + 2];
 
-                                for (int a = k; a < k + 3; a++) {
-                                    temp += board[i][a];
-                                }
-
-                                for (int a = x; a < x + 3; a++) {
-                                    temp += board[i][a];
-                                }
-
-                                ans = Math.max(ans, temp);
+                                ans = Math.max(ans, temp);    
                             }
                         } else {
-                            int temp = 0;
-
-                            for (int a = k; a < k + 3; a++) {
-                                temp += board[i][a];
-                            }
-
-                            for (int a = x; a < x + 3; a++) {
-                                temp += board[y][a];
-                            }
+                            int temp = board[y1][x1] + board[y1][x1 + 1] + board[y1][x1 + 2] + board[y2][x2] + board[y2][x2 + 1] + board[y2][x2 + 2];
 
                             ans = Math.max(ans, temp);
                         }
