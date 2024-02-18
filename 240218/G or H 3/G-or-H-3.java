528be6389f274d19565904e6e68b87d4da7e3d96
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-18 21:33:37 +0900

    Title: G or H 3, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240218/G or H 3/G-or-H-3.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+
+        int[] people = new int[101];
+        for (int n = 0; n < N; n++) {
+            int person = sc.nextInt();
+            char card = sc.next().charAt(0);
+
+            if (card == 'G') people[person] += 1;
+            else people[person] += 2;
+        }
+
+        int max = 0;
+        for (int i = 1; i <= 100 - K; i++) {
+
+            int temp = 0;
+            for (int k = i; k <= i + K; k++) {
+                temp += people[k];
+            }
+
+            max = Math.max(max, temp);
+        }
+
+        System.out.print(max);
+    }
+}
