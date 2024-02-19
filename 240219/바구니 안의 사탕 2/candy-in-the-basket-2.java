d4d65778ac6fd5311bdfe325d3c3b63abd0d722f
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-19 13:28:01 +0900

    Title: 바구니 안의 사탕 2, Time: 153ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240219/바구니 안의 사탕 2/candy-in-the-basket-2.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+
+        int[] line = new int[101];
+        for (int n = 0; n < N; n++) {
+            int candy = sc.nextInt();
+            int pos = sc.nextInt();
+
+            line[pos] = candy;
+        }
+
+        int ans = 0;
+        for (int i = K + 1; i <= 100 - K; i++) {
+
+            int temp = 0;
+            for (int k = i - K; k <= i + K; k++) {
+                temp += line[k];
+            }
+
+            ans = Math.max(ans, temp);
+        }
+
+        System.out.print(ans);
+    }
+}
