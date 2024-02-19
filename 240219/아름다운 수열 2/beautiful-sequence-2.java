9ca34fb88fc810ba061aebebac281099aac92721
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-19 13:15:11 +0900

    Title: 아름다운 수열 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240219/아름다운 수열 2/beautiful-sequence-2.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+
+        int[] nList = new int[N];
+        for (int i = 0; i < N; i++) {
+            nList[i] = sc.nextInt();
+        }
+
+        int[] mList = new int[M];
+        for (int i = 0; i < M; i++) {
+            mList[i] = sc.nextInt();
+        }
+
+        int ans = 0;
+        for (int i = 0; i <= N - M; i++) {
+            int[] cnt = new int[10];
+
+            for (int k = i; k < i + M; k++) {
+                int idx = nList[k];
+
+                cnt[idx]++;
+            }
+
+            boolean flag = true;
+            for (int k = 0; k < M; k++) {
+                int idx = mList[k];
+
+                cnt[idx]--;
+
+                if (cnt[idx] < 0) {
+                    flag = false;
+                    break;
+                }
+            }
+
+            if (flag) ans++;
+        }
+
+        System.out.print(ans);
+    }
+}
