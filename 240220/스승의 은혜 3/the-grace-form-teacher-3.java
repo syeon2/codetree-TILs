0541ba970799458e9e7cdf7b0010f4b4c621a9f8
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-20 22:53:13 +0900

    Title: 스승의 은혜 3, Time: 409ms, Memory: 18MB, Status: Passed - CodeTree

--- 240220/스승의 은혜 3/the-grace-form-teacher-3.java
+++ 240220/스승의 은혜 3/the-grace-form-teacher-3.java
@@ -16,7 +16,7 @@ public class Main {
 
 		Arrays.sort(list, (a, b) -> {
 			if ((a[0] + a[1]) == (b[0] + b[1])) {
-				return b[0] - b[0];
+				return b[0] - a[0];
 			}
 
 			return (a[0] + a[1]) - (b[0] + b[1]);
