db3353063cbf8871a4108e017b6d132e3e0203cd
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-20 23:30:53 +0900

    Title: 등차수열, Time: 127ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240220/등차수열/arithmetic-sequence.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        int[] list = new int[N];
+        for (int i = 0; i < N; i++) {
+            list[i] = sc.nextInt();
+        }
+
+        int cnt = 0;
+        for (int i = 0; i < N; i++) {
+
+            for (int k = i + 1; k < N; k++) {
+                int sum = list[i] + list[k];
+
+                if (sum % 2 == 0) cnt++;
+            }
+        }
+
+        System.out.print(cnt);
+    }
+}
