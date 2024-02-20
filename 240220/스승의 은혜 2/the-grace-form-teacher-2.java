0bbe69a971b9cb22fcd17022469f971e2494e82b
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-20 18:06:30 +0900

    Title: 스승의 은혜 2, Time: 276ms, Memory: 12MB, Status: Passed - CodeTree

---
+++ 240220/스승의 은혜 2/the-grace-form-teacher-2.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int B = sc.nextInt();
+
+        int[] list = new int[N];
+        for (int i = 0; i < N; i++) {
+            list[i] = sc.nextInt();
+        }
+        
+        Arrays.sort(list);
+
+        int ans = 0;
+        for (int i = 0; i < N; i++) {
+
+            int temp = B;
+            int cnt = 0;
+
+            for (int k = 0; k < N; k++) {
+                if (i == k) temp -= (list[k] / 2);
+                else temp -= list[k];
+
+                if (temp < 0) break;
+                else cnt++;
+            }
+
+            ans = Math.max(ans, cnt);
+        }
+
+        System.out.print(ans);
+    }
+}
