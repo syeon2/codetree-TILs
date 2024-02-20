67ed15d2393d8d740bfd47b96a138c1723802c30
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-20 21:44:24 +0900

    Title: 상해버린 치즈, Time: 144ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240220/상해버린 치즈/rotten-cheese.java
+++ 240220/상해버린 치즈/rotten-cheese.java
@@ -1,63 +1,71 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        // 여기에 코드를 작성해주세요.
-        Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) {
+		// 여기에 코드를 작성해주세요.
+		Scanner sc = new Scanner(System.in);
 
-        int N = sc.nextInt();
-        int M = sc.nextInt();
-        int D = sc.nextInt();
-        int S = sc.nextInt();
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+		int D = sc.nextInt();
+		int S = sc.nextInt();
 
-        // N의 idx : 사람 번호, 101은 초(t), 각 값은 치즈 번호
-        int[][] personEatTimeACheese = new int[N + 1][101];
+		// N의 idx : 사람 번호, 101은 초(t), 각 값은 치즈 번호
+		ArrayList[][] personEatTimeACheese = new ArrayList[N + 1][101];
 
-        // 먹었는지 확인하는 사람 먹었으면 true
-        boolean[] people = new boolean[N + 1];
+		// 상한 치즈인지 확인 상했으면 true
+		boolean[] cheeses = new boolean[M + 1];
 
-        // 상한 치즈인지 확인 상했으면 true
-        boolean[] cheeses = new boolean[M + 1];
+		int[] eatingCheese = new int[M + 1];
 
-        for (int d = 0; d < D; d++) {
-            int pers = sc.nextInt();
-            int chee = sc.nextInt();
-            int sec = sc.nextInt();
+		for (int d = 0; d < D; d++) {
+			int pers = sc.nextInt();
+			int chee = sc.nextInt();
+			int sec = sc.nextInt();
 
-            personEatTimeACheese[pers][sec] = chee;
-        }
+            if (personEatTimeACheese[pers][sec] == null) {
+                personEatTimeACheese[pers][sec] = new ArrayList<>();
 
-        for (int s = 0; s < S; s++) {
-            int pers = sc.nextInt();
-            int illTime = sc.nextInt();
+                personEatTimeACheese[pers][sec].add(chee);
+            } else personEatTimeACheese[pers][sec].add(chee);
+		}
 
-            for (int i = 1; i < illTime; i++) {
-                if (personEatTimeACheese[pers][i] > 0) {
-                    int badChee = personEatTimeACheese[pers][i];
+		for (int s = 0; s < S; s++) {
+			int pers = sc.nextInt();
+			int illTime = sc.nextInt();
 
-                    cheeses[badChee] = true;
-                }
-            }
-        }
+			for (int i = 1; i < illTime; i++) {
+				if (personEatTimeACheese[pers][i] != null && !personEatTimeACheese[pers][i].isEmpty()) {
+					List<Integer> badCheese = personEatTimeACheese[pers][i];
 
-        for (int c = 1; c <= M; c++) {
-            if (cheeses[c]) {
+					for (Integer integer : badCheese) {
+						cheeses[integer] = true;
+					}
+				}
+			}
+		}
 
-                for (int n = 1; n <= N; n++) {
-                    for (int t = 1; t <= 100; t++) {
-                        if (personEatTimeACheese[n][t] == c) {
-                            people[n] = true;
-                        }
-                    }
-                }
-            }
-        }
+		int ans = 0;
+		for (int c = 1; c <= M; c++) {
+			if (cheeses[c]) {
 
-        int cnt = 0;
-        for (int i = 1; i <= N; i++) {
-            if (people[i]) cnt++;
-        }
+				int cnt = 0;
+				for (int n = 1; n <= N; n++) {
 
-        System.out.print(cnt);
-    }
+					for (int t = 1; t <= 100; t++) {
+						if (personEatTimeACheese[n][t] != null && personEatTimeACheese[n][t].contains(c)) {
+							cnt++;
+							break;
+						}
+					}
+				}
+
+				ans = Math.max(ans, cnt);
+			}
+		}
+
+		System.out.print(ans);
+	}
 }
