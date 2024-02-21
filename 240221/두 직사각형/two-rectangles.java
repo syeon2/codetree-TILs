34c3d771ffdb73f3b22b1c9e6076d90d5f2685a3
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-21 15:08:04 +0900

    Title: 두 직사각형, Time: 138ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240221/두 직사각형/two-rectangles.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int x1 = sc.nextInt();
+        int y1 = sc.nextInt();
+        int x2 = sc.nextInt();
+        int y2 = sc.nextInt();
+
+        int a1 = sc.nextInt();
+        int b1 = sc.nextInt();
+        int a2 = sc.nextInt();
+        int b2 = sc.nextInt();
+
+        if ((x1 < a1 && x2 < a1) || (y1 < b1 && y2 < b1) || (a1 < x1 && a2 < x1) || (b1 < y1 && b2 < y1)) System.out.print("nonoverlapping");
+        else System.out.print("overlapping");
+    }
+}
