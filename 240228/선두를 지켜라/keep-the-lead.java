9a506b67832d3c02455f6651a6b7d965d481bad4
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-28 16:24:04 +0900

    Title: 선두를 지켜라, Time: 152ms, Memory: 17MB, Status: Wrong Answer - CodeTree

---
+++ 240228/선두를 지켜라/keep-the-lead.java
@@ -0,0 +1,63 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+
+        int[] aRecord = new int[1000001];
+        int[] bRecord = new int[1000001];
+
+        int aSec = 1;
+        int bSec = 1;
+
+        for (int n = 0; n < N; n++) {
+            int v = sc.nextInt();
+            int t = sc.nextInt();
+
+            for (int i = 0; i < t; i++) {
+                aRecord[aSec] = aRecord[aSec - 1] + v;
+                aSec++;
+            }
+        }
+
+        for (int m = 0; m < M; m++) {
+            int v = sc.nextInt();
+            int t = sc.nextInt();
+
+            for (int i = 0; i < t; i++) {
+                bRecord[bSec] = bRecord[bSec - 1] + v;
+                bSec++;
+            }
+        }
+
+        boolean isAWin = true;
+        int cnt = -1;
+
+        for (int i = 1; i < aSec; i++) {
+            if (cnt == -1) {
+                if (aRecord[i] < bRecord[i]) {
+                    isAWin = false;
+                    cnt++;
+                }
+                else if (bRecord[i] > aRecord[i]) {
+                    isAWin = true;
+                    cnt++;
+                }
+            } else {
+                if (aRecord[i] < bRecord[i] && isAWin) {
+                    isAWin = false;
+                    cnt++;
+                } else if (aRecord[i] > bRecord[i] && !isAWin) {
+                    isAWin = true;
+                    cnt++;
+                }
+            }
+        }
+
+        System.out.print(cnt);
+    }
+}
