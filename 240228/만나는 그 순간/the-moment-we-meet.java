eec4a0ee22ca96a07123518d1adc30413b298940
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-28 16:08:34 +0900

    Title: 만나는 그 순간, Time: 242ms, Memory: 29MB, Status: Passed - CodeTree

--- 240228/만나는 그 순간/the-moment-we-meet.java
+++ 240228/만나는 그 순간/the-moment-we-meet.java
@@ -5,8 +5,8 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
 
-        int[] aTime = new int[1001];
-        int[] bTime = new int[1001];
+        int[] aTime = new int[1000001];
+        int[] bTime = new int[1000001];
 
         int N = sc.nextInt();
         int M = sc.nextInt();
