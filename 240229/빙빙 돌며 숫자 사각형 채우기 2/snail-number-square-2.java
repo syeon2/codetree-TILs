3a6c61965aab3a7681cf4afb82b36894da76bcec
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-29 08:15:03 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기 2, Time: 255ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240229/빙빙 돌며 숫자 사각형 채우기 2/snail-number-square-2.java
@@ -0,0 +1,56 @@
+import java.util.*;
+
+public class Main {
+
+    // D, R, U, L
+    public static int[] dx = {0, 1, 0, -1};
+    public static int[] dy = {1, 0, -1, 0};
+
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+
+        int[][] board = new int[N][M];
+
+        int num = 1;
+
+        int curX = 0;
+        int curY = 0;
+        board[0][0] = num++;
+
+        int curDirec = 0;
+
+        while (num <= N * M) {
+            int nx = curX + dx[curDirec];
+            int ny = curY + dy[curDirec];
+
+            if (isRange(nx, ny, N, M) && board[ny][nx] == 0) {
+                board[ny][nx] = num++;
+
+                curX = nx;
+                curY = ny;
+            } else curDirec = turnDirec(curDirec);
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int k = 0; k < N; k++) {
+                System.out.printf("%d ", board[i][k]);
+            }
+
+            System.out.println();
+        }
+    }
+
+    public static int turnDirec(int direc) {
+        return (direc + 1) % 4;
+    }
+
+    public static boolean isRange(int x, int y, int N, int M) {
+        if (x >= 0 && x < M && y >= 0 && y < N) return true;
+
+        return false;
+    }
+}
