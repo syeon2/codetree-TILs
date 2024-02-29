def09fbc782817b3016689289d6254c48f9d1a69
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-29 10:41:27 +0900

    Title: 괄호 쌍 만들어주기 2, Time: 150ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240229/괄호 쌍 만들어주기 2/pair-parentheses-2.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        char[] list = sc.next().toCharArray();
+
+        int cnt = 0;
+        for (int i = 0; i < list.length - 3; i++) {
+            if (list[i] == '(' && list[i + 1] == '(') {
+
+                for (int k = i + 2; k < list.length - 1; k++) {
+                    if (list[k] == ')' && list[k + 1] == ')') cnt++;
+                }
+            }
+        }
+
+        System.out.print(cnt);
+    }
+}
