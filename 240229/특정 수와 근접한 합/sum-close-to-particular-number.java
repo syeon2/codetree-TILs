67c3735a433c1f3a397a81c36015536c0620c7d6
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-29 14:15:09 +0900

    Title: 특정 수와 근접한 합, Time: 167ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240229/특정 수와 근접한 합/sum-close-to-particular-number.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int S = sc.nextInt();
+
+        int[] list = new int[N];
+        for (int i = 0; i < N; i++) {
+            list[i] = sc.nextInt();
+        }
+
+        int ans = Integer.MAX_VALUE;
+        for (int i = 0; i < N; i++) {
+
+            for (int k = i + 1; k < N; k++) {
+                int sum = 0;
+
+                for (int j = 0; j < N; j++) {
+                    if (i == j || k == j) continue;
+
+                    sum += list[j];
+                }
+
+                if (ans > Math.abs(S - sum)) ans = Math.abs(S - sum);
+            }
+        }
+
+        System.out.print(ans);
+    }
+}
