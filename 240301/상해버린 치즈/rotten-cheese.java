58095fb10a764719807aaf87f5db2a07499afbb5
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-03-01 22:46:21 +0900

    Title: 상해버린 치즈, Time: 141ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240301/상해버린 치즈/rotten-cheese.java
@@ -0,0 +1,59 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        int D = sc.nextInt();
+        int S = sc.nextInt();
+
+        int[][] records = new int[D][3];
+        for (int i = 0; i < D; i++) {
+            int p = sc.nextInt();
+            int m = sc.nextInt();
+            int t = sc.nextInt();
+
+            records[i][0] = p;
+            records[i][1] = m;
+            records[i][2] = t;
+        }
+
+        int[][] illRecords = new int[S][2];
+        for (int i = 0; i < S; i++) {
+            int p = sc.nextInt();
+            int t = sc.nextInt();
+
+            illRecords[i][0] = p;
+            illRecords[i][1] = t;
+        }
+
+        int ans = 0;
+
+        for (int i = 0; i < S; i++) {
+
+            int person = illRecords[i][0];
+            int time = illRecords[i][1];
+
+            for (int k = 0; k < D; k++) {
+                int eatPerson = records[k][0];
+                int cheese = records[k][1];
+                int eatTime = records[k][2];
+
+                if (person == eatPerson && eatTime < time) {
+
+                    int cnt = 0;
+                    for (int j = 0; j < D; j++) {
+                        if (records[j][1] == cheese) cnt++;
+                    }
+
+                    ans = Math.max(ans, cnt);
+                }
+            }
+        }
+
+        System.out.print(ans);
+    }
+}
