a666b3af23d484966940e58d963fcee3bb8f59d9
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-03-01 22:17:33 +0900

    Title: 겹치지 않는 선분 2, Time: 165ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240301/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        int[][] list = new int[N][2];
+        for (int i = 0; i < N; i++) {
+            list[i][0] = sc.nextInt();
+            list[i][1] = sc.nextInt();
+        }
+
+        int ans = 0;
+        for (int i = 0; i < N; i++) {
+            boolean isCross = true;
+
+            int x1 = list[i][0];
+            int x2 = list[i][1];
+
+            for (int k = 0; k < N; k++) {
+                if (i == k) continue;
+
+                int x3 = list[k][0];
+                int x4 = list[k][1];
+
+                if (x1 < x3 && x2 < x4) continue;
+                else if (x1 > x3 && x2 > x4) continue;
+                else isCross = false;
+            }
+
+            if (isCross) ans++;
+        }
+
+        System.out.print(ans);
+    }
+}
