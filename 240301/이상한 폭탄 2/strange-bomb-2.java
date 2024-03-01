d50b7106483a80250f76f868125375f5a596d9a6
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-03-01 23:44:57 +0900

    Title: 이상한 폭탄 2, Time: 143ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240301/이상한 폭탄 2/strange-bomb-2.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+
+        int[] list = new int[N];
+        for (int i = 0; i < N; i++) {
+            list[i] = sc.nextInt();
+        }
+
+        int ans = -1;
+        for (int i = 0; i < N; i++) {
+
+            int start = Math.max(0, i - K);
+            int end = Math.min(N - 1, i + K);
+
+            int bomb = list[i];
+
+            for (int k = start; k <= end; k++) {
+                if (i == k) continue;
+                
+                if (list[k] == bomb) {
+                    ans = Math.max(ans, bomb);
+                }
+            }
+        }
+
+        System.out.print(ans);
+    }
+}
