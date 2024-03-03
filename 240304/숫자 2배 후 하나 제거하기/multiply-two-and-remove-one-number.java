f0dd3a7000afd143dbad9ca8e34d533f07afaedb
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-03-04 08:58:59 +0900

    Title: 숫자 2배 후 하나 제거하기, Time: 181ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240304/숫자 2배 후 하나 제거하기/multiply-two-and-remove-one-number.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int[] list = new int[N];
+        for (int i = 0; i < N; i++) {
+            list[i] = sc.nextInt();
+        }
+
+        int ans = Integer.MAX_VALUE;
+        for (int i = 0; i < N; i++) {
+            list[i] *= 2;
+
+            for (int k = 0; k < N; k++) {
+
+                int temp = 0;
+
+                for (int j = 1; j < N; j++) {
+                    if (j == k) continue;
+                    else if (j == k + 1) {
+                        if (k == 0) continue;
+                        else temp += Math.abs(list[j] - list[j - 2]);
+                    } else temp += Math.abs(list[j] - list[j - 1]);
+                }
+
+                ans = Math.min(ans, temp);
+            }
+
+            list[i] /= 2;
+        }
+
+        System.out.print(ans);
+    }
+}
