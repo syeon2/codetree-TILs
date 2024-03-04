714769d9dcf79d491b9603f63ce6173ef5c00572
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-03-04 09:11:22 +0900

    Title: 3개의 선 2, Time: 134ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240304/3개의 선 2/three-lines-2.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int[][] list = new int[N][2];
+
+        for (int i = 0; i < N; i++) {
+            list[i][0] = sc.nextInt();
+            list[i][1] = sc.nextInt();
+        }
+
+        int[] xLine = new int[11];
+        int[] yLine = new int[11];
+
+        for (int i = 0; i < N; i++) {
+            int x = list[i][0];
+            int y = list[i][1];
+
+            xLine[x]++;
+            yLine[y]++;
+        }
+
+        int xCnt = 0;
+        int yCnt = 0;
+        for (int i = 0; i <= 10; i++) {
+            if (xLine[i] >= 2) xCnt++;
+            if (yLine[i] >= 2) yCnt++;
+        }
+
+        if (xCnt + yCnt == 3) System.out.print(1);
+        else System.out.print(0);
+    }
+}
