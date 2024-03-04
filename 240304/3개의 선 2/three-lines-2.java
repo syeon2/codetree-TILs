07ec28e961e27eebc224d77cbc0abdc12e652912
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-03-04 09:53:46 +0900

    Title: 3개의 선 2, Time: 196ms, Memory: 14MB, Status: Passed - CodeTree

--- 240304/3개의 선 2/three-lines-2.java
+++ 240304/3개의 선 2/three-lines-2.java
@@ -6,32 +6,135 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         int N = sc.nextInt();
-        int[][] list = new int[N][2];
+        boolean[][] board = new boolean[11][11];
 
         for (int i = 0; i < N; i++) {
-            list[i][0] = sc.nextInt();
-            list[i][1] = sc.nextInt();
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+
+            board[y][x] = true;
         }
 
-        int[] xLine = new int[11];
-        int[] yLine = new int[11];
+        int ans = 0;
 
-        for (int i = 0; i < N; i++) {
-            int x = list[i][0];
-            int y = list[i][1];
+        // x <= 3 && y == 0
+        for (int i = 0; i < 11; i++) {
+            for (int k = 0; k < 11; k++) {
+                for (int l = 0; l < 11; l++) {
+                    boolean[][] copyBoard = copy(board);
 
-            xLine[x]++;
-            yLine[y]++;
+                    for (int y = 0; y < 11; y++) {
+                        copyBoard[y][i] = false;
+                        copyBoard[y][k] = false;
+                        copyBoard[y][l] = false;
+                    }
+
+                    boolean flag = true;
+
+                    for (int y = 0; y < 11; y++) {
+                        for (int x = 0; x < 11; x++) {
+                            if (copyBoard[y][x]) flag = false;
+                        }
+                    }
+
+                    if (flag) ans = 1;
+                }
+            }
         }
 
-        int xCnt = 0;
-        int yCnt = 0;
-        for (int i = 0; i <= 10; i++) {
-            if (xLine[i] >= 2) xCnt++;
-            if (yLine[i] >= 2) yCnt++;
+        // x == 2 && y == 1
+
+        for (int i = 0; i < 11; i++) {
+            for (int k = 0; k < 11; k++) {
+                for (int l = 0; l < 11; l++) {
+                    boolean[][] copyBoard = copy(board);
+
+                    for (int y = 0; y < 11; y++) {
+                        copyBoard[y][i] = false;
+                        copyBoard[y][k] = false;
+                    }
+
+                    for (int x = 0; x < 11; x++) {
+                        copyBoard[l][x] = false;
+                    }
+
+                    boolean flag = true;
+
+                    for (int y = 0; y < 11; y++) {
+                        for (int x = 0; x < 11; x++) {
+                            if (copyBoard[y][x]) flag = false;
+                        }
+                    }
+
+                    if (flag) ans = 1;
+                }
+            }
         }
 
-        if (xCnt == 3 || yCnt == 3 || (xCnt < 3 && yCnt < 3 && xCnt + yCnt == 3)) System.out.print(1);
-        else System.out.print(0);
+        // x == 1 && y == 2
+        for (int i = 0; i < 11; i++) {
+            for (int k = 0; k < 11; k++) {
+                for (int j = 0; j < 11; j++) {
+                    boolean[][] copyBoard = copy(board);
+
+                    for (int y = 0; y < 11; y++) {
+                        copyBoard[y][i] = false;
+                    }
+
+                    for (int x = 0; x < 11; x++) {
+                        copyBoard[k][x] = false;
+                        copyBoard[j][x] = false;
+                    }
+
+                    boolean flag = true;
+
+                    for (int y = 0; y < 11; y++) {
+                        for (int x = 0; x < 11; x++) {
+                            if (copyBoard[y][x]) flag = false;
+                        }
+                    }
+
+                    if (flag) ans = 1;
+                }
+            }
+        }
+
+        for (int i = 0; i < 11; i++) {
+            for (int k = 0; k < 11; k++) {
+                for (int l = 0; l < 11; l++) {
+                    boolean[][] copyBoard = copy(board);
+
+                    for (int x = 0; x < 11; x++) {
+                        copyBoard[i][x] = false;
+                        copyBoard[k][x] = false;
+                        copyBoard[l][x] = false;
+                    }
+
+                    boolean flag = true;
+
+                    for (int y = 0; y < 11; y++) {
+                        for (int x = 0; x < 11; x++) {
+                            if (copyBoard[y][x]) flag = false;
+                        }
+                    }
+
+                    if (flag) ans = 1;
+                }
+            }
+        }
+
+        System.out.print(ans);
+    }
+
+    public static boolean[][] copy(boolean[][] origin) {
+        boolean[][] tempBoard = new boolean[11][11];
+
+        for (int i = 0; i < 11; i++) {
+            for (int k = 0; k < 11; k++) {
+                tempBoard[i][k] = origin[i][k];
+            }
+        }
+
+        return tempBoard;
     }
 }
