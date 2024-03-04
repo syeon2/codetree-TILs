3f112617d772bf10a100998338658b0b5349fee4
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-03-04 20:42:18 +0900

    Title: 훌륭한 점프, Time: 122ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240304/훌륭한 점프/great-jump.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+
+        int[] list = new int[N];
+        for (int i = 0; i < N; i++) {
+            list[i] = sc.nextInt();
+        }
+
+        int ans = N;
+
+        for (int i = N; i >= 1; i--) {
+            boolean flag = true;
+            int cnt = 0;
+
+            for (int k = 0; k < N; k++) {
+                if (list[k] > i) cnt++;
+                else cnt = 0;
+
+                if (cnt == K) {
+                    flag = false;
+                    break;
+                }
+            }
+
+            if (flag) ans = i;
+        }
+
+        System.out.print(ans);
+    }
+}
