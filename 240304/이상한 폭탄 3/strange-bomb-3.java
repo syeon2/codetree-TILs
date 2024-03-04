df13b41fe3bd831b529e835c5d664b33093611e2
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-03-04 22:05:01 +0900

    Title: 이상한 폭탄 3, Time: 140ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240304/이상한 폭탄 3/strange-bomb-3.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+
+        int[] list = new int[N];
+        for (int i = 0; i < N; i++) {
+            list[i] = sc.nextInt();
+        }
+
+        int ans = 0;
+        int max = 0;
+
+        int[] bomb = new int[1000001];
+
+        for (int i = 0; i < N; i++) {
+
+            int start = Math.max(0, i - K);
+            int end = Math.min(N - 1, i + K);
+
+            for (int k = start; k <= end; k++) {
+                if (k == i) continue;
+
+                if (list[k] == list[i]) {
+                    int b = list[i];
+
+                    bomb[b]++;
+                    break;
+                }
+            }
+        }
+
+        for (int i = 0; i < bomb.length; i++) {
+            if (max < bomb[i]) {
+                max = bomb[i];
+                ans = i;
+            }
+        }
+
+        System.out.print(ans);
+    }
+}
