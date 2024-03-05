f4d8232d7685f414d354b7083086e0a307d6ebb5
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-03-05 18:42:09 +0900

    Title: 코딩톡, Time: 131ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240305/코딩톡/coding-talk.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        int P = sc.nextInt();
+
+        char[] personList = new char[M + 1];
+        int[] readList = new int[M + 1];
+
+        for (int i = 1; i <= M; i++) {
+            char c = sc.next().charAt(0);
+            int u = sc.nextInt();
+
+            personList[i] = c;
+            readList[i] = u;
+        }
+
+        // A ~ n
+        boolean[] readPerson = new boolean[N];
+
+        for (int i = P; i <= M; i++) {
+            char person = personList[i];
+
+            int idx = person - 'A';
+
+            readPerson[idx] = true;
+        }
+
+        for (int i = 0; i < readPerson.length; i++) {
+            if (!readPerson[i]) {
+                System.out.printf("%s ", (char) ((char) i + 'A'));
+            }
+        }
+    }
+}
