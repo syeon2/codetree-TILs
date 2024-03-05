ae135ba134b87241f3b39325d5ab7bd27397e611
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-03-05 19:00:42 +0900

    Title: 코딩톡, Time: 174ms, Memory: 10MB, Status: Passed - CodeTree

--- 240305/코딩톡/coding-talk.java
+++ 240305/코딩톡/coding-talk.java
@@ -23,6 +23,25 @@ public class Main {
         // A ~ n
         boolean[] readPerson = new boolean[N];
 
+        for (int i = 1; i <= P; i++) {
+            if (readList[i] == 0) {
+                Arrays.fill(readPerson, true);
+            } else {
+                if (readList[i - 1] == readList[i]) {
+                    int idx1 = personList[i - 1] - 'A';
+                    int idx2 = personList[i] - 'A';
+
+                    readPerson[idx1] = true;
+                    readPerson[idx2] = true;
+                } else {
+                    Arrays.fill(readPerson, false);
+                    int idx2 = personList[i] - 'A';
+
+                    readPerson[idx2] = true;
+                }
+            }
+        }
+
         for (int i = P; i <= M; i++) {
             char person = personList[i];
 
