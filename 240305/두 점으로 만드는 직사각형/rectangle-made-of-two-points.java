0c9177091dc76bbf6ab051fae50f68251b175bf8
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-03-05 10:33:37 +0900

    Title: 두 점으로 만드는 직사각형, Time: 145ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240305/두 점으로 만드는 직사각형/rectangle-made-of-two-points.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int x1 = sc.nextInt();
+        int y1 = sc.nextInt();
+        int x2 = sc.nextInt();
+        int y2 = sc.nextInt();
+
+        int a1 = sc.nextInt();
+        int b1 = sc.nextInt();
+        int a2 = sc.nextInt();
+        int b2 = sc.nextInt();
+
+        int rightX = Math.min(x1, a1);
+        int rightY = Math.min(y1, b1);
+
+        int leftX = Math.max(x2, a2);
+        int leftY = Math.max(y2, b2);
+
+        System.out.print((leftX - rightX) * (leftY - rightY));
+    }
+}
