0f2d413e05fa0e5de1b978918f08feb53449c871
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-03-05 16:46:21 +0900

    Title: 비둘기와 전기줄, Time: 150ms, Memory: 10MB, Status: Passed - CodeTree

--- 240305/비둘기와 전기줄/pigeons-and-electric-cords.java
+++ 240305/비둘기와 전기줄/pigeons-and-electric-cords.java
@@ -6,7 +6,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         int N = sc.nextInt();
-        int[] list = new int[N + 1];
+        int[] list = new int[11];
         Arrays.fill(list, -1);
 
         int ans = 0;
