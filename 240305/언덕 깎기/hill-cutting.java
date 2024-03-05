cfebeffb9e59c8d919e706429fa333e6a4ce833a
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-03-05 15:17:46 +0900

    Title: 언덕 깎기, Time: 2968ms, Memory: 13MB, Status: Passed - CodeTree

--- 240305/언덕 깎기/hill-cutting.java
+++ 240305/언덕 깎기/hill-cutting.java
@@ -1,53 +1,94 @@
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        // 여기에 코드를 작성해주세요.
-        Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) {
+		// 여기에 코드를 작성해주세요.
+		Scanner sc = new Scanner(System.in);
 
-        int N = sc.nextInt();
-        int[] list = new int[N];
-        for (int i = 0; i < N; i++) {
-            list[i] = sc.nextInt();
-        }
+		int N = sc.nextInt();
+		int[] list = new int[N];
+		for (int i = 0; i < N; i++) {
+			list[i] = sc.nextInt();
+		}
 
-        Arrays.sort(list);
+		Arrays.sort(list);
 
-        int max = list[N - 1];
-        int min = list[0];
+		int max = list[N - 1];
+		int min = list[0];
 
-        if (max - min <= 17) System.out.print(0);
-        else {
-            int ans = Integer.MAX_VALUE;
+		if (max - min <= 17) System.out.print(0);
+		else {
+			int sub = max - min - 17;
 
-            for (int i = 0; i < N; i++) {
-                for (int k = 0; k < N; k++) {
-                    if (i == k) continue;
+			if (sub % 2 == 0) {
+				int downCut = min + (sub / 2);
+				int upCut = max - (sub / 2);
 
-                    max = list[i];
-                    min = list[k];
+				int tempSum = 0;
+				for (int i = 0; i < N; i++) {
+					if (list[i] < downCut) {
+						tempSum += (downCut - list[i]) * (downCut - list[i]);
+					}
 
-                    int sub = max - min - 17;
+					if (list[i] > upCut) {
+						tempSum += (list[i] - upCut) * (list[i] - upCut);
+					}
+				}
 
-                    int downCut = min + (sub / 2);
-                    int upCut = max - (sub / 2);
+				System.out.print(tempSum);
+			} else {
+				int ansMin = Integer.MAX_VALUE;
 
-                    int tempSum = 0;
-                    for (int j = 0; j < N; j++) {
-                        if (list[j] < downCut) {
-                            tempSum += (downCut - list[j]) * (downCut - list[j]);
-                        }
+				for (int a = 0; a < N; a++) {
+					for (int b = 0; b < N; b++) {
+						if (a == b) continue;
 
-                        if (list[j] > upCut) {
-                            tempSum += (list[j] - upCut) * (list[j] - upCut);
-                        }
-                    }
+						min = list[a];
+						max = list[b];
 
-                    ans = Math.min(ans, tempSum);
-                }
-            }
+						sub = max - min - 17;
 
-            System.out.print(ans);
-        }
-    }
+						if (max < min) continue;
+
+						int downCut = min + (sub / 2);
+						int upCut = max - (sub / 2);
+
+						// case1
+						downCut += 1;
+						int case1Sum = 0;
+						for (int i = 0; i < N; i++) {
+							if (list[i] < downCut) {
+								case1Sum += (downCut - list[i]) * (downCut - list[i]);
+							}
+
+							if (list[i] > upCut) {
+								case1Sum += (list[i] - upCut) * (list[i] - upCut);
+							}
+						}
+
+						// case2
+						downCut -=1;
+						upCut -= 1;
+
+						int case2Sum = 0;
+						for (int i = 0; i < N; i++) {
+							if (list[i] < downCut) {
+								case2Sum += (downCut - list[i]) * (downCut - list[i]);
+							}
+
+							if (list[i] > upCut) {
+								case2Sum += (list[i] - upCut) * (list[i] - upCut);
+							}
+						}
+
+						upCut += 1;
+
+						ansMin = Math.min(ansMin, Math.min(case1Sum, case2Sum));
+					}
+				}
+
+				System.out.print(ansMin);
+			}
+		}
+	}
 }
