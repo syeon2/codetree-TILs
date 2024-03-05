91fa2e885a869ec653fc36174241b4f8230eb44c
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-03-06 08:21:26 +0900

    Title: 독서실의 거리두기 2, Time: 151ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240306/독서실의 거리두기 2/study-cafe-keeping-distance-2.java
@@ -0,0 +1,54 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        String str = sc.next();
+
+        int[] list = new int[N];
+        for (int i = 0; i < N; i++) {
+            list[i] = str.charAt(i) - '0';
+        }
+
+        int prev = -1;
+        int maxGap = 0;
+        int minGap = Integer.MAX_VALUE;
+
+        for (int i = 0; i < N; i++) {
+            if (list[i] == 1) {
+                if (prev == -1) prev = i;
+                else {
+                    int gap = i - prev;
+
+                    maxGap = Math.max(maxGap, gap);
+                    minGap = Math.min(minGap, gap);
+
+                    prev = i;
+                }
+            }
+        }
+
+        int firstGap = 0;
+        int lastGap = 0;
+
+        for (int i = 0; i < N; i++) {
+            if (list[i] == 0) firstGap++;
+            else break;
+        }
+
+        for (int i = N - 1; i >= 0; i--) {
+            if (list[i] == 0) lastGap++;
+            else break;
+        }
+
+        if (firstGap < minGap) firstGap = minGap;
+        if (lastGap < minGap) lastGap = minGap;
+
+        int ans = Math.min(maxGap / 2, Math.min(minGap, Math.min(firstGap, lastGap)));
+
+        System.out.print(ans);
+    }
+}
