3f0c5c0a984d7f18ffa20d14caa676f890165459
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-03-05 10:29:35 +0900

    Title: A, B, C 찾기, Time: 157ms, Memory: 10MB, Status: Passed - CodeTree

--- 240305/A, B, C 찾기/finding-a-b-c.java
+++ 240305/A, B, C 찾기/finding-a-b-c.java
@@ -13,41 +13,11 @@ public class Main {
 
         Arrays.sort(list);
 
-        int ansA = 0;
-        int ansB = 0;
-        int ansC = 0;
+        int A = list[0];
+        int B = list[1];
 
-        for (int i = 0; i < N; i++) {
-            for (int k = i + 1; k < N; k++) {
-                for (int j = k + 1; j < N; j++) {
-                    int a = list[i];
-                    int b = list[k];
-                    int c = list[j];
+        int ABC = list[N - 1];
 
-                    int[] newList = {a, b, c, a + b, b + c, c + a, a + b + c};
-
-                    Arrays.sort(newList);
-
-                    boolean flag = true;
-
-                    for (int x = 0; x < N; x++) {
-                        if (list[x] != newList[x]) flag = false;
-                    }
-
-                    if (flag) {
-                        ansA = list[i];
-                        ansB = list[k];
-                        ansC = list[j];
-                        break;
-                    }
-                }
-
-                if (ansA != 0) break;
-            }
-
-            if (ansA != 0) break;
-        }
-
-        System.out.printf("%d %d %d", ansA, ansB, ansC);
+        System.out.printf("%d %d %d", A, B, ABC - (A + B));
     }
 }
