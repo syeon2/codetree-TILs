9b839589cd5fee0f0f2e5fc513bb9dcac8cb6759
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-03-06 10:52:21 +0900

    Title: 수열의 순서 바꾸기, Time: 137ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240306/수열의 순서 바꾸기/reorder-sequence.java
+++ 240306/수열의 순서 바꾸기/reorder-sequence.java
@@ -1,55 +1,57 @@
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        // 여기에 코드를 작성해주세요.
-        Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) {
+		// 여기에 코드를 작성해주세요.
+		Scanner sc = new Scanner(System.in);
 
-        int N = sc.nextInt();
-        int[] list = new int[N + 1];
+		int N = sc.nextInt();
+		int[] list = new int[N];
 
-        for (int i = 1; i <= N; i++) {
-            list[i] = sc.nextInt();
-        }
+		for (int i = 0; i < N; i++) {
+			list[i] = sc.nextInt();
+		}
 
-        int ans = 0;
-        while (true) {
-            int[] copy = new int[N + 1];
-            for (int i = 1; i <= N; i++) {
-                copy[i] = list[i];
-            }
+		int ans = 0;
 
-            boolean flag = true;
+		while (true) {
+			boolean flag = true;
 
-            for (int i = 1; i <= N; i++) {
-                if (copy[i - 1] > copy[i]) flag = false;
-            }
+			for (int i = 0; i < N - 1; i++) {
+				if (list[i] > list[i + 1]) flag = false;
+			}
 
-            if (flag) break;
+			if (flag) break;
 
-            int curNum = copy[1];
+			int curNum = list[0];
 
-            int changeIdx = N + 1;
-            
-            for (int i = 1; i <= N; i++) {
-                if (copy[i] > curNum && copy[i] != i) {
-                    changeIdx = i;
-                }
-            }
+			int maxNum = 0;
+			int maxIdx = 0;
 
-            for (int i = 2; i < changeIdx; i++) {
-                copy[i - 1] = copy[i];
-            }
+			for (int i = 0; i < N; i++) {
+				if (maxNum < list[i]) {
+					maxNum = list[i];
+					maxIdx = i;
+				}
+			}
 
-            copy[changeIdx - 1] = curNum;
+			int changeIdx = maxIdx;
 
-            for (int i = 1; i <= N; i++) {
-                list[i] = copy[i];
-            }
+			for (int i = N - 1; i >= maxIdx; i--) {
+				if (curNum > list[i]) {
+					changeIdx = i;
+					break;
+				}
+			}
 
-            ans++;
-        }
+			for (int i = 1; i <= changeIdx; i++) {
+				list[i - 1] = list[i];
+			}
 
-        System.out.print(ans);
-    }
+			list[changeIdx] = curNum;
+			ans++;
+		}
+
+		System.out.print(ans);
+	}
 }
