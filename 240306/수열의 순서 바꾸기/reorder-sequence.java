55993fff2c26cbb4c622605cd7fef2a9ec482b72
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-03-06 10:13:57 +0900

    Title: 수열의 순서 바꾸기, Time: 156ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240306/수열의 순서 바꾸기/reorder-sequence.java
@@ -0,0 +1,65 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		// 여기에 코드를 작성해주세요.
+		Scanner sc = new Scanner(System.in);
+
+		int N = sc.nextInt();
+		int[] list = new int[N + 1];
+		for (int i = 1; i <= N; i++) {
+			list[i] = sc.nextInt();
+		}
+
+		int ans = 0;
+
+		while (true) {
+			int[] copy = new int[N + 1];
+			for (int i = 1; i <= N; i++) {
+				copy[i] = list[i];
+			}
+
+			int maxIdx = -1;
+			int maxNum = 0;
+
+			for (int i = 1; i <= N; i++) {
+				if (list[i] == i) continue;
+
+				if (maxNum < list[i]) {
+					maxNum = list[i];
+					maxIdx = i;
+				}
+			}
+
+			for (int i = maxIdx + 1; i <= N; i++) {
+				if (list[1] > list[i]) maxIdx = i;
+				else break;
+			}
+
+			int temp = copy[1];
+
+			for (int i = 2; i <= maxIdx; i++) {
+				copy[i - 1] = copy[i];
+			}
+
+			copy[maxIdx] = temp;
+
+			ans++;
+
+			boolean flag = true;
+
+			for (int i = 1; i < N; i++) {
+				if (copy[i] >= copy[i + 1]) flag = false;
+			}
+
+			if (flag) break;
+			else {
+				for (int i = 1; i <= N; i++) {
+					list[i] = copy[i];
+				}
+			}
+		}
+
+		System.out.print(ans);
+	}
+}
