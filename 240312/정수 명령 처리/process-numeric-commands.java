3bfd7ce97c3fac2cee280ebe31c6eeed88b21f94
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-03-12 22:13:50 +0900

    Title: 정수 명령 처리, Time: 292ms, Memory: 18MB, Status: Passed - CodeTree

--- 240312/정수 명령 처리/process-numeric-commands.java
+++ 240312/정수 명령 처리/process-numeric-commands.java
@@ -26,7 +26,7 @@ public class Main {
                 if (stack.isEmpty()) sb.append(1).append("\n");
                 else sb.append(0).append("\n");
             } else {
-                sb.append(stack.peek());
+                sb.append(stack.peek()).append("\n");
             }
         }
 
