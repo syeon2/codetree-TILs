bb36e2fbf5834174230e9db92d19a88822f75ec5
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-03-16 16:38:14 +0900

    Title: 정수 명령 처리 8, Time: 344ms, Memory: 21MB, Status: Wrong Answer - CodeTree

---
+++ 240316/정수 명령 처리 8/process-numeric-commands-8.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+
+        int N = sc.nextInt();
+
+        LinkedList<Integer> list = new LinkedList<>();
+
+        for (int i = 0; i < N; i++) {
+            String cmd = sc.next();
+
+            if (cmd.equals("push_front")) {
+                int num = sc.nextInt();
+
+                list.addFirst(num);
+            } else if (cmd.equals("push_back")) {
+                int num = sc.nextInt();
+
+                list.addLast(num);
+            } else if (cmd.equals("pop_front")) {
+                sb.append(list.pollFirst()).append("\n");
+            } else if (cmd.equals("pop_back")) {
+                sb.append(list.pollLast()).append("\n");
+            } else if (cmd.equals("size")) {
+                sb.append(list.size()).append("\n");
+            } else if (cmd.equals("empty")) {
+                if (cmd.isEmpty()) sb.append(1).append("\n");
+                else sb.append(0).append("\n");
+            } else if (cmd.equals("front")) {
+                sb.append(list.get(0)).append("\n");
+            } else {
+                sb.append(list.get(list.size() - 1)).append("\n");
+            }
+        }
+
+        System.out.print(sb);
+    }
+}
