c64ea6ce77a7003a8499afc33bb684fcc5ecad4a
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-03-16 16:44:29 +0900

    Title: 정수 명령 처리 8, Time: 469ms, Memory: 22MB, Status: Passed - CodeTree

--- 240316/정수 명령 처리 8/process-numeric-commands-8.java
+++ 240316/정수 명령 처리 8/process-numeric-commands-8.java
@@ -10,26 +10,30 @@ public class Main {
         LinkedList<Integer> list = new LinkedList<>();
 
         for (int i = 0; i < N; i++) {
-            String cmd = sc.next();
+            String str = sc.next();
 
-            if (cmd.equals("push_front")) {
+            if (str.equals("push_front")) {
                 int num = sc.nextInt();
 
                 list.addFirst(num);
-            } else if (cmd.equals("push_back")) {
+            } else if (str.equals("push_back")) {
                 int num = sc.nextInt();
 
                 list.addLast(num);
-            } else if (cmd.equals("pop_front")) {
-                System.out.println(list.pollFirst());
-            } else if (cmd.equals("pop_back")) {
-                System.out.println(list.pollLast());
-            } else if (cmd.equals("size")) {
+            } else if (str.equals("pop_front")) {
+                int num = list.pollFirst();
+
+                System.out.println(num);
+            } else if (str.equals("pop_back")) {
+                int num = list.pollLast();
+
+                System.out.println(num);
+            } else if (str.equals("size")) {
                 System.out.println(list.size());
-            } else if (cmd.equals("empty")) {
-                if (cmd.isEmpty()) System.out.println(1);
+            } else if (str.equals("empty")) {
+                if (list.isEmpty()) System.out.println(1);
                 else System.out.println(0);
-            } else if (cmd.equals("front")) {
+            } else if (str.equals("front")) {
                 System.out.println(list.peekFirst());
             } else {
                 System.out.println(list.peekLast());
