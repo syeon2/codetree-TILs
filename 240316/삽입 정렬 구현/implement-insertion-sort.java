a1f1a9a71a5b2f4359616daaca79ae59b3f06bd5
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-03-16 20:25:18 +0900

    Title: 삽입 정렬 구현, Time: 310ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 240316/삽입 정렬 구현/implement-insertion-sort.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 1; i < N; i++) {
+            int j = i - 1;
+            int key = arr[i];
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+
+            arr[j + 1] = key;
+        }
+
+        Arrays.stream(arr)
+            .forEach(n -> System.out.printf("%d ", n));
+    }
+}
