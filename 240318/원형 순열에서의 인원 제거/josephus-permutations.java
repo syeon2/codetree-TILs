b4d6dfa0c75904b0d42b2db9b0ec14d9bf52893a
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-03-18 18:05:25 +0900

    Title: 원형 순열에서의 인원 제거, Time: 130ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240318/원형 순열에서의 인원 제거/josephus-permutations.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        Queue<Integer> que = new LinkedList<>();
+
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+
+        for (int i = 1; i <= N; i++) {
+            que.add(i);
+        }
+
+        while (que.size() != 1) {
+            for (int i = 1; i < K; i++) {
+                que.add(que.remove());
+            }
+
+            System.out.printf("%d ", que.remove());
+        }
+    }
+}
