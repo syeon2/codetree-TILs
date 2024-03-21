f8b3cca0f4be141bbc72f810b7cb56212aca270a
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-03-22 08:50:59 +0900

    Title: 최고의 13위치 2, Time: 124ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240322/최고의 13위치 2/best-place-of-13-2.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int[][] board = new int[N][N];
+
+        for (int i = 0; i < N; i++) {
+            for (int k = 0; k < N; k++) {
+                board[i][k] = sc.nextInt();
+            }
+        }
+
+        int ans = 0;
+        for (int i = 0; i < N; i++) {
+            for (int k = 0; k < N - 2; k++) {
+
+                for (int j = 0; j < N; j++) {
+                    for (int l = 0; l < N - 2; l++) {
+                        if (i == j) {
+                            if ((k >= l && k <= l + 2) || (k + 1 >= l && k + 1 <= l + 2) || (k + 2 >= l && k + 2 <= l + 2)) continue;
+                            if ((l >= k && l <= k + 2) || (l + 1 >= k && l + 1 <= k + 2) || (l + 2 >= k && l + 2 <= k + 2)) continue;
+                        }
+
+                        int sum = 0;
+
+                        for (int n = 0; n < k + 3; n++) {
+                            sum += board[i][n];
+                        }
+
+                        for (int n = 0; n < l + 3; n++) {
+                            sum += board[j][n];
+                        }
+
+                        ans = Math.max(ans, sum);
+                    }
+                }
+            }
+        }
+
+        System.out.print(ans);
+    }
+}
