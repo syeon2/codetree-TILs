c22cb224fd3df1e9480baf24cd53fb454aa6fe7b
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-03-22 01:01:49 +0900

    Title: 원 모양으로 되어있는 방, Time: 237ms, Memory: 13MB, Status: Passed - CodeTree

--- 240322/원 모양으로 되어있는 방/a-room-in-a-circle.java
+++ 240322/원 모양으로 되어있는 방/a-room-in-a-circle.java
@@ -7,26 +7,22 @@ public class Main {
 
         int N = sc.nextInt();
 
-        int total = 0;
         int[] list = new int[N];
         for (int i = 0; i < N; i++) {
             list[i] = sc.nextInt();
-            total += list[i];
         }
 
         int ans = Integer.MAX_VALUE;
         for (int i = 0; i < N; i++) {
-            int tempTotal = total;
-            int tempMove = 0;
+            int temp = 0;
 
-            for (int k = i; k < N + i; k++) {
-                int idx = k % N;
+            for (int k = 0; k < N; k++) {
+                int room = (k + i) % N;
 
-                tempTotal -= list[idx];
-                tempMove += tempTotal;
+                temp += k * list[room];
             }
 
-            ans = Math.min(ans, tempMove);
+            ans = Math.min(ans, temp);
         }
 
         System.out.print(ans);
