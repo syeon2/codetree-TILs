a634ffd19161d91a82a9d20ad06f720698284cf2
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-03-21 13:46:35 +0900

    Title: 최대로 겹치는 지점, Time: 148ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240321/최대로 겹치는 지점/maximum-overlapped-points.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int[] arr = new int[101];
+
+        for (int n = 0; n < N; n++) {
+            int x1 = sc.nextInt();
+            int x2 = sc.nextInt();
+
+            for (int i = x1; i <= x2; i++) {
+                arr[i]++;
+            }
+        }
+
+        int ans = 0;
+        for (int i = 0; i < arr.length; i++) {
+            ans = Math.max(ans, arr[i]);
+        }
+
+        System.out.print(ans);
+    }
+}
