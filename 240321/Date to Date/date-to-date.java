52e9df8fe651ee91a4a13c7d7e991cc774ab14c4
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-03-21 12:19:26 +0900

    Title: Date to Date, Time: 148ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240321/Date to Date/date-to-date.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Main {
+
+    public static int[] month = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int m1 = sc.nextInt();
+        int d1 = sc.nextInt();
+
+        int m2 = sc.nextInt();
+        int d2 = sc.nextInt();
+
+        int ans = 0;
+
+        for (int i = m1 + 1; i < m2; i++) {
+            ans += month[i];
+        }
+
+        ans += month[m1] - d1 + 1;
+        ans += d2;
+
+        System.out.print(ans);
+    }
+}
