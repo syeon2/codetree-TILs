e6a2df9fdeb41238a2fc77d4c20a5f492de8b966
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-03-22 01:09:18 +0900

    Title: 씨 오 더블유 2, Time: 151ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240322/씨 오 더블유 2/c-o-w-2.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        char[] list = sc.next().toCharArray();
+
+        int ans = 0;
+        for (int i = 0; i < N; i++) {
+            if (list[i] != 'C') continue;
+
+            for (int k = i + 1; k < N; k++) {
+                if (list[k] != 'O') continue;
+
+                for (int j = k + 1; j < N; j++) {
+                    if (list[j] == 'W') ans++;
+                }
+            }
+        }
+
+        System.out.print(ans);
+    }
+}
