32afa4269b0565243fee5efb46584cc360da1b4a
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-03-21 12:13:44 +0900

    Title: Time to Time, Time: 162ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240321/Time to Time/time-to-time.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+
+        int start = (60 * a) + b;
+        int end = (60 * c) + d;
+
+        if (start > end) System.out.print(start - end);
+        else System.out.print(end - start);
+    }
+}
