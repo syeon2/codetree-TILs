f7d73dddfa26af40afc62b095919f149a04d8d96
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-03-22 09:27:06 +0900

    Title: 구간 중 최대 합, Time: 141ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240322/구간 중 최대 합/max-sum-of-subarray.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+
+        int[] list = new int[N];
+        for (int i = 0; i < N; i++) {
+            list[i] = sc.nextInt();
+        }
+
+        int ans = 0;
+        for (int i = 0; i <= N - K; i++) {
+            int temp = 0;
+
+            for (int k = i; k < i + K; k++) {
+                temp += list[k];
+            }
+
+            ans = Math.max(ans, temp);
+        }
+
+        System.out.print(ans);
+    }
+}
