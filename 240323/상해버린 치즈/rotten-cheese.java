d35f0c61d850df87b55cfec1bd46ba7126d3c8ff
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-03-23 23:24:21 +0900

    Title: 상해버린 치즈, Time: 141ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240323/상해버린 치즈/rotten-cheese.java
+++ 240323/상해버린 치즈/rotten-cheese.java
@@ -5,61 +5,60 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
 
-        int N = sc.nextInt(); // people
-        int M = sc.nextInt(); // cheese
-        int D = sc.nextInt(); // eatRecords
-        int S = sc.nextInt(); // illRecords
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        int D = sc.nextInt();
+        int S = sc.nextInt();
 
-        boolean[] people = new boolean[N + 1];
-        int[] cheeses = new int[M + 1];
+        boolean[][] peopleCheese = new boolean[N + 1][M + 1]; // p, m
+        boolean[] cheeses = new boolean[M + 1];
         int[][] eatRecords = new int[D][3]; // p, m, t
         int[][] illRecords = new int[S][2]; // p, t
 
-        for (int d = 0; d < D; d++) {
-            int p = sc.nextInt();
-            int m = sc.nextInt();
-            int t = sc.nextInt();
-
-            eatRecords[d][0] = p;
-            eatRecords[d][1] = m;
-            eatRecords[d][2] = t;
+        for (int i = 0; i < D; i++) {
+            eatRecords[i][0] = sc.nextInt();
+            eatRecords[i][1] = sc.nextInt();
+            eatRecords[i][2] = sc.nextInt();
         }
 
-        for (int s = 0; s < S; s++) {
-            int p = sc.nextInt();
-            int t = sc.nextInt();
-
-            illRecords[s][0] = p;
-            illRecords[s][1] = t;
+        for (int i = 0; i < S; i++) {
+            illRecords[i][0] = sc.nextInt();
+            illRecords[i][1] = sc.nextInt();
         }
 
-        for (int s = 0; s < S; s++) {
-            int sickPerson = illRecords[s][0];
-            int sickSeconds = illRecords[s][1];
+        for (int i = 0; i < S; i++) {
+            int illPerson = illRecords[i][0];
+            int illSeconds = illRecords[i][1];
 
-            for (int d = 0; d < D; d++) {
-                int eatPerson = eatRecords[d][0];
-                int eatenCheese = eatRecords[d][1];
-                int eatSeconds = eatRecords[d][2];
+            for (int k = 0; k < D; k++) {
+                int eatPerson = eatRecords[k][0];
+                int eatenCheese = eatRecords[k][1];
+                int eatSeconds = eatRecords[k][2];
 
-                if (eatPerson == sickPerson && sickSeconds > eatSeconds) cheeses[eatenCheese]++;
+                if (eatPerson == illPerson && illSeconds > eatSeconds) cheeses[eatenCheese] = true;
             }
         }
 
-        for (int i = 1; i < cheeses.length; i++) {
-            if (cheeses[i] != S) continue;
+        for (int i = 1; i <= M; i++) {
+            if (!cheeses[i]) continue;
 
-            for (int d = 0; d < D; d++) {
-                int eatPerson = eatRecords[d][0];
-                int eatenCheese = eatRecords[d][1];
+            for (int k = 0; k < D; k++) {
+                int eatPerson = eatRecords[k][0];
+                int eatenCheese = eatRecords[k][1];
 
-                if (eatenCheese == i) people[eatPerson] = true;
+                if (i == eatenCheese) peopleCheese[eatPerson][eatenCheese] = true;
             }
         }
 
         int ans = 0;
-        for (int i = 1; i <= N; i++) {
-            if (people[i]) ans++;
+        for (int i = 1; i <= M; i++) {
+            int cnt = 0;
+            
+            for (int k = 1; k <= N; k++) {
+                if (peopleCheese[k][i]) cnt++;
+            }
+
+            ans = Math.max(ans, cnt);
         }
 
         System.out.print(ans);
