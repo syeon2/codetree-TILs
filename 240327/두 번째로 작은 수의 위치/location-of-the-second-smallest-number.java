76ee3257fc5fab76cd662f4adeed9b987cd42d12
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-03-27 01:11:10 +0900

    Title: 두 번째로 작은 수의 위치, Time: 122ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240327/두 번째로 작은 수의 위치/location-of-the-second-smallest-number.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int[] list = new int[N];
+        for (int i = 0; i < N; i++) {
+            list[i] = sc.nextInt();
+        }
+
+        int first = Integer.MAX_VALUE;
+        int second = Integer.MAX_VALUE;
+
+        for (int i = 0; i < N; i++) {
+            if (list[i] < first) {
+                second = first;
+                first = list[i];
+            } else if (second > list[i]) {
+                second = list[i];
+            }
+        }
+
+        if (second == Integer.MAX_VALUE) System.out.print(-1);
+        else {
+            int cnt = 0;
+            int idx = 0;
+
+            for (int i = 0; i < N; i++) {
+                if (list[i] == second) {
+                    cnt++;
+                    idx = i;
+                }
+            }
+
+            if (cnt > 1) System.out.print(-1);
+            else System.out.print(idx + 1);
+        }
+    }
+}
