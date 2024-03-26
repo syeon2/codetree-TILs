fe43097f01460f081fbd454cd8661447aaa5d7b5
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-03-26 17:40:44 +0900

    Title: 순 간 이 동, Time: 142ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240326/순 간 이 동/teleportation.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int X = sc.nextInt();
+        int Y = sc.nextInt();
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int case1 = Y - X;
+        int case2 = Math.abs(X - a) + Math.abs(b - Y);
+        int case3 = Math.abs(X - b) + Math.abs(a - Y);
+
+        int ans = Math.min(case1, Math.min(case2, case3));
+
+        System.out.print(ans);
+    }
+}
