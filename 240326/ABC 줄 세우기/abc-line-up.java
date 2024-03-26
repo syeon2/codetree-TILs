40ada3cadbc506209b71716f1b0989d4f9f546cd
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-03-26 18:59:02 +0900

    Title: ABC 줄 세우기, Time: 120ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240326/ABC 줄 세우기/abc-line-up.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        char[] list = new char[N];
+        for (int i = 0; i < N; i++) {
+            list[i] = sc.next().charAt(0);
+        }
+
+        int ans = 0;
+
+        for (int i = 0; i < N; i++) {
+            char c = (char) (i + 'A');
+
+            if (list[i] == c) continue;
+            else {
+                for (int k = i + 1; k < N; k++) {
+                    if (list[k] == c) {
+                        char temp = list[i];
+                        list[i] = list[k];
+                        list[k] = temp;
+                        ans++;
+                        break;
+                    }
+                }
+            }
+        }
+
+        System.out.print(ans);
+    }
+}
