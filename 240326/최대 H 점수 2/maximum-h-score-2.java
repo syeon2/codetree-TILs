0505e3457a683080f5c1312f927b9d564e41fb32
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-03-26 12:28:22 +0900

    Title: 최대 H 점수 2, Time: 146ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240326/최대 H 점수 2/maximum-h-score-2.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int L = sc.nextInt();
+
+        int[] list = new int[N];
+        for (int i = 0; i < N; i++) {
+            list[i] = sc.nextInt();
+        }
+
+        Arrays.sort(list);
+
+        int ans = 0;
+        for (int i = 0; i < N; i++) {
+            int[] temp = new int[N];
+            for (int k = 0; k < N; k++) {
+                temp[k] = list[k];
+            }
+
+            int pivot = temp[i];
+            int cnt = 0;
+
+            int tempL = L;
+
+            for (int k = N - 1; k >= 0; k--) {
+                if (pivot > temp[k] && tempL != 0) {
+                    tempL -= 1;
+                    temp[k] += 1;
+                }
+            }
+
+            for (int k = 0; k < N; k++) {
+                if (pivot <= temp[k]) cnt++;
+            }
+
+            if (pivot <= cnt) ans = Math.max(ans, pivot);
+        }
+
+        System.out.print(ans);
+    }
+}
