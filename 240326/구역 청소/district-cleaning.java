4a645241c3b81b6efb5961cde23560e8d9aa5c5f
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-03-26 15:59:21 +0900

    Title: 구역 청소, Time: 124ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240326/구역 청소/district-cleaning.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+
+        if ((a < c && b < d) || (c < a && d < a)) {
+            int total = 0;
+            total += (b - a) + (d - c);
+
+            System.out.print(total);
+        } else {
+            int total = 0;
+
+            if (a < c) {
+                total += (b - a) + (d - c) - (c - b);
+            } else {
+                total += (b - a) + (d - c) - (d - a);
+            }
+
+            System.out.print(total);
+        }
+    }
+}
