6d2254922d0b6f32bc07ce183bd0055e9b5dd2e1
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-03-26 17:19:13 +0900

    Title: 코딩톡, Time: 140ms, Memory: 10MB, Status: Passed - CodeTree

--- 240326/코딩톡/coding-talk.java
+++ 240326/코딩톡/coding-talk.java
@@ -9,24 +9,22 @@ public class Main {
         int M = sc.nextInt();
         int P = sc.nextInt();
 
-        char[] cList = new char[N];
-        int[] uList = new int[N];
-        for (int i = 0; i < N; i++) {
+        char[] cList = new char[M + 1];
+        int[] uList = new int[M + 1];
+        for (int i = 1; i <= M; i++) {
             cList[i] = sc.next().charAt(0);
             uList[i] = sc.nextInt();
         }
 
         boolean[] people = new boolean[N];
 
-        for (int i = 0; i < N; i++) {
-            int order = i + 1;
-
+        for (int order = 1; order <= M; order++) {
             if (order < P) continue;
             else if (order == P) {
-                if (uList[i] == 0) Arrays.fill(people, true);
+                if (uList[order] == 0) Arrays.fill(people, true);
                 else {
-                    for (int k = i; k >= 0; k--) {
-                        if (uList[i] == uList[k]) {
+                    for (int k = order; k >= 0; k--) {
+                        if (uList[order] == uList[k]) {
                             int personIdx = cList[k] - 'A';
 
                             people[personIdx] = true;
@@ -34,11 +32,9 @@ public class Main {
                     }
                 }
             } else {
-                if (uList[i] == 0) Arrays.fill(people, true);
-                else {
-                    int personIdx = cList[i] - 'A';
-                    people[personIdx] = true;
-                }
+                int personIdx = cList[order] - 'A';
+
+                people[personIdx] = true;
             }
         }
 
