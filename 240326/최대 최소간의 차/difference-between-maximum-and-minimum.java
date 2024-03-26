5958938347de8e03442edd1fe3ca83ebc0fd98ab
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-03-26 12:39:57 +0900

    Title: 최대 최소간의 차, Time: 131ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240326/최대 최소간의 차/difference-between-maximum-and-minimum.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+        int[] list = new int[N];
+        for (int i = 0; i < N; i++) {
+            list[i] = sc.nextInt();
+        }
+
+        int ans = Integer.MAX_VALUE;
+
+        for (int i = 1; i <= N; i++) {
+            int totalCost = 0;
+
+            for (int k = 0; k < N; k++) {
+                int num = list[k];
+
+                if (i <= num && num <= i + K) continue;
+                else if (num < i) totalCost += (i - num);
+                else if (num > (i + K)) totalCost += (num - (i + K));
+            }
+
+            ans = Math.min(ans, totalCost);
+        }
+
+        System.out.print(ans);
+    }
+}
