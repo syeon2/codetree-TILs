2ad279cb298b9315e0abc9276bda03ee14104c0b
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-03-26 18:22:39 +0900

    Title: 연속된 숫자 만들기 3, Time: 130ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240326/연속된 숫자 만들기 3/create-consecutive-numbers-3.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        int[] list = {a, b, c};
+        Arrays.sort(list);
+
+        if (a + 1 == b && b + 1 == c) System.out.print(1);
+        else {
+            int fGap = b - a;
+            int sGap = c - b;
+
+            System.out.print(Math.max(fGap, sGap) - 1);
+        }
+    }
+}
