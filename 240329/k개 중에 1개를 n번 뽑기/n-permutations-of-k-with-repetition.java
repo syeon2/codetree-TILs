ad644a66f9c3eb86378843d6bec0f2913ea81a78
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-03-29 16:52:33 +0900

    Title: k개 중에 1개를 n번 뽑기, Time: 3060ms, Memory: 80MB, Status: Time Limit Exceed - CodeTree

--- 240329/k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition.java
+++ 240329/k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition.java
@@ -1,31 +1,35 @@
 import java.util.*;
 
 public class Main {
+
+    public static List<Integer> list = new ArrayList<>();
+    public static int K;
+    public static int N;
+
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
 
-        int N = sc.nextInt();
-        int K = sc.nextInt();
+        N = sc.nextInt();
+        K = sc.nextInt();
 
-        int[] list = new int[N];
-        for (int i = 0; i < N; i++) {
-            list[i] = i + 1;
-        }
-
-        for (int i = 0; i < N; i++) {
-            recur(list, 1, String.valueOf(list[i]), K);
-        }
+        recur(0);
     }
 
-    public static void recur(int[] list, int cnt, String str, int K) {
+    public static void recur(int cnt) {
         if (cnt == K) {
-            System.out.println(str);
+            for (int i = 0; i < list.size(); i++) {
+                System.out.printf("%d ", list.get(i));
+            }
+
+            System.out.println();
             return;
         }
 
-        for (int i = 0; i < list.length; i++) {
-            recur(list, cnt + 1, str + " " + list[i], K);
+        for (int i = 1; i <= N; i++) {
+            list.add(i);
+            recur(cnt + 1);
+            list.remove(list.size() - 1);
         }
     }
 }
