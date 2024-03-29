ee00f9069b41882fd1bcf842736dfa2850ac281d
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-03-29 16:55:47 +0900

    Title: k개 중에 1개를 n번 뽑기, Time: 133ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240329/k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition.java
+++ 240329/k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition.java
@@ -1,38 +1,35 @@
-import java.util.Scanner;
-import java.util.ArrayList;
+import java.util.*;
 
 public class Main {
-    public static int k, n;
-    public static ArrayList<Integer> selectedNums = new ArrayList<>();
-    
-    // 선택된 원소들을 출력해줍니다.
-    public static void printPermutation() {
-        for(int i = 0; i < selectedNums.size(); i++)
-            System.out.print(selectedNums.get(i) + " ");
-        System.out.println();
+
+    public static List<Integer> list = new ArrayList<>();
+    public static int K;
+    public static int N;
+
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        K = sc.nextInt();
+        N = sc.nextInt();
+
+        recur(0);
     }
 
-    public static void findPermutations(int cnt) {
-        // n개를 모두 뽑은 경우 답을 출력해줍니다.
-        if(cnt == n) {
-            printPermutation();
+    public static void recur(int cnt) {
+        if (cnt == K) {
+            for (int i = 0; i < list.size(); i++) {
+                System.out.printf("%d ", list.get(i));
+            }
+
+            System.out.println();
             return;
         }
 
-        // 1부터 k까지의 각 숫자가 뽑혔을 때의 경우를 탐색합니다.
-        for(int i = 1; i <= k; i++) {
-            selectedNums.add(i);
-            findPermutations(cnt + 1);
-            selectedNums.remove(selectedNums.size() - 1);
+        for (int i = 1; i <= N; i++) {
+            list.add(i);
+            recur(cnt + 1);
+            list.remove(list.size() - 1);
         }
     }
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        k = sc.nextInt();
-        n = sc.nextInt();
-        
-		findPermutations(0);
-	}
 }
