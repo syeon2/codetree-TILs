5d12182df91d8c26003678e5d06cca209b1172e0
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-03-31 17:29:35 +0900

    Title: 강력한 폭발, Time: 3012ms, Memory: 11MB, Status: Time Limit Exceed - CodeTree

--- 240331/강력한 폭발/strong-explosion.java
+++ 240331/강력한 폭발/strong-explosion.java
@@ -1,35 +1,49 @@
 import java.util.*;
+import java.io.*;
 
 public class Main {
 
-    public static int N;
     public static int[][] board;
     public static int[][] memo;
+
+    public static int N;
+    public static int ans = 0;
 
     public static int[][] dx = {{0, 0, 0, 0, 0}, {0, 0, 1, 0, -1}, {0, 1, 1, -1, -1}};
     public static int[][] dy = {{0, -2, -1, 1, 2}, {0, -1, 0, 1, 0}, {0, -1, 1, 1, -1}};
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // 여기에 코드를 작성해주세요.
-        Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
 
-        N = sc.nextInt();
-
+        N = Integer.parseInt(br.readLine());
         board = new int[N][N];
         memo = new int[N][N];
-
         for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+
             for (int k = 0; k < N; k++) {
-                board[i][k] = sc.nextInt();
+                board[i][k] = Integer.parseInt(st.nextToken());
             }
         }
 
-        int ans = recur();
-
-        System.out.print(ans);
+        recur();
+        
+        bw.write(String.valueOf(ans));
+        bw.flush();
+        bw.close();
     }
 
-    public static int recur() {
+    public static boolean isRange(int x, int y) {
+        if (x >= 0 && x < N && y >= 0 && y < N) return true;
+
+        return false;
+    }
+
+    public static int getAns() {
         int sum = 0;
 
         for (int i = 0; i < N; i++) {
@@ -38,40 +52,37 @@ public class Main {
             }
         }
 
-        for (int i = 0; i < N; i++) {
-            for (int k = 0; k < N; k++) {
-                if (board[i][k] == 0) continue;
+        return sum;
+    }
 
-                board[i][k] = 0;
+    public static void recur() {
+        for (int r = 0; r < N; r++) {
+            for (int c = 0; c < N; c++) {
+                if (board[r][c] == 0) continue;
 
-                for (int j = 0; j < 3; j++) {
-                    for (int l = 0; l < 5; l++) {
-                        int nx = k + dx[j][l];
-                        int ny = i + dy[j][l];
+                board[r][c] = 0;
+
+                for (int i = 0; i < 3; i++) {
+                    for (int k = 0; k < 5; k++) {
+                        int nx = c + dx[i][k];
+                        int ny = r + dy[i][k];
 
                         if (isRange(nx, ny)) memo[ny][nx]++;
                     }
 
-                    sum = Math.max(sum, recur());
+                    ans = Math.max(ans, getAns());
+                    recur();
 
-                    for (int l = 0; l < 5; l++) {
-                        int nx = k + dx[j][l];
-                        int ny = i + dy[j][l];
+                    for (int k = 0; k < 5; k++) {
+                        int nx = c + dx[i][k];
+                        int ny = r + dy[i][k];
 
                         if (isRange(nx, ny)) memo[ny][nx]--;
                     }
                 }
 
-                board[i][k] = 1;
+                board[r][c] = 1;
             }
         }
-
-        return sum;
-    }
-
-    public static boolean isRange(int x, int y) {
-        if (x >= 0 && x < N && y >= 0 && y < N) return true;
-
-        return false;
     }
 }
