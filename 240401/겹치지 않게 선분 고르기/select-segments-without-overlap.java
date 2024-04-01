5835404d442488e21408a26cd92a34b9c4525b67
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-04-01 15:24:28 +0900

    Title: 겹치지 않게 선분 고르기, Time: 188ms, Memory: 10MB, Status: Passed - CodeTree

--- 240401/겹치지 않게 선분 고르기/select-segments-without-overlap.java
+++ 240401/겹치지 않게 선분 고르기/select-segments-without-overlap.java
@@ -5,50 +5,56 @@ public class Main {
     public static List<Line> list = new ArrayList<>();
     public static List<Line> ansList = new ArrayList<>();
 
+    public static int N;
     public static int ans = 0;
 
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
 
-        int N = sc.nextInt();
-        for (int i = 0; i < N; i++) {
+        N = sc.nextInt();
+        for (int n = 0; n < N; n++) {
             int x1 = sc.nextInt();
             int x2 = sc.nextInt();
 
             list.add(new Line(x1, x2));
         }
 
-        list.sort((a, b) -> a.x2 - b.x2);
-
         permutation(0);
 
         System.out.print(ans);
     }
 
+    public static int getAnswer() {
+        for (int i = 0; i < ansList.size(); i++) {
+            int x1 = ansList.get(i).x1;
+            int x2 = ansList.get(i).x2;
+
+            for (int k = i + 1; k < ansList.size(); k++) {
+                int x3 = ansList.get(k).x1;
+                int x4 = ansList.get(k).x2;
+
+                if ((x1 <= x3 && x2 >= x3) || (x3 <= x1 && x4 >= x1) || 
+                    (x1 <= x3 && x2 >= x4) || (x3 <= x1 && x4 >= x2)) {
+                        return 0;
+                    }
+            }
+        }
+
+        return ansList.size();
+    }
+
     public static void permutation(int idx) {
-        if (idx == list.size()) {
-            ans = Math.max(ans, ansList.size());
+        if (idx == N) {
+            ans = Math.max(ans, getAnswer());
 
             return;
         }
 
-        for (int i = idx; i < list.size(); i++) {
-            if (ansList.isEmpty()) {
-                ansList.add(list.get(i));
-                permutation(idx + 1);
-                ansList.remove(0);
-            } else {
-                Line last = ansList.get(ansList.size() - 1);
-                Line curLine = list.get(i);
-
-                if (last.x2 < curLine.x1) {
-                    ansList.add(curLine);
-                    permutation(i + 1);
-                    ansList.remove(ansList.size() - 1);
-                } else permutation(i + 1);
-            }
-        }
+        ansList.add(list.get(idx));
+        permutation(idx + 1);
+        ansList.remove(ansList.size() - 1);
+        permutation(idx + 1);
     }
 
     public static class Line {
