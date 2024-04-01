283de1793b348196808900407b4ca14fbc6814d6
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-04-01 19:50:38 +0900

    Title: 최소 점프 횟수, Time: 123ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240401/최소 점프 횟수/min-num-of-jumps.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class Main {
+
+    public static int N;
+    public static int[] list;
+
+    public static int ans = -1;
+
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        N = sc.nextInt();
+        list = new int[N];
+        for (int i = 0; i < N; i++) {
+            list[i] = sc.nextInt();
+        }
+        ans = N;
+
+        permutation(0, 0);
+
+        System.out.print(ans);
+    }
+
+    public static void permutation(int idx, int cnt) {
+        if (idx > N - 1) return;
+        else if (idx == N - 1) {
+            ans = Math.min(ans, cnt);
+            return;
+        }
+
+        int jump = list[idx];
+        for (int i = 1; i <= jump; i++) {
+            permutation(idx + i, cnt + 1);
+        }
+    }
+}
