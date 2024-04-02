0a535d6c94bd1278b34cf3149e342d66d3f52024
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-04-02 09:42:49 +0900

    Title: n개의 점 중 m개 고르기, Time: 122ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240402/n개의 점 중 m개 고르기/choose-m-out-of-n-points.java
+++ 240402/n개의 점 중 m개 고르기/choose-m-out-of-n-points.java
@@ -4,8 +4,10 @@ public class Main {
 
     public static int N;
     public static int M;
-    public static int[][] list;
-    public static int[][] ansList = new int[2][2];
+
+    public static int[][] posList;
+    public static boolean[] isVisit;
+    public static int[][] ansList;
 
     public static int ans = Integer.MAX_VALUE;
 
@@ -16,32 +18,45 @@ public class Main {
         N = sc.nextInt();
         M = sc.nextInt();
 
-        list = new int[N][2];
-        for (int i = 0; i < N; i++) {
-            list[i][0] = sc.nextInt();
-            list[i][1] = sc.nextInt();
+        posList = new int[N][2];
+        ansList = new int[2][2];
+        isVisit = new boolean[N];
+
+        for (int n = 0; n < N; n++) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+
+            posList[n][0] = x;
+            posList[n][1] = y;
         }
 
-        recur(0, 0);
+        recur(0);
 
         System.out.print(ans);
     }
 
-    public static void recur(int idx, int depth) {
+    public static void recur(int depth) {
         if (depth == 2) {
-            int sum = 0;
+            int dist = 0;
+            int x1 = ansList[0][0];
+            int y1 = ansList[0][1];
 
-            sum = (Math.abs(ansList[0][0] - ansList[1][0]) * Math.abs(ansList[0][0] - ansList[1][0])) + (Math.abs(ansList[0][1] - ansList[1][1]) * Math.abs(ansList[0][1] - ansList[1][1]));
+            int x2 = ansList[1][0];
+            int y2 = ansList[1][1];
 
-            ans = Math.min(ans, sum);
+            dist += (Math.abs(x1 - x2) * Math.abs(x1 - x2)) + (Math.abs(y1 - y2) * Math.abs(y1 - y2));
+
+            ans = Math.min(ans, dist);
             return;
         }
 
-        for (int i = idx; i < N; i++) {
-            ansList[depth][0] = list[i][0];
-            ansList[depth][1] = list[i][1];
-
-            recur(i + 1, depth + 1);
+        for (int i = 0; i < N; i++) {
+            if (isVisit[i]) continue;
+            isVisit[i] = true;
+            ansList[depth][0] = posList[i][0];
+            ansList[depth][1] = posList[i][1];
+            recur(depth + 1);
+            isVisit[i] = false;
         }
     }
 }
