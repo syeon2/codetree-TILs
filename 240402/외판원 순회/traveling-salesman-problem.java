0c04f03eb4519ecbd1bd2c6833fc8c99ff1ca34f
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-04-02 18:01:59 +0900

    Title: 외판원 순회, Time: 176ms, Memory: 11MB, Status: Passed - CodeTree

--- 240402/외판원 순회/traveling-salesman-problem.java
+++ 240402/외판원 순회/traveling-salesman-problem.java
@@ -34,8 +34,8 @@ public class Main {
         }
 
         if (depth == N - 1) {
-            if (board[nextY][0] == 0) continue;
-            
+            if (board[nextY][0] == 0) return;
+
             recur(nextY, sum + board[nextY][0], depth + 1);
         } else {
             for (int i = 1; i < N; i++) {
