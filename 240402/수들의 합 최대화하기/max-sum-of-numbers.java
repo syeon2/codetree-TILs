6f66934838c6c27a8575af86a1bc5c3a25a954a3
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-04-02 17:04:17 +0900

    Title: 수들의 합 최대화하기, Time: 194ms, Memory: 10MB, Status: Passed - CodeTree

--- 240402/수들의 합 최대화하기/max-sum-of-numbers.java
+++ 240402/수들의 합 최대화하기/max-sum-of-numbers.java
@@ -4,10 +4,7 @@ public class Main {
 
     public static int N;
     public static int[][] board;
-    public static boolean[] isVisitX;
-    public static boolean[] isVisitY;
-
-    public static int[][] ansList;
+    public static boolean[] isVisit;
 
     public static int ans = 0;
 
@@ -17,10 +14,7 @@ public class Main {
 
         N = sc.nextInt();
         board = new int[N][N];
-        isVisitX = new boolean[N];
-        isVisitY = new boolean[N];
-
-        ansList = new int[N][2];
+        isVisit = new boolean[N];
 
         for (int i = 0; i < N; i++) {
             for (int k = 0; k < N; k++) {
@@ -28,46 +22,24 @@ public class Main {
             }
         }
 
-        permutation(0);
+        permutation(0, 0);
 
         System.out.print(ans);
     }
 
-    public static void check() {
-        int sum = 0;
-
-        for (int i = 0; i < N; i++) {
-            int x1 = ansList[i][0];
-            int y1 = ansList[i][1];
-
-            sum += board[y1][x1];
-        }
-
-        ans = Math.max(ans, sum);
-    }
-
-    public static void permutation(int depth) {
-        if (depth == N) {
-            check();
+    public static void permutation(int sum, int x) {
+        if (x == N) {
+            ans = Math.max(ans, sum);
             return;
         }
 
-        for (int r = 0; r < N; r++) {
-            if (isVisitY[r]) continue;
-            isVisitY[r] = true;
+        for (int y = 0; y < N; y++) {
+            if (isVisit[y]) continue;
+            isVisit[y] = true;
 
-            for (int c = 0; c < N; c++) {
-                if (isVisitX[c]) continue;
-                isVisitX[c] = true;
+            permutation(sum + board[y][x], x + 1);
 
-                ansList[depth][0] = c;
-                ansList[depth][1] = r;
-                permutation(depth + 1);
-
-                isVisitX[c] = false;
-            }
-
-            isVisitY[r] = false;
+            isVisit[y] = false;
         }
     }
 }
