a3671eb8cb31fa63a42525c0bd500ee050fa7753
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-04-04 13:34:25 +0900

    Title: 사각형 채우기, Time: 131ms, Memory: 10MB, Status: Passed - CodeTree

--- 240404/사각형 채우기/rectangle-fill.java
+++ 240404/사각형 채우기/rectangle-fill.java
@@ -1,19 +1,32 @@
 import java.util.*;
 
 public class Main {
+
+    public static int N;
+    public static int[] ans;
+    public static boolean[] visited;
+
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
 
-        int N = sc.nextInt();
+        N = sc.nextInt();
+        ans = new int[N + 1];
+        visited = new boolean[N + 1];
 
-        int[] list = new int[N + 1];
-        
-        for (int i = 1; i <= N; i++) {
-            if (i <= 2) list[i] = i;
-            else list[i] = (list[i - 1] + list[i - 2]) % 10007;
-        }
+        dp(N);
 
-        System.out.print(list[N]);
+        System.out.print(ans[N]);
+    }
+
+    public static int dp(int n) {
+        if (visited[n]) return ans[n];
+
+        visited[n] = true;
+
+        if (n <= 2) ans[n] = n;
+        else ans[n] = (dp(n - 1) + dp(n - 2)) % 10007;
+
+        return ans[n];
     }
 }
