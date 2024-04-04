e0d39c47e20ec8ae4ae834570b8097ed984e603e
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-04-04 16:51:24 +0900

    Title: 정수 사각형 최대 합, Time: 322ms, Memory: 23MB, Status: Passed - CodeTree

--- 240404/정수 사각형 최대 합/maximum-sum-path-in-square.java
+++ 240404/정수 사각형 최대 합/maximum-sum-path-in-square.java
@@ -2,41 +2,45 @@ import java.util.*;
 
 public class Main {
 
-    public static int[] dx = {0, -1};
-    public static int[] dy = {-1, 0};
+    public static int N;
+    public static int[][] board;
+    public static boolean[][] visited;
 
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
 
-        int N = sc.nextInt();
-        int[][] board = new int[N][N];
+        N = sc.nextInt();
+        board = new int[N][N];
+        visited = new boolean[N][N];
         for (int i = 0; i < N; i++) {
             for (int k = 0; k < N; k++) {
                 board[i][k] = sc.nextInt();
             }
         }
 
-        for (int i = 0; i < N; i++) {
-            for (int k = 0; k < N; k++) {
+        int ans = dp(N - 1, N - 1);
 
-                int max = 0;
-
-                for (int j = 0; j < 2; j++) {
-                    int nx = k + dx[j];
-                    int ny = i + dy[j];
-
-                    if (isRange(nx, ny, N)) max = Math.max(max, board[ny][nx]);
-                }
-
-                board[i][k] += max;
-            }
-        }
-
-        System.out.print(board[N - 1][N - 1]);
+        System.out.print(ans);
     }
 
-    public static boolean isRange(int x, int y, int N) {
+    public static int dp(int x, int y) {
+        if (!isRange(x, y)) return 0;
+        if (visited[y][x]) return board[y][x];
+        visited[y][x] = true;
+
+        if (x == 0 && y == 0) return board[0][0];
+        else {
+            int max = Math.max(dp(x - 1, y), dp(x, y - 1));
+
+            board[y][x] += max;
+
+            return board[y][x];
+        }
+
+    }
+
+    public static boolean isRange(int x, int y) {
         if (x >= 0 && x < N && y >= 0 && y < N) return true;
 
         return false;
