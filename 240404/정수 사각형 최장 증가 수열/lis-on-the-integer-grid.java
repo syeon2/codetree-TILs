03b2f079bfcbcbcbc6c9080632c82a3275aba668
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-04-04 23:35:15 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 1344ms, Memory: 78MB, Status: Passed - CodeTree

--- 240404/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.java
+++ 240404/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.java
@@ -5,47 +5,27 @@ public class Main {
     public static int[] dx = {0, 1, 0, -1};
     public static int[] dy = {-1, 0, 1, 0};
 
+    public static int N;
+    public static int[][] board;
+    public static int[][] memo;
+
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
 
-        int N = sc.nextInt();
-        int[][] board = new int[N][N];
-        int[][] memo = new int[N][N];
-
+        N = sc.nextInt();
+        board = new int[N][N];
+        memo = new int[N][N];
         for (int i = 0; i < N; i++) {
             for (int k = 0; k < N; k++) {
                 board[i][k] = sc.nextInt();
             }
         }
 
-        for (int r = 0; r < N; r++) {
-            for (int c = 0; c < N; c++) {
-
-                if (memo[r][c] == 0) {
-                    memo[r][c] = 1;
-
-                    Queue<Pair> que = new LinkedList<>();
-                    que.add(new Pair(c, r));
-
-                    while (!que.isEmpty()) {
-                        Pair node = que.remove();
-
-                        int x = node.x;
-                        int y = node.y;
-
-                        for (int i = 0; i < 4; i++) {
-                            int nx = x + dx[i];
-                            int ny = y + dy[i];
-
-                            if (isRange(nx, ny, N) && board[y][x] < board[ny][nx]) {
-                                if (memo[ny][nx] < memo[y][x] + 1) {
-                                    memo[ny][nx] = memo[y][x] + 1;
-                                    que.add(new Pair(nx, ny));
-                                }
-                            }
-                        }
-                    }
+        for (int i = 0; i < N; i++) {
+            for (int k = 0; k < N; k++) {
+                if (memo[i][k] == 0) {
+                    dp(k, i);
                 }
             }
         }
@@ -60,19 +40,26 @@ public class Main {
         System.out.print(ans);
     }
 
-    public static boolean isRange(int x, int y, int N) {
+    public static int dp(int x, int y) {
+        if (memo[y][x] != 0) return memo[y][x];
+
+        int maxCnt = 1;
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (isRange(nx, ny) && board[ny][nx] > board[y][x]) {
+                maxCnt = Math.max(maxCnt, dp(nx, ny) + 1);
+            }
+        }
+
+        memo[y][x] = maxCnt;
+        return memo[y][x];
+    }
+
+    public static boolean isRange(int x, int y) {
         if (x >= 0 && x < N && y >= 0 && y < N) return true;
 
         return false;
-    }
-
-    public static class Pair {
-        public int x;
-        public int y;
-
-        public Pair(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
     }
 }
