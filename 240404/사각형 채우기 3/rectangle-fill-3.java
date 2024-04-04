8f02aa72d6d13f13f97b1d6b8ac18499fda9bcd7
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-04-04 14:50:11 +0900

    Title: 사각형 채우기 3, Time: 177ms, Memory: 11MB, Status: Passed - CodeTree

--- 240404/사각형 채우기 3/rectangle-fill-3.java
+++ 240404/사각형 채우기 3/rectangle-fill-3.java
@@ -3,17 +3,17 @@ import java.util.*;
 public class Main {
 
     public static int N;
-    public static int[] ans;
+    public static long[] ans;
     public static boolean[] visited;
 
-    public static int MOD = 1000000007;
+    public static long MOD = 1000000007;
 
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
 
         N = sc.nextInt();
-        ans = new int[N + 1];
+        ans = new long[N + 1];
         visited = new boolean[N + 1];
         
         dp(N);
@@ -21,7 +21,7 @@ public class Main {
         System.out.print(ans[N]);
     }
 
-    public static int dp(int n) {
+    public static long dp(int n) {
         if (visited[n]) return ans[n];
 
         visited[n] = true;
@@ -30,10 +30,10 @@ public class Main {
         else if (n == 1) ans[n] = 2;
         else if (n == 2) ans[n] = 7;
         else {
-            ans[n] = (((2 * dp(n - 1)) % MOD) + ((3 * dp(n - 2)) % MOD)) % MOD;
+            ans[n] = ((2 * dp(n - 1)) + (3 * dp(n - 2))) % MOD;
             
             for (int i = 0; i <= n - 3; i++) {
-                ans[n] = (((2 * dp(i)) % MOD) + ans[n]) % MOD;
+                ans[n] = (ans[n] + (2 * dp(i))) % MOD;
             }
         }
 
