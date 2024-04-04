4288caf8dc59062f8c4f4fd40aca9bb73a5669ff
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-04-04 16:14:42 +0900

    Title: 서로 다른 BST 개수 세기, Time: 124ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240404/서로 다른 BST 개수 세기/number-of-unique-bst.java
+++ 240404/서로 다른 BST 개수 세기/number-of-unique-bst.java
@@ -23,7 +23,8 @@ public class Main {
         if (visited[n]) return ans[n];
         visited[n] = true;
 
-        if (n <= 2) ans[n] = n;
+        if (n <= 1) ans[n] = 1;
+        else if (n == 2) ans[n] = 2;
         else {
             for (int i = 0; i < n - 1; i++) {
                 ans[n] += dp(i) + dp(n - i - 1);
