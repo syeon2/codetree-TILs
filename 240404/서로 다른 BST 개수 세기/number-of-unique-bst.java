95b544a0c4c287e2bf79f6a3dbc236eb882eaee7
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-04-04 15:50:57 +0900

    Title: 서로 다른 BST 개수 세기, Time: 128ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240404/서로 다른 BST 개수 세기/number-of-unique-bst.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        int[] list = new int[N + 1];
+        list[1] = 1;
+        if (N >= 2) list[2] = 2;
+
+        for (int i = 3; i <= N; i++) {
+            list[i] = list[i - 1];
+
+            for (int k = 1; k <= N - 1; k++) {
+                list[i] += (list[N - k] * list[N - (N - k)]);
+            }
+        }
+
+        System.out.print(list[N]);
+    }
+}
