b4d558dd5338b0b3b65e9ed8f5e356acfa4fc36d
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-04-04 13:25:52 +0900

    Title: 계단 오르기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240404/계단 오르기/climbing-stairs.java
+++ 240404/계단 오르기/climbing-stairs.java
@@ -1,20 +1,31 @@
 import java.util.*;
 
 public class Main {
+
+    public static int N;
+    public static int[] ans;
+    public static boolean[] visited;
+
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
 
-        int N = sc.nextInt();
+        N = sc.nextInt();
+        ans = new int[N + 1];
+        visited = new boolean[N + 1];
+        
+        dp(N);
 
-        long[] list = new long[N + 1];
-        list[2] = 1;
-        if (N >= 3) list[3] = 1;
+        System.out.print(ans[N]);
+    }
 
-        for (int i = 4; i <= N; i++) {
-            list[i] = (list[i - 2] + list[i - 3]) % 10007;
-        }
+    public static int dp(int n) {
+        if (visited[n]) return ans[n];
+        visited[n] = true;
+        
+        if (n == 2 || n == 3) ans[n] = 1;
+        else ans[n] = dp(n - 2) + dp(n - 3);
 
-        System.out.print(list[N]);
+        return ans[n];
     }
 }
