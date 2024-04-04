2a078182046bde240180ac0ce9fc0ba4a8a7dcfe
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-04-04 13:41:35 +0900

    Title: 사각형 채우기 3, Time: 127ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240404/사각형 채우기 3/rectangle-fill-3.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main {
+
+    public static int N;
+    public static long[] ans;
+    public static boolean[] visited;
+
+    public static long MOD = 1000000007;
+
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        N = sc.nextInt();
+        ans = new long[N + 1];
+        ans[1] = 2;
+        if (N >= 2) ans[2] = 7;
+
+        for (int i = 3; i <= N; i++) {
+            ans[i] = ((2 * ans[i - 1]) + (4 * ans[i - 2])) % MOD;
+        }
+
+        System.out.print(ans[N]);
+    }
+}
