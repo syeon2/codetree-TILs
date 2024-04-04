448280f0bc4322809b1f9a553efbf543b7240b3f
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-04-04 18:13:48 +0900

    Title: 정수 사각형 최솟값의 최대, Time: 343ms, Memory: 23MB, Status: Passed - CodeTree

--- 240404/정수 사각형 최솟값의 최대/maximin-path-in-square.java
+++ 240404/정수 사각형 최솟값의 최대/maximin-path-in-square.java
@@ -1,34 +1,41 @@
 import java.util.*;
 
 public class Main {
+
+    public static int N;
+    public static int[][] board;
+    public static boolean[][] visited;
+
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
 
-        int N = sc.nextInt();
-        int[][] board = new int[N][N];
+        N = sc.nextInt();
+        board = new int[N][N];
+        visited = new boolean[N][N];
+
         for (int i = 0; i < N; i++) {
             for (int k = 0; k < N; k++) {
                 board[i][k] = sc.nextInt();
             }
         }
 
-        for (int i = 1; i < N; i++) {
-            board[0][i] = Math.min(board[0][i], board[0][i - 1]);
-        }
+        int ans = dp(N - 1, N - 1);
 
-        for (int i = 1; i < N; i++) {
-            board[i][0] = Math.min(board[i][0], board[i - 1][0]);
-        }
+        System.out.print(ans);
+    }
 
-        for (int i = 1; i < N; i++) {
-            for (int k = 1; k < N; k++) {
-                int max = Math.max(board[i - 1][k], board[i][k - 1]);
+    public static int dp(int x, int y) {
+        if (x < 0 || y < 0) return 0;
+        if (visited[y][x]) return board[y][x];
 
-                board[i][k] = Math.min(board[i][k], max);
-            }
-        }
+        visited[y][x] = true;
 
-        System.out.print(board[N - 1][N - 1]);
+        if (x == 0 && y == 0) return board[y][x];
+
+        int max = Math.max(dp(x - 1, y), dp(x, y - 1));
+        board[y][x] = Math.min(board[y][x], max);
+
+        return board[y][x];
     }
 }
