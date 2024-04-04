8abd0f5bf8199cbca12242d38d236fd7785dcd2a
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-04-05 00:53:03 +0900

    Title: 정수 사각형 차이의 최소 2, Time: 150ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240405/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.java
+++ 240405/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.java
@@ -1,46 +1,40 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // 여기에 코드를 작성해주세요.
-        Scanner sc = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[][] grid = new int[n][n];
 
-        int N = sc.nextInt();
-        int[][] board = new int[N][N];
-        for (int i = 0; i < N; i++) {
-            for (int k = 0; k < N; k++) {
-                board[i][k] = sc.nextInt();
+        // 격자 정보 입력
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                grid[i][j] = scanner.nextInt();
             }
         }
 
-        int[][][] memo = new int[N][N][2];
-        memo[0][0][0] = memo[0][0][1] = board[0][0];
-
-        for (int i = 1; i < N; i++) {
-            memo[0][i][0] = Math.min(memo[0][i - 1][0], board[0][i]);
-            memo[0][i][1] = Math.max(memo[0][i - 1][1], board[0][i]);
-
-            memo[i][0][0] = Math.min(memo[i - 1][0][0], board[i][0]);
-            memo[i][0][1] = Math.max(memo[i - 1][0][1], board[i][0]);
+        // dp 배열 초기화
+        int[][] dp = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                dp[i][j] = Integer.MAX_VALUE;
+            }
         }
+        dp[0][0] = grid[0][0];
 
-        for (int i = 1; i < N; i++) {
-            for (int k = 1; k < N; k++) {
-                int case1 = Math.max(memo[i - 1][k][1], board[i][k]) - Math.min(memo[i - 1][k][0], board[i][k]);
-                int case2 = Math.max(memo[i][k - 1][1], board[i][k]) - Math.min(memo[i][k - 1][0], board[i][k]);
-
-                if (case1 <= case2) {
-                    memo[i][k][0] = Math.min(memo[i - 1][k][0], board[i][k]);
-                    memo[i][k][1] = Math.max(memo[i - 1][k][1], board[i][k]);
-                } else {
-                    memo[i][k][0] = Math.min(memo[i][k - 1][0], board[i][k]);
-                    memo[i][k][1] = Math.max(memo[i][k - 1][1], board[i][k]);
+        // dp 배열 업데이트
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i > 0) {
+                    dp[i][j] = Math.min(dp[i][j], Math.max(dp[i-1][j], grid[i][j]));
+                }
+                if (j > 0) {
+                    dp[i][j] = Math.min(dp[i][j], Math.max(dp[i][j-1], grid[i][j]));
                 }
             }
         }
 
-        int sub = memo[N - 1][N - 1][1] - memo[N - 1][N - 1][0];
-
-        System.out.print(sub);
+        // 결과 출력
+        System.out.println(dp[n-1][n-1] - dp[0][0]);
     }
 }
