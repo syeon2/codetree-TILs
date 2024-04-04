70436fbead187d220a028f7ce9236b473711591a
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-04-04 13:14:37 +0900

    Title: 피보나치 수, Time: 241ms, Memory: 10MB, Status: Passed - CodeTree

--- 240404/피보나치 수/fibonacci-number.java
+++ 240404/피보나치 수/fibonacci-number.java
@@ -1,20 +1,30 @@
 import java.util.*;
 
 public class Main {
+
+    public static int N;
+    public static int[] ans;
+
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
 
-        int N = sc.nextInt();
+        N = sc.nextInt();
+        ans = new int[N + 1];
 
-        int[] list = new int[N + 1];
-        list[1] = 1;
-        if (N >= 2) list[2] = 1;
+        dp(N);
 
-        for (int i = 3; i <= N; i++) {
-            list[i] = list[i - 1] + list[i - 2];
+        System.out.print(ans[N]);
+    }
+
+    public static int dp(int n) {
+        if (ans[n] != 0) return ans[n];
+
+        if (n <= 2) ans[n] = 1;
+        else {
+            ans[n] = dp(n - 1) + dp(n - 2);    
         }
 
-        System.out.print(list[N]);
+        return ans[n];
     }
 }
