0882978b6c12e58546dec89bd5ee0a52343f7207
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-04-04 18:23:36 +0900

    Title: 정수 사각형 최댓값의 최소, Time: 358ms, Memory: 23MB, Status: Passed - CodeTree

---
+++ 240404/정수 사각형 최댓값의 최소/minimax-path-in-square.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int[][] board = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            for (int k = 0; k < N; k++) {
+                board[i][k] = sc.nextInt();
+            }
+        }
+
+        for (int i = 1; i < N; i++) {
+            board[0][i] = Math.max(board[0][i], board[0][i - 1]);
+        }
+
+        for (int i = 1; i < N; i++) {
+            board[i][0] = Math.max(board[i][0], board[i - 1][0]);
+        }
+
+        for (int i = 1; i < N; i++) {
+            for (int k = 1; k < N; k++) {
+                int min = Math.min(board[i - 1][k], board[i][k - 1]);
+
+                board[i][k] = Math.max(board[i][k], min);
+            }
+        }
+
+        System.out.print(board[N - 1][N - 1]);
+    }
+}
