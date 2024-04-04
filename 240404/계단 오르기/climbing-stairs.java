3d97106433c1fb7d9c078bf36fd0dcaf7f9f7bba
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-04-04 13:19:10 +0900

    Title: 계단 오르기, Time: 129ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240404/계단 오르기/climbing-stairs.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        int[] list = new int[N + 1];
+        list[2] = 1;
+        if (N >= 3) list[3] = 1;
+
+        for (int i = 4; i <= N; i++) {
+            list[i] = list[i - 2] + list[i - 3];
+        }
+
+        System.out.print(list[N]);
+    }
+}
