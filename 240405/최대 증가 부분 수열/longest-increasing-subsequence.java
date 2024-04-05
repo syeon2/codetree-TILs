f019f83cffd386204f64df4c364928737314973e
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-04-05 15:10:26 +0900

    Title: 최대 증가 부분 수열, Time: 126ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240405/최대 증가 부분 수열/longest-increasing-subsequence.java
+++ 240405/최대 증가 부분 수열/longest-increasing-subsequence.java
@@ -1,37 +1,51 @@
 import java.util.*;
 
 public class Main {
+
+    public static int N;
+    public static int[] list;
+    public static int[] memo;
+    
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
 
-        int N = sc.nextInt();
-        int[] list = new int[N];
-        int[] dp = new int[N];
+        N = sc.nextInt();
+        list = new int[N];
+        memo = new int[N];
 
         for (int i = 0; i < N; i++) {
             list[i] = sc.nextInt();
         }
 
-        dp[0] = 1;
-
-        for (int i = 1; i < N; i++) {
-            int max = 0;
-
-            for (int k = 0; k < i; k++) {
-                if (list[i] > list[k]) {
-                    max = Math.max(max, dp[k]);
-                }
-            }
-
-            dp[i] = max + 1;
-        }
+        dp(N - 1);
 
         int ans = 0;
         for (int i = 0; i < N; i++) {
-            ans = Math.max(ans, dp[i]);
+            ans = Math.max(ans, memo[i]);
         }
 
         System.out.print(ans);
+    }
+
+    public static int dp(int n) {
+        if (memo[n] == 1) return memo[n];
+
+        if (n == 0) memo[n] = 1;
+        else {
+            int max = 0;
+
+            for (int i = 0; i < n; i++) {
+                if (list[i] < list[n]) {
+                    int cnt = dp(i);
+
+                    max = Math.max(max, cnt);
+                }
+            }
+
+            memo[n] = max + 1;
+        }
+
+        return memo[n];
     }
 }
