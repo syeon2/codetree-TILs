7670d3a7a8b55f59757d5216061dcd9174542291
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-04-05 15:00:33 +0900

    Title: 최대 증가 부분 수열, Time: 267ms, Memory: 13MB, Status: Passed - CodeTree

---
+++ 240405/최대 증가 부분 수열/longest-increasing-subsequence.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int[] list = new int[N];
+        int[] dp = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            list[i] = sc.nextInt();
+        }
+
+        dp[0] = 1;
+
+        for (int i = 1; i < N; i++) {
+            int max = 0;
+
+            for (int k = 0; k < i; k++) {
+                if (list[i] > list[k]) {
+                    max = Math.max(max, dp[k]);
+                }
+            }
+
+            dp[i] = max + 1;
+        }
+
+        int ans = 0;
+        for (int i = 0; i < N; i++) {
+            ans = Math.max(ans, dp[i]);
+        }
+
+        System.out.print(ans);
+    }
+}
