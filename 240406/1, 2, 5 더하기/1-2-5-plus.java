e3473c2acd111d03d216a6971eeeeaef598820bf
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-04-06 00:31:55 +0900

    Title: 1, 2, 5 더하기, Time: 129ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240406/1, 2, 5 더하기/1-2-5-plus.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        int[] list = new int[N + 1];
+        list[0] = 1;
+        list[1] = 1;
+        if (N >= 2) list[2] = 2;
+
+        for (int i = 3; i <= N; i++) {
+            list[i] = list[i - 1] + list[i - 2];
+
+            if (i >= 5) list[i] += list[i - 5];
+        }
+
+        System.out.print(list[N]);
+    }
+}
