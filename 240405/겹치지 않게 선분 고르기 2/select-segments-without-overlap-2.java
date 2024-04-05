bd94669fee91761f8186205944df1b426c276b3a
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-04-05 16:49:46 +0900

    Title: 겹치지 않게 선분 고르기 2, Time: 436ms, Memory: 17MB, Status: Passed - CodeTree

--- 240405/겹치지 않게 선분 고르기 2/select-segments-without-overlap-2.java
+++ 240405/겹치지 않게 선분 고르기 2/select-segments-without-overlap-2.java
@@ -17,6 +17,14 @@ public class Main {
             list[i][1] = x2;
         }
 
+        Arrays.sort(list, (a, b) -> {
+            if (a[0] == b[0]) {
+                return a[1] - b[1];
+            }
+
+            return a[0] - b[0];
+        });
+
         memo[0] = 1;
         for (int i = 1; i < N; i++) {
             int cnt = 0;
