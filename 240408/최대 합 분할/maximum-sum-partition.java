9243aaeea80017f52632185977b087f43c0351aa
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-04-08 17:44:26 +0900

    Title: 최대 합 분할, Time: 149ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240408/최대 합 분할/maximum-sum-partition.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int M = 0;
+
+        int N = sc.nextInt();
+        int[] list = new int[N];
+        for (int i = 0; i < N; i++) {
+            list[i] = sc.nextInt();
+            M += list[i];
+        }
+
+        int[] memo = new int[M + 1];
+        Arrays.fill(memo, -1);
+        memo[0] = 0;
+
+        for (int i = 0; i < N; i++) {
+            int value = list[i];
+
+            for (int k = M; k >= 0; k--) {
+                if (k == value && memo[k] == -1) memo[k] = Math.max(memo[k], value);
+                else if (k >= value && memo[k - value] != -1) {
+                    memo[k] = Math.max(memo[k], memo[k - value] + value);
+                }
+            }
+        }
+
+        int ans = 0;
+
+        for (int i = 0; i <= M; i++) {
+            if (memo[i] != -1) {
+                int a = memo[i];
+
+                if (memo[(M - a) / 2] != -1) ans = Math.max(ans, memo[(M - a) / 2]);
+            }
+        }
+
+        System.out.print(ans);
+    }
+}
