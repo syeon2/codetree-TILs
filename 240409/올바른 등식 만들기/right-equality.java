7e3bfbb797ab5737152108f97d874b26781d1eaf
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-04-09 19:14:29 +0900

    Title: 올바른 등식 만들기, Time: 117ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240409/올바른 등식 만들기/right-equality.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class Main {
+
+    public static int OFFSET = 20;
+
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+
+        int[] list = new int[N];
+        for (int i = 0; i < N; i++) {
+            list[i] = sc.nextInt();
+        }
+
+        int[][] memo = new int[N][41];
+
+        memo[0][list[0] + OFFSET] = 1;
+        memo[0][(list[0] * -1) + OFFSET] = 1;
+
+        for (int i = 1; i < N; i++) {
+
+            int value = list[i];
+
+            for (int k = 0; k <= 40; k++) {
+                if (memo[i - 1][k] > 0) {
+                    if (k - value >= 0 && memo[i][k - value] == 0) memo[i][k - value] = memo[i - 1][k];
+                    else if (k - value >= 0) memo[i][k - value]++;
+
+                    if (k + value <= 40 && memo[i][k + value] == 0) memo[i][k + value] = memo[i - 1][k];
+                    else if (k + value <= 40) memo[i][k + value]++;
+                }
+            }
+        }
+
+        System.out.print(memo[N - 1][M + OFFSET]);
+    }
+
+}
