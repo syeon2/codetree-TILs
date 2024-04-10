8ff4131215d6e71600bdf21933b333ef30d48adf
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-04-10 17:06:42 +0900

    Title: 금 채굴하기, Time: 152ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240410/금 채굴하기/gold-mining.java
+++ 240410/금 채굴하기/gold-mining.java
@@ -6,6 +6,9 @@ public class Main {
     public static int M;
     public static int[][] board;
 
+    public static int[] dx = {-1, 1, 1, -1};
+    public static int[] dy = {1, 1, -1, -1};
+
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
@@ -24,7 +27,7 @@ public class Main {
         for (int r = 0; r < N; r++) {
             for (int c = 0; c < N; c++) {
 
-                for (int k = 0; k <= N; k++) {
+                for (int k = 0; k < N; k++) {
                     int cnt = getGoldCnt(c, r, k);
 
                     if ((cnt * M) >= getCost(k)) ans = Math.max(ans, cnt);
@@ -36,15 +39,34 @@ public class Main {
     }
 
     public static int getGoldCnt(int x, int y, int k) {
-        int cnt = 0;
+        if (k == 0) return board[y][x];
 
-        for (int r = 0; r < N; r++) {
-            for (int c = 0; c < N; c++) {
-                if (Math.abs(x - c) + Math.abs(y - r) <= k) cnt += board[r][c];
+        int cnt = board[y][x];
+
+        for (int i = 1; i <= k; i++) {
+            int curX = x;
+            int curY = y - i;
+
+            for (int j = 0; j < 4; j++) {
+                int temp = i;
+
+                while (temp-- > 0) {
+                    int nx = curX + dx[j];
+                    int ny = curY + dy[j];
+
+                    if (isRange(nx, ny)) cnt += board[ny][nx];
+
+                    curX = nx;
+                    curY = ny;
+                }
             }
         }
 
         return cnt;
+    }
+
+    public static boolean isRange(int x, int y) {
+        return x >= 0 && x < N && y >= 0 && y < N;
     }
 
     public static int getCost(int k) {
