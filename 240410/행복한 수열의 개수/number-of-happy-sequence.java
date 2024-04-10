83a440ff6d2f72fe6ca9848e5b6aac2ffe5da84f
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-04-10 13:55:04 +0900

    Title: 행복한 수열의 개수, Time: 312ms, Memory: 20MB, Status: Passed - CodeTree

--- 240410/행복한 수열의 개수/number-of-happy-sequence.java
+++ 240410/행복한 수열의 개수/number-of-happy-sequence.java
@@ -1,14 +1,23 @@
 import java.util.*;
 
 public class Main {
+
+    public static int N;
+    public static int M;
+    public static int[][] board;
+
+    public static int[] seq;
+
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
 
-        int N = sc.nextInt();
-        int M = sc.nextInt();
+        N = sc.nextInt();
+        M = sc.nextInt();
+        board = new int[N][N];
 
-        int[][] board = new int[N][N];
+        seq = new int[N];
+
         for (int i = 0; i < N; i++) {
             for (int k = 0; k < N; k++) {
                 board[i][k] = sc.nextInt();
@@ -17,39 +26,33 @@ public class Main {
 
         int ans = 0;
         for (int i = 0; i < N; i++) {
-            int cnt = 0;
-            int value = 0;
-
             for (int k = 0; k < N; k++) {
-                if (board[i][k] == value) cnt++;
-                else {
-                    value = board[i][k];
-                    cnt = 1;
-                }
-
-                if (cnt == M) {
-                    ans++;
-                    break;
-                }
+                seq[k] = board[i][k];
             }
 
-            cnt = 0;
-            value = 0;
+            if (isHappy()) ans++;
 
             for (int k = 0; k < N; k++) {
-                if (board[k][i] == value) cnt++;
-                else {
-                    value = board[k][i];
-                    cnt = 1;
-                }
-
-                if (cnt == M) {
-                    ans++;
-                    break;
-                }
+                seq[k] = board[k][i];
             }
+
+            if (isHappy()) ans++;
         }
 
         System.out.print(ans);
+    }
+
+    public static boolean isHappy() {
+        int cnt = 1;
+        int maxCnt = 1;
+
+        for (int i = 1; i < N; i++) {
+            if (seq[i] == seq[i - 1]) cnt++;
+            else cnt = 1;
+
+            maxCnt = Math.max(maxCnt, cnt);
+        }
+
+        return maxCnt >= M;
     }
 }
