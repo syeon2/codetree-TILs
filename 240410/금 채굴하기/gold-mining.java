d45bfad48c5fadf1358856ece484a82742c8c168
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-04-10 16:33:17 +0900

    Title: 금 채굴하기, Time: 255ms, Memory: 11MB, Status: Passed - CodeTree

--- 240410/금 채굴하기/gold-mining.java
+++ 240410/금 채굴하기/gold-mining.java
@@ -13,7 +13,6 @@ public class Main {
         N = sc.nextInt();
         M = sc.nextInt();
         board = new int[N][N];
-
         for (int i = 0; i < N; i++) {
             for (int k = 0; k < N; k++) {
                 board[i][k] = sc.nextInt();
@@ -21,6 +20,7 @@ public class Main {
         }
 
         int ans = 0;
+
         for (int r = 0; r < N; r++) {
             for (int c = 0; c < N; c++) {
                 int cnt = getGoldCnt(c, r);
@@ -33,43 +33,34 @@ public class Main {
     }
 
     public static int getGoldCnt(int x, int y) {
-        int result = 0;
+        int maxCnt = 0;
 
-        for (int i = 0; i <= N / 2; i++) {
-
-            boolean canDig = true;
-
+        for (int i = 0; i <= N; i++) {
             int K = i;
 
             int cost = (K * K) + ((K + 1) * (K + 1));
             int cnt = 0;
 
-            for (int k = K; k >= 0; k--) {
+            for (int k = 0; k <= K; k++) {
                 if (k == 0) {
-                    for (int j = x - k; j <= x + k; j++) {
+                    for (int j = x - K; j <= x + K; j++) {
                         if (isRange(j, y)) cnt += board[y][j];
-                        else canDig = false;
                     }
                 } else {
                     int y1 = y - k;
                     int y2 = y + k;
 
-                    for (int j = x - k; j <= x + k; j++) {
+                    for (int j = x - (K - k); j <= x + (K - k); j++) {
                         if (isRange(j, y1)) cnt += board[y1][j];
-                        else canDig = false;
-
                         if (isRange(j, y2)) cnt += board[y2][j];
-                        else canDig = false;
                     }
                 }
             }
 
-            if (canDig && (cnt * M) >= cost) {
-                result = Math.max(result, cnt);
-            }
+            if ((cnt * M) >= cost) maxCnt = Math.max(maxCnt, cnt);
         }
 
-        return result;
+        return maxCnt;
     }
 
     public static boolean isRange(int x, int y) {
