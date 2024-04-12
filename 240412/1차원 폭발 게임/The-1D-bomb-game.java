817ad980b9bb3d08fd04c5e40db087e2a43953d7
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-04-12 19:36:31 +0900

    Title: 1차원 폭발 게임, Time: 250ms, Memory: 10MB, Status: Passed - CodeTree

--- 240412/1차원 폭발 게임/The-1D-bomb-game.java
+++ 240412/1차원 폭발 게임/The-1D-bomb-game.java
@@ -1,10 +1,9 @@
 import java.util.*;
 
 public class Main {
-
+    
     public static int N;
     public static int M;
-
     public static int[] list;
 
     public static void main(String[] args) {
@@ -13,70 +12,71 @@ public class Main {
 
         N = sc.nextInt();
         M = sc.nextInt();
-
         list = new int[N];
         for (int i = 0; i < N; i++) {
             list[i] = sc.nextInt();
         }
 
-        boolean bomed = true;
+        boolean bombed = true;
 
-        while (bomed) {
-            if (N == 0) break;
+        do {
+            bombed = false;
 
-            bomed = false;
+            for (int i = 0; i < N; i++) {
+                int endIdx = findEndValue(i);
 
-            int prev = 0;
-            int v = list[0];
-
-            int cnt = 0;
-
-            for (int i = 1; i < N; i++) {
-                if (v == list[i]) continue;
-                else {
-                    if (i - prev >= M) {
-                        for (int k = prev; k < i; k++) {
-                            list[k] = 0;
-                            cnt++;
-                        }
-
-                        prev = i;
-                        v = list[i];
-                        bomed = true;
-                    } else {
-                        prev = i;
-                        v = list[i];
-                    }
+                if (endIdx - i + 1 >= M) {
+                    checkBomb(i, endIdx);
+                    bombed = true;
                 }
             }
 
-            if (N - prev >= M) {
-                for (int i = prev; i < N; i++) {
-                    list[i] = 0;
-                    cnt++;
-                }
-
-                bomed = true;
-            }
-
-            N -= cnt;
-            int[] memo = new int[N];
-
-            int idx = 0;
-            for (int i = 0; i < list.length; i++) {
-                if (list[i] == 0) continue;
-                else memo[idx++] = list[i];
-            }
-
-            list = memo;
-        }
+            sorted();
+        } while (bombed);
 
         StringBuilder sb = new StringBuilder();
         sb.append(N).append("\n");
+        
         for (int i = 0; i < N; i++) {
             sb.append(list[i]).append("\n");
         }
 
-        System.out.print(sb);
+        System.out.print(sb.toString());
+    }
+
+    public static void sorted() {
+        int idx = 0;
+
+        int[] memo = new int[N];
+        for (int i = 0; i < N; i++) {
+            if (list[i] != 0) memo[idx++] = list[i];
+        }
+
+        int[] temp = new int[idx];
+        for (int i = 0; i < idx; i++) {
+            temp[i] = memo[i];
+        }
+
+        list = temp;
+        N = idx;
+    }
+
+    public static void checkBomb(int start, int end) {
+        for (int i = start; i <= end; i++) {
+            list[i] = 0;
+        }
+    }
+
+    public static int findEndValue(int start) {
+
+        int value = list[start];
+        int endIdx = start + 1;
+
+        for (int i = start + 1; i < N; i++) {
+            if (value != list[i]) break;
+            else endIdx++;
+        }
+
+        return endIdx - 1;
     }
 }
