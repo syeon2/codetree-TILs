83158a659b75d75230ced846c1e8d459a17d1cd4
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-04-15 18:13:51 +0900

    Title: 최적의 십자 모양 폭발, Time: 388ms, Memory: 21MB, Status: Wrong Answer - CodeTree

--- 240415/최적의 십자 모양 폭발/best-cross-shape-bomb.java
+++ 240415/최적의 십자 모양 폭발/best-cross-shape-bomb.java
@@ -2,12 +2,14 @@ import java.util.*;
 
 public class Main {
 
-    public static int[] dx = {0, 1, 0, -1};
-    public static int[] dy = {-1, 0, 1, 0};
+    public static int[] dx = {1, 0, -1, 0};
+    public static int[] dy = {0, 1, 0, -1};
 
     public static int N;
     public static int[][] board;
     public static int[][] memo;
+
+    public static int ans = 0;
 
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
@@ -16,46 +18,61 @@ public class Main {
         N = sc.nextInt();
         board = new int[N][N];
         memo = new int[N][N];
-        
-        for (int r = 0; r < N; r++) {
-            for (int c = 0; c < N; c++) {
-                board[r][c] = sc.nextInt();
+
+        for (int i = 0; i < N; i++) {
+            for (int k = 0; k < N; k++) {
+                board[i][k] = sc.nextInt();
             }
         }
 
-        int ans = 0;
-
         for (int r = 0; r < N; r++) {
             for (int c = 0; c < N; c++) {
+
                 initialize();
 
                 bomb(c, r);
-                drop();
-                
-                for (int i = 1; i <= 2; i++) {
-                    int nx = c + dx[i];
-                    int ny = r + dy[i];
 
-                    if (isRange(nx, ny) && memo[ny][nx] == memo[r][c]) ans++;
+                for (int i = 0; i < N; i++) {
+                    drop(i);
                 }
+
+                checkPair();
             }
         }
 
         System.out.print(ans);
     }
 
-    public static void drop() {
-        int[][] memo2 = new int[N][N];
+    public static void checkPair() {
+        int temp = 0;
 
-        for (int c = 0; c < N; c++) {
-            int idx = N - 1;
+        for (int r = 0; r < N; r++) {
+            for (int c = 0; c < N; c++) {
+                if (memo[r][c] == 0) continue;
 
-            for (int r = N - 1; r >= 0; r--) {
-                if (memo[r][c] != 0) memo2[idx--][c] = memo[r][c];
+                for (int i = 0; i < 2; i++) {
+                    int nx = c + dx[i];
+                    int ny = r + dy[i];
+
+                    if (isRange(nx, ny) && memo[r][c] == memo[ny][nx]) temp++;
+                }
             }
         }
+    
+        ans = Math.max(ans, temp);
+    }
 
-        memo = memo2;
+    public static void drop(int col) {
+        int[] temp = new int[N];
+
+        int idx = N - 1;
+        for (int r = N - 1; r >= 0; r--) {
+            if (memo[r][col] != 0) temp[idx--] = memo[r][col];
+        }
+
+        for (int r = 0; r < N; r++) {
+            memo[r][col] = temp[r];
+        }
     }
 
     public static boolean isRange(int x, int y) {
@@ -65,13 +82,14 @@ public class Main {
     }
 
     public static void bomb(int x, int y) {
-        int cnt = memo[y][x];
-
         for (int i = 0; i < 4; i++) {
+            int cnt = memo[y][x];
+            memo[y][x] = 0;
+
             int curX = x;
             int curY = y;
 
-            while (cnt-- > 0) {
+            while (cnt-- > 1) {
                 int nx = curX + dx[i];
                 int ny = curY + dy[i];
 
