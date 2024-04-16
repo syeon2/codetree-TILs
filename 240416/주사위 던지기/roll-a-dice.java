564a1ce44e5cc449c006214ca95961f97e6b551f
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-04-16 16:18:01 +0900

    Title: 주사위 던지기, Time: 124ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240416/주사위 던지기/roll-a-dice.java
+++ 240416/주사위 던지기/roll-a-dice.java
@@ -2,15 +2,20 @@ import java.util.*;
 
 public class Main {
 
+    public static int[] dx = {1, 0, -1, 0};
+    public static int[] dy = {0, 1, 0, -1};
+
     public static int N;
     public static int M;
+
     public static int R;
     public static int C;
 
-    public static int[][] board;
+    public static int[] upAndDown = {1, 6};
+    public static int[] leftAndRight = {4, 3};
+    public static int[] frontAndBack = {2, 5};
 
-    public static int[] dx = {0, 0, -1, 1};
-    public static int[] dy = {-1, 1, 0, 0};
+    public static int[][] board;
 
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
@@ -18,79 +23,67 @@ public class Main {
 
         N = sc.nextInt();
         M = sc.nextInt();
+
         R = sc.nextInt() - 1;
         C = sc.nextInt() - 1;
 
         board = new int[N][N];
-
-
-        int f = 2;
-        int b = 5;
-        int u = 1;
-        int d = 6;
-        int l = 4;
-        int r = 5;
-
-        int curX = C;
-        int curY = R;
-        board[curY][curX] = d;
+        board[R][C] = upAndDown[1];
 
         for (int m = 0; m < M; m++) {
-            char c = sc.next().charAt(0);
+            int direc = getDirec(sc.next().charAt(0));
 
-            int direc = getDirec(c);
-
-            int nx = curX + dx[direc];
-            int ny = curY + dy[direc];
+            int nx = C + dx[direc];
+            int ny = R + dy[direc];
 
             if (isRange(nx, ny)) {
-                curX = nx;
-                curY = ny;
-
                 if (direc == 0) {
-                    int tempF = f;
-                    int tempU = u;
-                    int tempB = b;
-                    int tempD = d;
+                    int up = upAndDown[0];
+                    int down = upAndDown[1];
+                    int left = leftAndRight[0];
+                    int right = leftAndRight[1];
 
-                    u = tempF;
-                    b = tempU;
-                    d = tempB;
-                    f = tempD;
+                    leftAndRight[0] = down;
+                    leftAndRight[1] = up;
+                    upAndDown[0] = left;
+                    upAndDown[1] = right;
                 } else if (direc == 1) {
-                    int tempB = b;
-                    int tempU = u;
-                    int tempF = f;
-                    int tempD = d;
+                    int up = upAndDown[0];
+                    int down = upAndDown[1];
+                    int front = frontAndBack[0];
+                    int back = frontAndBack[1];
 
-                    u = tempB;
-                    f = tempU;
-                    d = tempF;
-                    b = tempD;
+                    frontAndBack[0] = up;
+                    frontAndBack[1] = down;
+                    upAndDown[0] = back;
+                    upAndDown[1] = front;
                 } else if (direc == 2) {
-                    int tempU = u;
-                    int tempL = l;
-                    int tempD = d;
-                    int tempR = r;
+                    int up = upAndDown[0];
+                    int down = upAndDown[1];
+                    int left = leftAndRight[0];
+                    int right = leftAndRight[1];
 
-                    l = tempU;
-                    d = tempL;
-                    r = tempD;
-                    u = tempR;
+                    leftAndRight[0] = up;
+                    leftAndRight[1] = down;
+                    upAndDown[0] = right;
+                    upAndDown[1] = left;
                 } else {
-                    int tempU = u;
-                    int tempR = r;
-                    int tempD = d;
-                    int tempL = l;
+                    int up = upAndDown[0];
+                    int down = upAndDown[1];
+                    int front = frontAndBack[0];
+                    int back = frontAndBack[1];
 
-                    r = tempU;
-                    d = tempR;
-                    l = tempD;
-                    u = tempL;
+                    frontAndBack[0] = down;
+                    frontAndBack[1] = up;
+                    upAndDown[0] = front;
+                    upAndDown[1] = back;
                 }
 
-                board[ny][nx] = d;
+                board[ny][nx] = upAndDown[1];
             }
+
+            C = nx;
+            R = ny;
         }
 
         int ans = 0;
@@ -103,16 +96,16 @@ public class Main {
         System.out.print(ans);
     }
 
-    public static int getDirec(char c) {
-        if (c == 'L') return 2;
-        else if (c == 'R') return 3;
-        else if (c == 'U') return 0;
-        else return 1;
-    }
-
     public static boolean isRange(int x, int y) {
         if (x >= 0 && x < N && y >= 0 && y < N) return true;
 
         return false;
+    }
+
+    public static int getDirec(char direc) {
+        if (direc == 'R') return 0;
+        else if (direc == 'D') return 1;
+        else if (direc == 'L') return 2;
+        else return 3;
     }
 }
