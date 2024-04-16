4580ca873f1330b6a1286591be266c233adce69d
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-04-16 17:44:54 +0900

    Title: 핀볼게임, Time: 3084ms, Memory: 15MB, Status: Time Limit Exceed - CodeTree

---
+++ 240416/핀볼게임/pinball-game.java
@@ -0,0 +1,125 @@
+import java.util.*;
+
+public class Main {
+
+    public static int[] dx = {1, 0, -1, 0};
+    public static int[] dy = {0, 1, 0, -1};
+
+    public static int N;
+
+    public static int[][] board;
+
+    public static int ans = 0;
+
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        N = sc.nextInt();
+        board = new int[N][N];
+
+        for (int i = 0; i < N; i++) {
+            for (int k = 0; k < N; k++) {
+                board[i][k] = sc.nextInt();
+            }
+        }
+
+        // D
+        for (int c = 0; c < N; c++) {
+            int curX = c;
+            int curY = 0;
+
+            int sec = simulate(curX, curY, 1);
+
+            ans = Math.max(ans, sec);
+        }
+
+        // L
+        for (int r = 0; r < N; r++) {
+            int curX = N - 1;
+            int curY = r;
+
+            int sec = simulate(curX, curY, 2);
+
+            ans = Math.max(ans, sec);
+        }
+
+        // U
+        for (int c = 0; c < N; c++) {
+            int curX = c;
+            int curY = N - 1;
+
+            int sec = simulate(curX, curY, 3);
+
+            ans = Math.max(ans, sec);
+        }
+
+        // R
+        for (int r = 0; r < N; r++) {
+            int curX = 0;
+            int curY = r;
+
+            int sec = simulate(curX, curY, 0);
+
+            ans = Math.max(ans, sec);
+        }
+
+        System.out.print(ans);
+    }
+
+    public static int simulate(int x, int y, int direc) {
+        int sec = 1;
+
+        int curX = x;
+        int curY = y;
+
+        int curDirec = direc;
+
+        while (true) {
+            int nx = curX + dx[curDirec];
+            int ny = curY + dy[curDirec];
+
+            if (isRange(nx, ny) && board[ny][nx] == 0) {
+                sec++;
+
+                curX = nx;
+                curY = ny;
+            } else if (isRange(nx, ny) && board[ny][nx] != 0) {
+                sec++;
+
+                curX = nx;
+                curY = ny;
+
+                curDirec = turnDirec(curDirec, board[ny][nx]);
+            } else {
+                sec++;
+
+                break;
+            }
+        }
+
+        return sec;
+    }
+
+    public static boolean isRange(int x, int y) {
+        if (x >= 0 && x < N && y >= 0 && y < N) return true;
+
+        return false;
+    }
+
+    public static int turnDirec(int direc, int bar) {
+        if (direc == 0) {
+            if (bar == 1) return 3;
+            else return 1;
+        } else if (direc == 1) {
+            if (bar == 1) return 2;
+            else return 0;
+        } else if (direc == 2) {
+            if (bar == 1) return 1;
+            else return 3;
+        } else {
+            if (bar == 1) return 0;
+            else return 2;
+        }
+    }
+}
