378adf6e177a8185181c801268f628109db58143
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-04-22 12:28:12 +0900

    Title: xor 결과 최대 만들기, Time: 125ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240422/xor 결과 최대 만들기/max-of-xor.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+public class Main {
+
+    public static int N;
+    public static int M;
+    public static int[] list;
+
+    public static int[] memo;
+
+    public static int ans = 0;
+
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        N = sc.nextInt();
+        M = sc.nextInt();
+
+        list = new int[N];
+        memo = new int[M];
+        for (int i = 0; i < N; i++) {
+            list[i] = sc.nextInt();
+        }
+
+        perm(0, 0);
+
+        System.out.print(ans);
+    }
+
+    public static void perm(int idx, int depth) {
+        if (depth == M) {
+            check();
+            return;
+        }
+
+        for (int i = idx; i < N; i++) {
+            memo[depth] = i;
+            perm(i + 1, depth + 1);
+        }
+    }
+
+    public static void check() {
+        int temp = memo[0];
+
+        for (int m = 1; m < M; m++) {
+            temp ^= memo[m];
+        }
+
+        ans = Math.max(ans, temp);
+    }
+}
