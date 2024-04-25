12c484dac21eb323effd807cc9a93363afb8ac08
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-04-25 15:46:44 +0900

    Title: 단순한 동전 챙기기, Time: 167ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240425/단순한 동전 챙기기/collect-coins-easy.java
@@ -0,0 +1,87 @@
+import java.util.*;
+
+public class Main {
+
+    public static int N;
+    public static List<Pair> list = new ArrayList<>();
+
+    public static int[] memo = new int[3];
+
+    public static int[] start = new int[2];
+    public static int[] end = new int[2];
+
+    public static int ans = Integer.MAX_VALUE;
+
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        for (int r = 0; r < N; r++) {
+            char[] cList = sc.next().toCharArray();
+
+            for (int c = 0; c < N; c++) {
+                if (cList[c] >= '0' && cList[c] <= '9') {
+                    list.add(new Pair(c, r, cList[c] - '0'));
+                } else if (cList[c] == 'S') {
+                    start[0] = c;
+                    start[1] = r;
+                } else if (cList[c] == 'E') {
+                    end[0] = c;
+                    end[1] = r;
+                }
+            }
+        }
+
+        perm(0, 0);
+
+        if (ans == Integer.MAX_VALUE) System.out.print(-1);
+        else System.out.print(ans);
+    }
+
+    public static void getMinDist() {
+        int dist = 0;
+
+        int curX = start[0];
+        int curY = start[1];
+
+        for (int i = 0; i < 3; i++) {
+            Pair node = list.get(memo[i]);
+
+            dist += Math.abs(curX - node.x) + Math.abs(curY - node.y);
+
+            curX = node.x;
+            curY = node.y;
+        }
+
+        dist += Math.abs(curX - end[0]) + Math.abs(curY - end[1]);
+
+        ans = Math.min(ans, dist);
+    }
+
+    public static void perm(int idx, int depth) {
+        if (depth == 3) {
+            getMinDist();
+
+            return;
+        }
+
+        for (int i = idx; i < list.size(); i++) {
+            memo[depth] = i;
+            perm(i + 1, depth + 1);
+        }
+    }
+
+    public static class Pair {
+        public int x;
+        public int y;
+        public int value;
+
+        public Pair(int x, int y, int value) {
+            this.x = x;
+            this.y = y;
+            this.value = value;
+        }
+    }
+}
