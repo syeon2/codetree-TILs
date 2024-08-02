1b98bcbfb798b9d43280f2ea6b6246093c76db0f
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-08-03 06:56:16 +0900

    Title: 변수 값 복사하기, Time: 91ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240803/변수 값 복사하기/copying-variable-values.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int a = 1;
+        int b = 2;
+        int c = 3;
+
+        a = b = c;
+
+        bw.write(String.format("%d %d %d", a, b, c));
+        bw.flush();
+        bw.close();
+    }
+}
