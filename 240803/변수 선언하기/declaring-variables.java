e390149419cbe347a1c2d83dfad86a90f98c8395
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-08-03 06:25:27 +0900

    Title: 변수 선언하기, Time: 95ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240803/변수 선언하기/declaring-variables.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int a = 3;
+        char b = 'C';
+
+        bw.write(String.valueOf(a));
+        bw.newLine();
+        bw.write(String.valueOf(b));
+        
+        bw.flush();
+        bw.close();
+    }
+}
