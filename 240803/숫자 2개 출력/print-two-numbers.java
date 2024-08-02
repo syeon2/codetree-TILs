630ed8f9ca6a0dc69a557aab615b47334e15132b
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-08-03 06:16:22 +0900

    Title: 숫자 2개 출력, Time: 88ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240803/숫자 2개 출력/print-two-numbers.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write("3 5");
+        bw.flush();
+        bw.close();
+    }
+}
