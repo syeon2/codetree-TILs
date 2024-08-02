9803b5abc3174af9370854e361850d3ee289a82c
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-08-03 06:51:49 +0900

    Title: 변수 값 교체하기 3, Time: 86ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240803/변수 값 교체하기 3/replacing-variable-values-3.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int a = 3;
+        int b = 5;
+
+        int temp = a;
+        a = b;
+        b = temp;
+
+        bw.write(String.valueOf(a + "\n" + b));
+        bw.flush();
+        bw.close();
+    }
+}
