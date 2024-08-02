4468f3634c382737cf0201f32f3f98232bf4daa5
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-08-03 06:15:18 +0900

    Title: 숫자 출력하기, Time: 85ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240803/숫자 출력하기/print-one-number.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write(String.valueOf(3));
+        bw.flush();
+        bw.close();
+    }
+}
