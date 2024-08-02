2f065259c54fe9744da7480074a987f973d7fcfd
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-08-03 07:18:50 +0900

    Title: 입력받은 값 교체하기, Time: 95ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240803/입력받은 값 교체하기/changing-inputs.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] list = br.readLine().split(" ");
+        int a = Integer.parseInt(list[0]);
+        int b = Integer.parseInt(list[1]);
+
+        int temp = a;
+        a = b;
+        b = temp;
+
+        bw.write(a + " " + b);
+        bw.flush();
+        bw.close();
+    }
+}
