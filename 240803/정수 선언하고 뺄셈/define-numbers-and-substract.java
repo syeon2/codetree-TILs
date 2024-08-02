7a92a4320a6d20b38c02eec6c86cf2e85dcd65f2
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-08-03 06:23:01 +0900

    Title: 정수 선언하고 뺄셈, Time: 88ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240803/정수 선언하고 뺄셈/define-numbers-and-substract.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int a = 97;
+        int b = 13;
+
+        bw.write(String.valueOf(a + " - " + b + " = " + (a - b)));
+        bw.flush();
+        bw.close();
+    }
+}
