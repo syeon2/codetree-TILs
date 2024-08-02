e6f745cdaee459e556b781eeed64a264fb5f3790
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-08-03 07:46:34 +0900

    Title: 날짜 변경하여 출력, Time: 124ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240803/날짜 변경하여 출력/print-date-with-different-format.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] list = br.readLine().split("\\.");
+
+        bw.write(String.format("%s-%s-%s", list[1], list[2], list[0]));
+        bw.flush();
+        bw.close();
+    }
+}
