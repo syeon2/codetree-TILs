67a5a32cd8d008ac453b21fdd5775ded4cc04c07
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-08-03 11:04:53 +0900

    Title: 간단한 사칙연산, Time: 99ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240803/간단한 사칙연산/simple-arithmetic-operation.java
@@ -0,0 +1,26 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        String[] list = br.readLine().split(" ");
+        int a = Integer.parseInt(list[0]);
+        int b = Integer.parseInt(list[1]);
+
+        sb.append(a + b)
+            .append("\n")
+            .append(a - b)
+            .append("\n")
+            .append(a / b)
+            .append("\n")
+            .append(a % b);
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+}
