4c0c3e882a7ba174d192abf9367af85c4a58f2ba
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-08-03 11:22:00 +0900

    Title: 합과 차의 나눗셈, Time: 123ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240803/합과 차의 나눗셈/divide-of-sum-and-sub.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] list = br.readLine().split(" ");
+        int a = Integer.parseInt(list[0]);
+        int b = Integer.parseInt(list[1]);
+
+        double sum = a + b;
+        double sub = a - b;
+
+        bw.write(String.format("%.2f", sum / sub));
+        bw.flush();
+        bw.close();
+    }
+}
