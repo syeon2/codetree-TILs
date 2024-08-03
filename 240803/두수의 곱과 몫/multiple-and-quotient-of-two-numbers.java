b3fbe41796ba51a6609f5ab0a94999c0e29c2e96
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-08-03 11:11:21 +0900

    Title: 두수의 곱과 몫, Time: 88ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240803/두수의 곱과 몫/multiple-and-quotient-of-two-numbers.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        String[] list = br.readLine().split(" ");
+        int a = Integer.parseInt(list[0]);
+        int b = Integer.parseInt(list[1]);
+
+        sb.append(a).append(" * ").append(b).append(" = ").append(a * b).append("\n")
+            .append(a).append(" / ").append(b).append(" = ").append(a / b);
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+}
