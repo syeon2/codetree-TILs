2e0d0ee5b70cb9442988d069009fb481e2706113
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-08-06 08:42:17 +0900

    Title: 별 n개 출력하기, Time: 94ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240806/별 n개 출력하기/print-n-stars.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+
+        while (n-- > 0) {
+            sb.append("*").append("\n");
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+}
