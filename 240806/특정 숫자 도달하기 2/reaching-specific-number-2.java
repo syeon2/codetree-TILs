3178dd3e33bff6e5ecd1c921befd4e8d28b3fe47
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-08-06 08:26:29 +0900

    Title: 특정 숫자 도달하기 2, Time: 97ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240806/특정 숫자 도달하기 2/reaching-specific-number-2.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = n; i >= 1; i--) {
+            sb.append(i).append(" ");
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+}
