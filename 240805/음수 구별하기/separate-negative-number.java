f4d7be3738d2ef8fcfce65c774987dddd387c8a2
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-08-05 10:50:35 +0900

    Title: 음수 구별하기, Time: 90ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240805/음수 구별하기/separate-negative-number.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int a = Integer.parseInt(br.readLine());
+
+        bw.write(String.valueOf(a));
+        bw.newLine();
+        if (a < 0) {
+            bw.write("minus");
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}
