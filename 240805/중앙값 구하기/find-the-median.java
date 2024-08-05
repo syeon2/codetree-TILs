11b050b8dac61ccf64666578066c271b7e1fd9ec
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-08-05 17:02:46 +0900

    Title: 중앙값 구하기, Time: 99ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240805/중앙값 구하기/find-the-median.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] list = br.readLine().split(" ");
+        int a = Integer.parseInt(list[0]);
+        int b = Integer.parseInt(list[1]);
+        int c = Integer.parseInt(list[2]);
+
+        if (a >= b && a <= c) bw.write(String.valueOf(a));
+        else if (b >= a && b <= c) bw.write(String.valueOf(b));
+        else if (c >= a && c <= b) bw.write(String.valueOf(a));
+
+        bw.flush();
+        bw.flush();
+    }
+}
