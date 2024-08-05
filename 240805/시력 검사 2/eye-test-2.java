53b68d2c091c1ea1709805392ff4336469f247ba
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-08-05 11:26:47 +0900

    Title: 시력 검사 2, Time: 100ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240805/시력 검사 2/eye-test-2.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        double a = Double.parseDouble(br.readLine());
+
+        String ans = (a >= 1.0) ? "High" : (a >= 0.5) ? "Middle" : "Low";
+
+        bw.write(ans);
+        bw.flush();
+        bw.close();
+    }
+}
