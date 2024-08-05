19f7a97f61dca0a1c9dc81b2cea5009c2d61867f
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-08-05 16:06:56 +0900

    Title: 4가지 관계연산자, Time: 88ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240805/4가지 관계연산자/four-relational-operators.java
@@ -0,0 +1,30 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        String[] list = br.readLine().split(" ");
+        int a = Integer.parseInt(list[0]);
+        int b = Integer.parseInt(list[1]);
+
+        if (a >= b) sb.append("1").append("\n");
+        else sb.append("0").append("\n");
+
+        if (a > b) sb.append("1").append("\n");
+        else sb.append("0").append("\n");
+
+        if (a <= b) sb.append("1").append("\n");
+        else sb.append("0").append("\n");
+
+        if (a < b) sb.append("1").append("\n");
+        else sb.append("0").append("\n");
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+}
