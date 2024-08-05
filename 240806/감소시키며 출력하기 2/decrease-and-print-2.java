d7ec8e0b064c1199364d798d7a6ccd27e3c36ecf
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-08-06 08:50:11 +0900

    Title: 감소시키며 출력하기 2, Time: 92ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240806/감소시키며 출력하기 2/decrease-and-print-2.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int start = 26;
+        while (start >= 1) {
+            sb.append(start--).append(" ");
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+}
