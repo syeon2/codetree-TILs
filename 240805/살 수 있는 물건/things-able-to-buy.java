8151ab86bc2261e5c07bf494da6dacea20ab0839
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-08-05 11:28:55 +0900

    Title: 살 수 있는 물건, Time: 86ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240805/살 수 있는 물건/things-able-to-buy.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        String item = (n >= 3000) ? "book" : "mask";
+
+        bw.write(item);
+        bw.flush();
+        bw.close();
+    }
+}
