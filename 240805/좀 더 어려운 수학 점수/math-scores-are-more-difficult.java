21383e9bd1865aa3ac2af15cad37b4efacdfc3ca
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-08-05 16:39:56 +0900

    Title: 좀 더 어려운 수학 점수, Time: 86ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240805/좀 더 어려운 수학 점수/math-scores-are-more-difficult.java
@@ -0,0 +1,24 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] a = br.readLine().split(" ");
+        int aM = Integer.parseInt(a[0]);
+        int aE = Integer.parseInt(a[1]);
+
+        String[] b = br.readLine().split(" ");
+        int bM = Integer.parseInt(a[0]);
+        int bE = Integer.parseInt(a[1]);
+
+        if (aM > bM) bw.write("A");
+        else if (aM == bM && aE > bE) bw.write("A");
+        else bw.write("B");
+
+        bw.flush();
+        bw.close(); 
+    }
+}
