594b1fca8945f804d9bc694800324d83f6238526
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-08-06 10:19:40 +0900

    Title: a부터 b까지의 합, Time: 94ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240806/a부터 b까지의 합/sum-from-a-to-b.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] list = br.readLine().split(" ");
+        int a = Integer.parseInt(list[0]);
+        int b = Integer.parseInt(list[1]);
+
+        int sum = 0;
+        for (int i = a; i <= b; i++) {
+            sum += i;
+        }
+
+        bw.write(String.valueOf(sum));
+        bw.flush();
+        bw.close();
+    }
+}
