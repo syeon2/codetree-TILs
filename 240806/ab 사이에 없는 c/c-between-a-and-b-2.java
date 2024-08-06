74d6633598b9349777e9f6308599fb8b55f3bd7e
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-08-06 12:03:04 +0900

    Title: ab 사이에 없는 c, Time: 112ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240806/ab 사이에 없는 c/c-between-a-and-b-2.java
@@ -0,0 +1,26 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] list = br.readLine().split(" ");
+        int a = Integer.parseInt(list[0]);
+        int b = Integer.parseInt(list[1]);
+        int c = Integer.parseInt(list[2]);
+
+        boolean isFalse = true;
+
+        for (int i = a; i <= b; i++) {
+            if (i % c == 0) isFalse = false;
+        }
+
+        if (!isFalse) bw.write("NO");
+        else bw.write("YES");
+
+        bw.flush();
+        bw.close();
+    }
+}
