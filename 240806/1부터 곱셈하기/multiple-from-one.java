49863100c3fb435b9d6700c7bb62a4aabc997834
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-08-06 11:03:39 +0900

    Title: 1부터 곱셈하기, Time: 109ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240806/1부터 곱셈하기/multiple-from-one.java
@@ -0,0 +1,26 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int prod = 1;
+        int ans = 0;
+        for (int i = 1; i <= 10; i++) {
+            prod *= i;
+
+            if (prod >= n) {
+                ans = i;
+                break;
+            }
+        }
+
+        bw.write(String.valueOf(ans));
+        bw.flush();
+        bw.close();
+    }
+}
