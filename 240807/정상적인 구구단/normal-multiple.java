74cbed3d85a6dbd2ca024c589ed3d40f86068f8b
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-08-07 16:26:29 +0900

    Title: 정상적인 구구단, Time: 105ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240807/정상적인 구구단/normal-multiple.java
@@ -0,0 +1,26 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= n; i++) {
+            for (int k = 1; k <= n; k++) {
+                sb.append(i).append(" * ").append(k).append(" = ").append(i * k);
+
+                if (k != n) sb.append(", ");
+            }
+
+            sb.append("\n");
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+}
