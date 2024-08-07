7b526be248430b525ad59c8b67df190018314c86
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-08-07 10:30:35 +0900

    Title: 별표 출력하기 7, Time: 104ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240807/별표 출력하기 7/print-star-7.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= n; i++) {
+            for (int k = 1; k <= i; k++) {
+                sb.append("* ");
+            }
+            sb.append("\n");
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+}
