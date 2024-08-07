b853d168259224a7c20126e3b89e4d3816e5de3c
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-08-07 16:41:38 +0900

    Title: 숫자 피라미드 2, Time: 96ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240807/숫자 피라미드 2/number-pyramid-2.java
@@ -0,0 +1,26 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+
+        int cnt = 0;
+
+        for (int i = 1; i <= n; i++) {
+            for (int k = 1; k <= i; k++) {
+                sb.append(++cnt).append(" ");
+            }
+
+            sb.append("\n");
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+}
