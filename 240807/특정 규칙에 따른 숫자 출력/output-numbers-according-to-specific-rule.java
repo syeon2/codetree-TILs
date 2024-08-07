fb5405a1b58a41ec19d9c32be5e6eaf37f27545a
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-08-07 16:55:03 +0900

    Title: 특정 규칙에 따른 숫자 출력, Time: 83ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240807/특정 규칙에 따른 숫자 출력/output-numbers-according-to-specific-rule.java
@@ -0,0 +1,33 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+
+        int cnt = 1;
+
+        for (int i = n; i >= 1; i--) {
+
+            for (int k = n; k > i; k--) {
+                sb.append("  ");
+            }
+
+            for (int k = i; k >= 1; k--) {
+                sb.append(cnt++).append(" ");
+
+                if (cnt == 10) cnt = 1;
+            }
+
+            sb.append("\n");
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+}
