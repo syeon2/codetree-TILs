2f973603d76c178f83a74eb0c069b7469d801efa
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-08-09 13:15:06 +0900

    Title: 지그재그로 숫자 채우기, Time: 118ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240809/지그재그로 숫자 채우기/zigzag-numbering.java
@@ -0,0 +1,41 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        String[] list = br.readLine().split(" ");
+        int n = Integer.parseInt(list[0]);
+        int m = Integer.parseInt(list[1]);
+
+        int[][] board = new int[n][m];
+
+        int cnt = 0;
+        for (int i = 0; i < m; i++) {
+            if (i % 2 == 0) {
+                for (int k = 0; k < n; k++) {
+                    board[k][i] = cnt++;
+                }
+            } else {
+                for (int k = n - 1; k >= 0; k--) {
+                    board[k][i] = cnt++;
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int k = 0; k < m; k++) {
+                sb.append(board[i][k]).append(" ");
+            }
+
+            sb.append("\n");
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+}
