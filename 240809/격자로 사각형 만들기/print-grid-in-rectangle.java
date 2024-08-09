354b3072dfc9261d4d400badadc6073bb92503da
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-08-09 12:30:37 +0900

    Title: 격자로 사각형 만들기, Time: 94ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240809/격자로 사각형 만들기/print-grid-in-rectangle.java
@@ -0,0 +1,28 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+        int[][] nList = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int k = 0; k < n; k++) {
+                if (i == 0 || k == 0) nList[i][k] = 1;
+                else nList[i][k] = nList[i - 1][k] + nList[i - 1][k - 1] + nList[i][k - 1];
+
+                sb.append(nList[i][k]).append(" ");
+            }
+
+            sb.append("\n");
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+}
