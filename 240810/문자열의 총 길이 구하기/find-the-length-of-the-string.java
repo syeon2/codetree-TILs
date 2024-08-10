b1381e559b2243bc066e41dd26a8b3a86a088e2e
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-08-10 11:57:09 +0900

    Title: 문자열의 총 길이 구하기, Time: 138ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240810/문자열의 총 길이 구하기/find-the-length-of-the-string.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] list = br.readLine().split(" ");
+
+        int cnt = 0;
+        for (int i = 0; i < list.length; i++) {
+            cnt += list[i].length();
+        }
+
+        bw.write(String.valueOf(cnt));
+        bw.flush();
+        bw.close();
+    }
+}
