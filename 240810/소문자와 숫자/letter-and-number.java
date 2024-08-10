bdb9c424e37bf68a291bd34cd4be28cd415257de
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-08-10 15:09:23 +0900

    Title: 소문자와 숫자, Time: 97ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240810/소문자와 숫자/letter-and-number.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String str = br.readLine();
+
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+
+            if ((c >= 'a' && c <= 'z') || (c >= '0' && c <= '9')) bw.write(String.valueOf(c));
+            else if (c >= 'A' && c <= 'Z') bw.write(String.valueOf((char) (c - 'A' + 'a')));
+        }
+
+        bw.flush();
+        bw.close();
+    
+    }
+}
