770bfab1691cdcd15665cc936ead998905ff2724
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-08-10 14:55:06 +0900

    Title: 아스키코드표 맞추기, Time: 112ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240810/아스키코드표 맞추기/chart-of-ASCII.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] list = br.readLine().split(" ");
+
+        for (int i = 0; i < list.length; i++) {
+            char c = (char) Integer.parseInt(list[i]);
+
+            bw.write(String.valueOf(c) + " ");
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}
