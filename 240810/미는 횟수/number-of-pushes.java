0a5f0b61663260666a2538bd0f28e6725ca46e01
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-08-10 15:43:27 +0900

    Title: 미는 횟수, Time: 81ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240810/미는 횟수/number-of-pushes.java
@@ -0,0 +1,24 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String str1 = br.readLine();
+        String str2 = br.readLine();
+
+        int cnt = 0;
+        while (cnt <= str1.length()) {
+            if (str1.equals(str2)) break;
+
+            cnt++;
+            str1 = str1.substring(str1.length() - 1) + str1.substring(0, str1.length() - 1);
+        }
+
+        bw.write(String.valueOf(cnt));
+        bw.flush();
+        bw.close();
+    }
+}
