ff20f5b1ec528740891b2d8d94dbcadabedfce13
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-08-10 14:25:52 +0900

    Title: 양옆 문자 제거하기, Time: 86ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240810/양옆 문자 제거하기/del-both-side-char.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String str = br.readLine();
+
+        str = str.substring(0, 1) + str.substring(2, str.length() - 2) + str.substring(str.length() - 1);
+
+        bw.write(str);
+        bw.flush();
+        bw.close();
+    }
+}
