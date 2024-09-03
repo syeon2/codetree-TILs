cffc71191d8c2b97e670d2b4661e4558e27c434a
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-09-03 11:47:26 +0900

    Title: 함수를 이용한 윤년 판별, Time: 87ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 240903/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        bw.write(String.valueOf(isYunYear(n)));
+        bw.flush();
+        bw.close();
+    }
+
+    public static boolean isYunYear(int n) {
+        if (n % 4 == 0 || !(n % 100 == 0 && n % 400 != 0)) return true;
+        else return false;
+    }
+}
