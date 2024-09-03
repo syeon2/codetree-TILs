e4ae6d55e45726f83f3386b5023c46633aa4834f
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-09-03 11:07:28 +0900

    Title: 최대공약수 구하기, Time: 95ms, Memory: 8MB, Status: Passed - CodeTree

--- 240903/최대공약수 구하기/find-the-greatest-common-divisor.java
+++ 240903/최대공약수 구하기/find-the-greatest-common-divisor.java
@@ -10,19 +10,16 @@ public class Main {
         int n = Integer.parseInt(list[0]);
         int m = Integer.parseInt(list[1]);
 
-        if (n > m) {
-            int temp = n;
-            n = m;
-            m = temp;
-        }
-
-        int ans = 0;
-        for (int i = 1; i <= n; i++) {
-            if (n % i == 0 && m % i == 0) ans = i;
-        }
+        int ans = euclidean(n, m);
 
         bw.write(String.valueOf(ans));
         bw.flush();
         bw.close();
+    }
+
+    public static int euclidean(int n, int m) {
+        if (n % m == 0) return m;
+
+        return euclidean(m, n % m);
     }
 }
