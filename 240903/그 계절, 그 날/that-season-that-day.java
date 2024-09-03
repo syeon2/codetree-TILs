70c19da604de6a8003ca1bd06e072b5e85188169
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-09-03 13:40:30 +0900

    Title: 그 계절, 그 날, Time: 83ms, Memory: 8MB, Status: Passed - CodeTree

--- 240903/그 계절, 그 날/that-season-that-day.java
+++ 240903/그 계절, 그 날/that-season-that-day.java
@@ -28,7 +28,7 @@ public class Main {
         if (m == 2 && isYun(y) && d <= 29) return true;
         else if (m == 2 && !isYun(y) && d <= 28) return true;
         else if ((m == 1 || m == 3 || m == 5 || m == 7 || m == 8 || m == 10 || m == 12) && d <= 31) return true;
-        else if ((m == 4 || m == 6 || m == 9 || m == 11) && d <= 31) return true;
+        else if ((m == 4 || m == 6 || m == 9 || m == 11) && d <= 30) return true;
 
         return false;
     }
