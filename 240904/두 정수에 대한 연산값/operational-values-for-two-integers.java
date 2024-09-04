988beab599d48c5c6202736c499b795d19441cac
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-09-04 09:32:11 +0900

    Title: 두 정수에 대한 연산값, Time: 104ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240904/두 정수에 대한 연산값/operational-values-for-two-integers.java
@@ -0,0 +1,41 @@
+import java.io.*;
+
+public class Main {
+
+    static class InnerValue {
+        
+        public int value;
+
+        public InnerValue(int value) {
+            this.value = value;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] list = br.readLine().split(" ");
+        int a = Integer.parseInt(list[0]);
+        int b = Integer.parseInt(list[1]);
+
+        InnerValue A = new InnerValue(a);
+        InnerValue B = new InnerValue(b);
+        updateValue(A, B);
+
+        bw.write(A.value + " " + B.value);
+        bw.flush();
+        bw.close();
+    }
+
+    public static void updateValue(InnerValue a, InnerValue b) {
+        if (a.value > b.value) {
+            a.value += 25;
+            b.value *= 2;
+        } else {
+            b.value += 25;
+            a.value *= 2;
+        }
+    }
+}
