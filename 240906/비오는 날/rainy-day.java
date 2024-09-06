f4a4803ba8271f82ae794562213ce440f7066fa1
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-09-06 13:05:44 +0900

    Title: 비오는 날, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240906/비오는 날/rainy-day.java
@@ -0,0 +1,43 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int N = Integer.parseInt(br.readLine());
+
+        Date[] list = new Date[N];
+        for (int i = 0; i < N; i++) {
+            String[] str = br.readLine().split(" ");
+
+            list[i] = new Date(str[0], str[1], str[2]);
+        }
+
+        Date ans = null;
+
+        for (int i = 1; i < N; i++) {
+            if (ans == null && list[i].weather.equals("Rain")) ans = list[i];
+            else if (ans.date.compareTo(list[i].date) > 0 && list[i].weather.equals("Rain")) ans = list[i];
+        }
+
+        ans.printAns();
+    }
+
+    public static class Date {
+
+        public String date;
+        public String dayOfWeek;
+        public String weather;
+
+        public Date(String date, String dayOfWeek, String weather) {
+            this.date = date;
+            this.dayOfWeek = dayOfWeek;
+            this.weather = weather;
+        }
+
+        public void printAns() {
+            System.out.println(date + " " + dayOfWeek + " " + weather);
+        }
+    }
+}
