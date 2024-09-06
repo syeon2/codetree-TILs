7504365f1c7cc0d42a316784b495274c5d4db8f9
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-09-06 13:48:41 +0900

    Title: 줄 세우기, Time: 399ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 240906/줄 세우기/line-up-students.java
@@ -0,0 +1,63 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        Student[] list = new Student[N];
+        for (int i = 0; i < N; i++) {
+            Integer[] nList = Arrays.stream(br.readLine().split(" "))
+                .map(Integer::parseInt)
+                .toArray(Integer[]::new);
+            
+            list[i] = new Student(i + 1, nList[0], nList[1]);
+        }
+
+        Arrays.sort(list);
+
+        for (int i = 0; i < N; i++) {
+            list[i].printAns();
+        }
+    }
+
+    public static class Student implements Comparable<Student> {
+
+        public int num;
+        public int height;
+        public int weight;
+
+        public Student(int num, int height, int weight) {
+            this.num = num;
+            this.height = height;
+            this.weight = weight;
+        }
+
+        public void printAns() {
+            System.out.println(height + " " + weight + " " + num);
+        }
+
+        @Override
+        public int compareTo(Student s) {
+            if (this.height < s.height) {
+                return 1;
+            } else if (this.height > s.height) {
+                return -1;
+            }
+
+            if (this.weight < s.weight) {
+                return 1;
+            } else if (this.weight > s.weight) {
+                return -1;
+            }
+
+            if (this.num > s.num) {
+                return 1;
+            } else return -1;
+        }
+    }
+}
