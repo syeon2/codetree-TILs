f0dd49507c86fe86bec0d33eec8a611eec2a5870
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-09-06 11:34:54 +0900

    Title: 순서를 바꾸었을 때 같은 단어인지 판별하기, Time: 125ms, Memory: 12MB, Status: Passed - CodeTree

--- 240906/순서를 바꾸었을 때 같은 단어인지 판별하기/determine-same-word.java
+++ 240906/순서를 바꾸었을 때 같은 단어인지 판별하기/determine-same-word.java
@@ -10,23 +10,23 @@ public class Main {
         char[] list1 = br.readLine().toCharArray();
         char[] list2 = br.readLine().toCharArray();
 
-        int[] alp1 = new int[26];
-        int[] alp2 = new int[26];
+        int[] alp1 = new int[200];
+        int[] alp2 = new int[200];
 
         boolean ans = true;
 
         if (list1.length != list2.length) ans = false;
         else {
             for (int i = 0; i < list1.length; i++) {
-                int v1 = list1[i] - 'a';
-                int v2 = list2[i] - 'a';
+                int v1 = list1[i];
+                int v2 = list2[i];
 
                 alp1[v1]++;
                 alp2[v2]++;
             }
         }
 
-        for (int i = 0; i < 26; i++) {
+        for (int i = 0; i < 200; i++) {
             if (alp1[i] != alp2[i]) ans = false;
         }
 
