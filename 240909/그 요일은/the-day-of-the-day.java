5ff16220b375d588403ebae8bb64160bea7e6ab3
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-09-09 10:44:28 +0900

    Title: 그 요일은, Time: 89ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240909/그 요일은/the-day-of-the-day.java
@@ -0,0 +1,39 @@
+import java.io.*;
+
+public class Main {
+
+    public static int[] monthes = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    public static String[] dayOfWeek = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
+
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] list = br.readLine().split(" ");
+        int m1 = Integer.parseInt(list[0]);
+        int d1 = Integer.parseInt(list[1]);
+        int m2 = Integer.parseInt(list[2]);
+        int d2 = Integer.parseInt(list[3]);
+
+        String date = br.readLine();
+
+        int diff = numOfDay(m2, d2) - numOfDay(m1, d1);
+
+        int ans = diff / 7;
+        if (diff % 7 > 0) ans++;
+
+        bw.write(String.valueOf(ans));
+        bw.flush();
+        bw.close();
+    }
+
+    public static int numOfDay(int m, int d) {
+        int days = 0;
+        for (int i = 1; i < m; i++) {
+            days += monthes[i];
+        }
+
+        return (days + d);
+    }
+}
