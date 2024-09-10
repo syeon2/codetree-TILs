2093feba2ac0a58ffa7334936de7be821ef81e70
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-09-10 12:00:55 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 243ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240910/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.java
+++ 240910/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.java
@@ -8,59 +8,54 @@ public class Main {
 
         int[][] board = new int[2001][2001];
 
-        String[] strs = br.readLine().split(" ");
+        for (int i = 1; i <= 2; i++) {
+            String[] strs = strs = br.readLine().split(" ");
 
-        int x1 = Integer.parseInt(strs[0]) + 1000;
-        int y1 = Integer.parseInt(strs[1]) + 1000;
-        int x2 = Integer.parseInt(strs[2]) + 1000;
-        int y2 = Integer.parseInt(strs[3]) + 1000;
+            int x1 = Integer.parseInt(strs[0]) + 1000;
+            int y1 = Integer.parseInt(strs[1]) + 1000;
+            int x2 = Integer.parseInt(strs[2]) + 1000;
+            int y2 = Integer.parseInt(strs[3]) + 1000;
 
-        for (int y = y1; y < y2; y++) {
             for (int x = x1; x < x2; x++) {
-                board[y][x] = 1;
-            }
-        }
-
-        strs = br.readLine().split(" ");
-
-        x1 = Integer.parseInt(strs[0]) + 1000;
-        y1 = Integer.parseInt(strs[1]) + 1000;
-        x2 = Integer.parseInt(strs[2]) + 1000;
-        y2 = Integer.parseInt(strs[3]) + 1000;
-
-        for (int y = y1; y < y2; y++) {
-            for (int x = x1; x < x2; x++) {
-                board[y][x] = 0;
-            }
-        }
-
-        int ansX1 = 10000;
-        int ansX2 = -10000;
-        int ansY1 = 10000;
-        int ansY2 = -10000;
-
-        for (int y = 0; y < 2001; y++) {
-
-            for (int x = 0; x < 2001; x++) {
-                if (board[y][x] > 0) {
-                    ansX1 = Math.min(ansX1, x);
-                    ansX2 = Math.max(ansX2, x);
+                for (int y = y1; y < y2; y++) {
+                    if (i == 1) board[y][x] = 1;
+                    else board[y][x] = 0;
                 }
             }
+        }
+
+        int width = 0;
+        int height = 0;
+
+        for (int y = 0; y < 2001; y++) {
+            int start = -1;
+            int end = 0;
+
+            for (int x = 0; x < 2001; x++) {
+                if (board[y][x] > 0 && start == -1) start = x;
+                else if (board[y][x] > 0 && start > 0) end = x;
+            }
+
+            if (start == -1) continue;
+            width = Math.max(width, end - start + 1);
         }
 
         for (int x = 0; x < 2001; x++) {
+            int start = -1;
+            int end = 0;
 
             for (int y = 0; y < 2001; y++) {
-                if (board[y][x] > 0) {
-                    ansY1 = Math.min(ansY1, y);
-                    ansY2 = Math.max(ansY2, y);
-                }
+                if (board[y][x] > 0 && start == -1) start = y;
+                else if (board[y][x] > 0 && start > 0) end = y;
             }
+
+            if (start == -1) continue;
+            height = Math.max(height, end - start + 1);
         }
 
-        bw.write(String.valueOf((ansX2 - ansX1 + 1) * (ansY2 - ansY1 + 1)));
+        bw.write(String.valueOf(width * height));
         bw.flush();
         bw.close();
+        
     }
 }
