54e6513d8ff154d94929ecb69622fe4d89d0c92e
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-09-10 10:09:15 +0900

    Title: 사각형의 총 넓이 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240910/사각형의 총 넓이 2/total-width-of-a-rectangle2.java
@@ -0,0 +1,39 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[][] board = new int[201][201];
+
+        for (int i = 0; i < N; i++) {
+            String[] strs = br.readLine().split(" ");
+
+            int x1 = Integer.parseInt(strs[0]);
+            int y1 = Integer.parseInt(strs[1]);
+            int x2 = Integer.parseInt(strs[2]);
+            int y2 = Integer.parseInt(strs[3]);
+
+            for (int y = y1; y < y2; y++) {
+                for (int x = x1; x < x2; x++) {
+                    board[y][x]++;
+                }
+            }
+        }
+
+        int ans = 0;
+        for (int y = 0; y < 201; y++) {
+            for (int x = 0; x < 201; x++) {
+                if (board[y][x] >= 1) ans++;
+            }
+        }
+
+        bw.write(String.valueOf(ans));
+        bw.flush();
+        bw.close();
+    }
+}
