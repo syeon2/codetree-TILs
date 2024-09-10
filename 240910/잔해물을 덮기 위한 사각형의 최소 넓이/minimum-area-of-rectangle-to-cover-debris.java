75a6b28d1a1bc0f2851674e9521a20f81015db07
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-09-10 10:25:56 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 215ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240910/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.java
@@ -0,0 +1,63 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int[][] board = new int[2001][2001];
+        String[] strs = br.readLine().split(" ");
+
+        int x1 = Integer.parseInt(strs[0]) + 1000;
+        int y1 = Integer.parseInt(strs[1]) + 1000;
+        int x2 = Integer.parseInt(strs[2]) + 1000;
+        int y2 = Integer.parseInt(strs[3]) + 1000;
+
+        for (int i = y1; i < y2; i++) {
+            for (int k = x1; k < x2; k++) {
+                board[i][k]++;
+            }
+        }
+
+        strs = br.readLine().split(" ");
+
+        x1 = Integer.parseInt(strs[0]) + 1000;
+        y1 = Integer.parseInt(strs[1]) + 1000;
+        x2 = Integer.parseInt(strs[2]) + 1000;
+        y2 = Integer.parseInt(strs[3]) + 1000;
+
+        for (int i = y1; i < y2; i++) {
+            for (int k = x1; k < x2; k++) {
+                board[i][k] = 0;
+            }
+        }
+
+        int width = 0;
+        int height = 0;
+
+        for (int y = 0; y < 2001; y++) {
+            int temp = 0;
+
+            for (int x = 0; x < 2001; x++) {
+                if (board[y][x] > 0) temp++;
+            }
+
+            width = Math.max(width, temp);
+        }
+
+        for (int x = 0; x < 2001; x++) {
+            int temp = 0;
+
+            for (int y = 0; y < 2001; y++) {
+                if (board[y][x] > 0) temp++;
+            }
+
+            height = Math.max(height, temp);
+        }
+
+        bw.write(String.valueOf(width * height));
+        bw.flush();
+        bw.close();
+    }
+}
