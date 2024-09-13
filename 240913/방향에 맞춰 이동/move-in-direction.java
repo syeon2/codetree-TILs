d163cc5566b3328f979483b3558410ff22040799
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-09-13 10:03:10 +0900

    Title: 방향에 맞춰 이동, Time: 101ms, Memory: 8MB, Status: Passed - CodeTree

--- 240913/방향에 맞춰 이동/move-in-direction.java
+++ 240913/방향에 맞춰 이동/move-in-direction.java
@@ -1,6 +1,10 @@
 import java.io.*;
 
 public class Main {
+
+    public static int[] dx = {-1, 0, 0, 1};
+    public static int[] dy = {0, -1, 1, 0};
+
     public static void main(String[] args) throws IOException {
         // 여기에 코드를 작성해주세요.
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -8,8 +12,8 @@ public class Main {
 
         int N = Integer.parseInt(br.readLine());
 
-        int dx = 0;
-        int dy = 0;
+        int x = 0;
+        int y = 0;
 
         for (int n = 0; n < N; n++) {
             String[] list = br.readLine().split(" ");
@@ -17,13 +21,13 @@ public class Main {
             String dir = list[0];
             int move = Integer.parseInt(list[1]);
 
-            if (dir.equals("W")) dx -= move;
-            else if (dir.equals("S")) dy -= move;
-            else if (dir.equals("N")) dy += move;
-            else dx += move;
+            if (dir.equals("W")) x += (dx[0] * move);
+            else if (dir.equals("S")) y += (dy[1] * move);
+            else if (dir.equals("N")) y += (dy[2] * move);
+            else x += (dx[3] * move);
         }
 
-        bw.write(dx + " " + dy);
+        bw.write(x + " " + y);
         bw.flush();
         bw.close();
     }
