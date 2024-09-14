fd79171ec874ab037b7c2a8091d89a27acf106b4
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-09-14 09:56:58 +0900

    Title: 이상한 진수 2, Time: 88ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240914/이상한 진수 2/awkward-digits-2.java
@@ -0,0 +1,28 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        char[] list = br.readLine().toCharArray();
+        
+        int max = 0;
+        for (int i = 1; i < list.length; i++) {
+
+            int temp = 0;
+            for (int k = 0; k < list.length; k++) {
+                char value = (i == k) ? '1' : list[k];
+
+                temp = (temp * 2) + (value - '0');
+            }
+
+            max = Math.max(max, temp);
+        }
+
+        bw.write(String.valueOf(max));
+        bw.flush();
+        bw.close();
+    }
+}
