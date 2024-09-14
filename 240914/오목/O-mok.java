05a154799c527c61b1086ce77955eb4cf07d7bfa
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-09-14 15:21:38 +0900

    Title: 오목, Time: 100ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240914/오목/O-mok.java
@@ -0,0 +1,80 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int[][] board = new int[19][19];
+        for (int r = 0; r < 19; r++) {
+            String[] strs = br.readLine().split(" ");
+
+            for (int c = 0; c < 19; c++) {
+                board[r][c] = Integer.parseInt(strs[c]);
+            }
+        }
+
+        int ans = 0;
+        int ansR = 0;
+        int ansC = 0;
+
+        for (int r = 0; r < 19; r++) {
+
+            for (int c = 0; c <= 14; c++) {
+                int target = board[r][c];
+
+                if (target == 0) continue;
+
+                boolean isWin = true;
+                for (int i = c; i < c + 5; i++) {
+                    if (target != board[r][i]) isWin = false;
+                }
+
+                if (isWin) {
+                    ans = target;
+                    ansR = r + 1;
+                    ansC = c + 3;
+
+                    break;
+                }
+
+                if (r >= 15) break;
+
+                isWin = true;
+                for (int i = 0; i < 5; i++) {
+                    if (target != board[r + i][c + 1]) isWin = false;
+                }
+
+                if (isWin) {
+                    ans = target;
+                    ansR = r + 3;
+                    ansC = c + 3;
+
+                    break;
+                }
+
+                isWin = true;
+                for (int i = 0; i < 5; i++) {
+                    if (target != board[r + i][c]) isWin = false;
+                }
+
+                if (isWin) {
+                    ans = target;
+                    ansR = r + 3;
+                    ansC = c + 1;
+
+                    break;
+                }
+            }
+
+            if (ans != 0) break;
+        }
+
+        bw.write(String.valueOf(ans));
+        bw.newLine();
+        bw.write(ansR + " " + ansC);
+        bw.flush();
+        bw.close();
+    }
+}
