00bae16d8b09821a0fb1cadfad47c4acacd03e88
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-09-14 14:09:02 +0900

    Title: 원 모양으로 되어있는 방, Time: 118ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240914/원 모양으로 되어있는 방/a-room-in-a-circle.java
@@ -0,0 +1,32 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] nList = new int[N];
+        for (int i = 0; i < N; i++) {
+            nList[i] = Integer.parseInt(br.readLine());
+        }
+
+        int ans = Integer.MAX_VALUE;
+
+        for (int i = 0; i < N; i++) {
+            int temp = 0;
+
+            for (int k = 0; k < N; k++) {
+                if (i <= k) temp += (k - i) * nList[k];
+                else temp += (N - i + k) * nList[k];
+            }
+
+            ans = Math.min(ans, temp);
+        }
+
+        bw.write(String.valueOf(ans));
+        bw.flush();
+        bw.close();
+    }
+}
