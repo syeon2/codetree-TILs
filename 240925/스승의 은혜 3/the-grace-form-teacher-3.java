2cda3528d984ad10a46ebdcd1cfc9eaa5106c010
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-09-25 17:49:06 +0900

    Title: 스승의 은혜 3, Time: 209ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240925/스승의 은혜 3/the-grace-form-teacher-3.java
@@ -0,0 +1,67 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] strs = br.readLine().split(" ");
+        int N = Integer.parseInt(strs[0]);
+        int B = Integer.parseInt(strs[1]);
+
+        Node[] list = new Node[N];
+
+        for (int n = 0; n < N; n++) {
+            strs = br.readLine().split(" ");
+
+            int p = Integer.parseInt(strs[0]);
+            int s = Integer.parseInt(strs[1]);
+
+            list[n] = new Node(p, s);
+        }
+
+		Arrays.sort(list, (o1, o2) -> {
+			int price1 = (o1.p / 2) + o1.s;
+			int price2 = (o2.p / 2) + o2.s;
+
+			if (price1 > price2) {
+				return 1;
+			} else {
+				return -1;
+			}
+		});
+
+        int ans = 0;
+
+        for (int i = 0; i < N; i++) {
+            int tempB = B;
+
+            for (int k = 0; k < N; k++) {
+
+                if (i == k) tempB -= ((list[k].p / 2) + list[k].s);
+                else tempB -= (list[k].p + list[k].s);
+
+                if (tempB >= 0) {
+                    ans = Math.max(ans, k + 1);
+                } else break;
+            }
+        }
+
+        bw.write(String.valueOf(ans));
+        bw.flush();
+        bw.close();
+    }
+
+    public static class Node {
+
+        public int p;
+        public int s;
+
+        public Node(int p, int s) {
+            this.p = p;
+            this.s = s;
+        }
+    }
+}
