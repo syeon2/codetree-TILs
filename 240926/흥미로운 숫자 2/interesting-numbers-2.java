f5a3e6f3ee0623e0e16444c8074aa16d02a441fd
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-09-26 11:21:49 +0900

    Title: 흥미로운 숫자 2, Time: 107ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240926/흥미로운 숫자 2/interesting-numbers-2.java
@@ -0,0 +1,42 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] strs = br.readLine().split(" ");
+        int X = Integer.parseInt(strs[0]);
+        int Y = Integer.parseInt(strs[1]);
+
+        int cnt = 0;
+        for (int i = X; i <= Y; i++) {
+            if (isExcited(i)) cnt++;
+        }
+
+        bw.write(String.valueOf(cnt));
+        bw.flush();
+        bw.close();
+    }
+
+    public static boolean isExcited(int num) {
+        int[] list = new int[10];
+
+        while (num != 0) {
+            int n = num % 10;
+
+            list[n]++;
+
+            num /= 10;
+        }
+
+        int temp = 0;
+        for (int i = 0; i < 10; i++) {
+            if (list[i] == 1) temp++;
+        }
+
+        if (temp == 1) return true;
+        else return false;
+    }
+}
