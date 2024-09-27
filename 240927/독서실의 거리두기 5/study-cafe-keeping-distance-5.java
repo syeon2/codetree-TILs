9f1d644b3c9b898a1f169af8b890a44869c699d9
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-09-27 10:22:39 +0900

    Title: 독서실의 거리두기 5, Time: 88ms, Memory: 8MB, Status: Passed - CodeTree

--- 240927/독서실의 거리두기 5/study-cafe-keeping-distance-5.java
+++ 240927/독서실의 거리두기 5/study-cafe-keeping-distance-5.java
@@ -13,13 +13,21 @@ public class Main {
         for (int i = 0; i < N; i++) {
             if (list[i] == '1') continue;
             
+            list[i] = '1';
             int dist = 100;
+
             for (int k = 0; k < N; k++) {
                 if (list[k] == '0') continue;
 
-                dist = Math.min(dist, Math.abs(i - k));
+                for (int j = k + 1; j < N; j++) {
+                    if (list[j] == '0') continue;
+
+                    dist = Math.min(dist, Math.abs(k - j));
+                    break;
+                }
             }
 
+            list[i] = '0';
             ans = Math.max(ans, dist);
         }
 
